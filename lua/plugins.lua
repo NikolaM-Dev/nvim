b96a3882727841638ff325dfd9fc2f7db02cda34
Juan David Merchan Torres <juan1014166@gmail.com>
2022-05-14 15:45:15 -0500

    feat(lua): Add autocomplete required plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,4 +5,8 @@ return require('packer').startup(function()
   use 'kyazdani42/nvim-web-devicons' -- Web devicons
   use 'kyazdani42/nvim-tree.lua' -- File explorer
   use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
+  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+  use 'L3MON4D3/LuaSnip' -- Snippets plugin
 end)
