30a4b274209bb34d124a4b4f6534fc613c276e68
Juan David Merchan Torres <juan1014166@gmail.com>
2022-05-14 17:17:51 -0500

    feat(lua): Add pictograms to cmp

--- lua/autocomplete.lua
+++ lua/autocomplete.lua
@@ -1,5 +1,6 @@
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+local lspkind = require('lspkind')
 
 cmp.setup {
 	snippet = {
@@ -38,4 +39,16 @@ cmp.setup {
 		{ name = 'nvim_lsp' },
 		{ name = 'luasnip' },
 	},
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = 'symbol', -- show only symbol annotations
+			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+			-- The function below will be called before any actual modifications from lspkind
+			-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+			before = function(entry, vim_item)
+				return vim_item
+			end
+		})
+	}
 }
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,4 +9,5 @@ return require('packer').startup(function()
   use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  use 'onsails/lspkind.nvim' -- Pictograms for LSP symbols
 end)
