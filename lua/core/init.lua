029960515a66911507fc4817df41b46bc7d15836
Juan David Merchan Torres <juan1014166@gmail.com>
2022-05-19 18:02:14 -0500

    feat(core): Add leader map function

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -21,3 +21,9 @@ local createdir = function()
     end
   end
 end
+
+local leader_map = function()
+  vim.g.mapleader = " "
+  vim.api.nvim_set_keymap('n', ' ', '', { noremap = true })
+  vim.api.nvim_set_keymap('x', ' ', '', { noremap = true })
+end
