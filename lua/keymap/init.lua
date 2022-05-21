3750d10fbf31a5f9ae66bc98b76dc39da2280517
Juan David Merchan Torres <juan1014166@gmail.com>
2022-05-21 09:22:05 -0500

    feat(keymap): Add packer keymappings

---
+++ lua/keymap/init.lua
@@ -0,0 +1,16 @@
+local bind = require('keymap.bind')
+local map_cr = bind.map_cr
+local map_cu = bind.map_cu
+local map_cmd = bind.map_cmd
+local map_args = bind.map_args
+require('keymap.config')
+
+local plug_map = {
+  -- Packer
+  ["n|<leader>ps"] = map_cr("PackerSync"):with_silent():with_noremap():with_nowait(),
+  ["n|<leader>pu"] = map_cr("PackerUpdate"):with_silent():with_noremap():with_nowait(),
+  ["n|<leader>pi"] = map_cr("PackerInstall"):with_silent():with_noremap():with_nowait(),
+  ["n|<leader>pc"] = map_cr("PackerClean"):with_silent():with_noremap():with_nowait(),
+};
+
+bind.nvim_load_mapping(plug_map)
