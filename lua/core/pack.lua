a61d98d1200d43e6ff813b2b26c83a7f72fb0eb2
Juan David Merchan Torres <juan1014166@gmail.com>
2022-05-21 21:03:07 -0500

    feat(core): Add event to packer config

--- lua/core/pack.lua
+++ lua/core/pack.lua
@@ -48,7 +48,11 @@ function Packer:load_packer()
   packer.reset()
   local use = packer.use
   self:load_plugins()
-  use({ 'wbthomason/packer.nvim', opt = true })
+  use({
+    'wbthomason/packer.nvim',
+    event = 'VimEnter',
+    opt   = true,
+  })
   for _, repo in ipairs(self.repos) do
     use(repo)
   end
