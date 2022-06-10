61dc7a6e246777950f67999f3a5872aff21d2fdc
Juan David Merchan Torres <juan1014166@gmail.com>
2022-06-10 17:45:31 -0500

    feat(lsp): Add handlers initial config

---
+++ lua/modules/completion/lsp/handlers.lua
@@ -0,0 +1,144 @@
+local M = {}
+
+-- TODO: backfill this to template
+M.setup = function()
+	local signs = {
+		{ name = 'DiagnosticSignError', text = '' },
+		{ name = 'DiagnosticSignWarn', text = ' ' },
+		{ name = 'DiagnosticSignHint', text = '' },
+		{ name = 'DiagnosticSignInfo', text = '' },
+	}
+
+	for _, sign in ipairs(signs) do
+		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
+	end
+
+	local config = {
+		virtual_text = true,
+		signs = {
+			active = signs,
+		},
+		update_in_insert = true,
+		underline = true,
+		severity_sort = true,
+		float = {
+			focusable = false,
+			style = 'minimal',
+			border = 'rounded',
+			source = 'always',
+			header = '',
+			prefix = '',
+		},
+	}
+
+	vim.diagnostic.config(config)
+
+	vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+		border = 'rounded',
+	})
+
+	vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+		border = 'rounded',
+	})
+end
+
+local function lsp_highlight_document(client)
+	-- Set autocommands conditional on server_capabilities
+	local status_ok, illuminate = pcall(require, 'illuminate')
+	if not status_ok then
+		return
+	end
+	illuminate.on_attach(client)
+	-- end
+end
+
+local function lsp_keymaps(bufnr)
+	local opts = { noremap = true, silent = true }
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	-- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
+	vim.api.nvim_buf_set_keymap(
+		bufnr,
+		'n',
+		'gl',
+		'<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>',
+		opts
+	)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
+end
+
+M.on_attach = function(client, bufnr)
+	-- vim.notify(client.name .. " starting...")
+	-- TODO: refactor this into a method that checks if string in list
+	if client.name == 'tsserver' then
+		client.resolved_capabilities.document_formatting = false
+	end
+	lsp_keymaps(bufnr)
+	lsp_highlight_document(client)
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+local status_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+if not status_ok then
+	return
+end
+
+M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+
+return M
+-- -- Mappings.
+-- -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+-- local opts = { noremap = true, silent = true }
+-- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+-- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- -- Use an on_attach function to only map the following keys
+-- -- after the language server attaches to the current buffer
+-- local on_attach = function(client, bufnr)
+-- 	-- Enable completion triggered by <c-x><c-o>
+-- 	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+-- 	-- Mappings.
+-- 	-- See `:help vim.lsp.*` for documentation on any of the below functions
+-- 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+-- 	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+-- 	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+-- 	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+-- 	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+-- 	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+-- 	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+-- 	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+-- 	vim.keymap.set('n', '<space>wl', function()
+-- 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+-- 	end, bufopts)
+-- 	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+-- 	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+-- 	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+-- 	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+-- 	vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+-- end
+
+-- -- Use a loop to conveniently call 'setup' on multiple servers and
+-- -- map buffer local keybindings when the language server attaches
+-- local servers = { 'pyright', 'rust_analyzer', 'tsserver' }
+-- for _, lsp in pairs(servers) do
+-- 	require('lspconfig')[lsp].setup({
+-- 		on_attach = on_attach,
+-- 		flags = {
+-- 			-- This will be the default in neovim 0.7+
+-- 			debounce_text_changes = 150,
+-- 		},
+-- 	})
+-- end
