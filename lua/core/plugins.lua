38f848ad8af864cf4bcfeafe6ca443f6ef60bbd6
Juan David Merchan Torres <juan1014166@gmail.com>
2022-06-23 20:55:05 -0500

    feat(core): Add galaxyline plugin

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -90,6 +90,7 @@ return packer.startup(function(use)
 	use('kyazdani42/nvim-tree.lua') -- Tree view for files
 	use('lewis6991/impatient.nvim') -- Cache files for faster loading
 	use('norcalli/nvim-colorizer.lua') -- Color highlighter
+	use('glepnir/galaxyline.nvim') -- Status line
 
 	-- use('akinsho/bufferline.nvim')
 	-- use('moll/vim-bbye')
