6f5c09db126738ef26cf9637d2992a66a7354e57
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-01 11:58:19 -0500

    feat(plugin): Add colorsheme config - tokyonight

---
+++ after/plugin/colorscheme.lua
@@ -0,0 +1,9 @@
+local colorscheme = 'tokyonight'
+
+vim.g.tokyonight_style = 'night'
+
+local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
+if not status_ok then
+	vim.notify('colorscheme ' .. colorscheme .. ' not found!')
+	return
+end
