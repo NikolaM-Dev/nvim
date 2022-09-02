ca851bd7336c32421dcd95e9cb5013b5535cfae6
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-02 09:47:21 -0500

    feat(plugin): Add nvim tree mappings

---
+++ after/plugin/nvim_tree.lua
@@ -0,0 +1,8 @@
+local keymap = require('core.keymap')
+
+local cmd = keymap.cmd
+local nmap = keymap.nmap
+local opts = keymap.new_opts
+local silent, noremap = keymap.silent, keymap.noremap
+
+nmap({ '<leader>e', cmd('NvimTreeToggle'), opts(noremap, silent) })
