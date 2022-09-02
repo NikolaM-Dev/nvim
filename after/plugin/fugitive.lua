2d14fc3cfc40e79923f2b46b3a725182b3d4dbe4
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-01 21:13:12 -0500

    feat(plugin): Add fugitive mappings

---
+++ after/plugin/fugitive.lua
@@ -0,0 +1,19 @@
+local keymap = require('core.keymap')
+
+local cmd = keymap.cmd
+local nmap = keymap.nmap
+local opts = keymap.new_opts
+local noremap = keymap.noremap
+
+nmap({
+	{ '<leader>ga', cmd('G add --all'), opts(noremap) },
+	{ '<leader>gap', cmd('G add % -p'), opts(noremap) },
+	{ '<leader>gbm', ':G branch -m ', opts(noremap) },
+	{ '<leader>gca', cmd(':G commit --amend'), opts(noremap) },
+	{ '<leader>g', cmd(':G'), opts(noremap) },
+	{ '<leader>gfa', cmd(':G fetch --all -p'), opts(noremap) },
+	{ '<leader>gi', cmd(':G init'), opts(noremap) },
+	{ '<leader>gp', cmd(':G push origin -u HEAD'), opts(noremap) },
+	{ '<leader>gpl', cmd(':G pull'), opts(noremap) },
+	{ '<leader>gu', ':G reset --soft HEAD~1', opts(noremap) },
+})
