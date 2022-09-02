a217a644804ba66ffb46c0fcf40ee69e5d6c4611
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-02 09:34:55 -0500

    fix(plugin): Fix keymap must has rhs in packer

--- after/plugin/packer.lua
+++ after/plugin/packer.lua
@@ -5,6 +5,4 @@ local nmap = keymap.nmap
 local opts = keymap.new_opts
 local silent, noremap = keymap.silent, keymap.noremap
 
-nmap({
-	{ '<Leader>ps', cmd('PackerSync'), opts(noremap, silent) },
-})
+nmap({ '<leader>ps', cmd('PackerSync'), opts(noremap, silent) })
