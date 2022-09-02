a59530e4a7be7646487a9acadb362a93f9857138
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-02 09:14:30 -0500

    feat(core): Remove leader map setting

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,14 +1,6 @@
 pcall(require, 'impatient')
 
-local leader_map = function()
-	vim.g.mapleader = ' '
-	vim.api.nvim_set_keymap('n', ' ', '', { noremap = true })
-	vim.api.nvim_set_keymap('x', ' ', '', { noremap = true })
-end
-
 local load_core = function()
-	leader_map()
-
 	require('core.options')
 	require('core.mappings')
 	require('core.plugins')
