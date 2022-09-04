2db7a238a3aa60858abea4cc09512a7a2a90ee69
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-04 10:32:59 -0500

    feat(treesitter): Add context_commentstring setup

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -30,6 +30,7 @@ require('nvim-treesitter.configs').setup({
 		-- Instead of true it can also be a list of languages
 		additional_vim_regex_highlighting = false,
 	},
+	context_commentstring = { enable = true },
 	textobjects = {
 		select = {
 			enable = true,
