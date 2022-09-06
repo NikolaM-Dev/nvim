704239e82409f7ef90f35f988ab70cf73e35ab88
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-06 17:53:39 -0500

    feat(cmp): Remove lspconfig setup capabilities for emmet_ls

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -154,10 +154,3 @@ cmp.setup.cmdline(':', {
 		{ name = 'cmdline' },
 	}),
 })
-
--- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['emmet_ls'].setup({
-	capabilities = capabilities,
-})
