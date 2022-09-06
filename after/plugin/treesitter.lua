8d32597f1f081e2f0c1f5ca8b87d205ca1935a9b
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-06 11:45:56 -0500

    refactor(treesitter): Add pcall for treesitter.configs

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,4 +1,9 @@
-require('nvim-treesitter.configs').setup({
+local status_ok, configs = pcall(require, 'nvim-treesitter.configs')
+if not status_ok then
+	return
+end
+
+configs.setup({
 	-- A list of parser names, or "all"
 	ensure_installed = 'all',
 
