a9eaadd9b10423fcf0f50fb852df66cc9c5fa158
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-06 18:11:33 -0500

    style(plugins): Change float window boder style

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -33,7 +33,7 @@ end
 packer.init({
 	display = {
 		open_fn = function()
-			return require('packer.util').float({ border = 'single' })
+			return require('packer.util').float({ border = 'rounded' })
 		end,
 		done_sym = '',
 		error_sym = '',
