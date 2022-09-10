d129fe89156b6902c139794416cc38671147cc62
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-10 08:13:35 -0500

    feat(plugins): Remove float window popup for packer

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -32,9 +32,6 @@ end
 -- Have packer use a popup window
 packer.init({
 	display = {
-		open_fn = function()
-			return require('packer.util').float({ border = 'rounded' })
-		end,
 		done_sym = '',
 		error_sym = '',
 		moved_sym = 'ﰳ',
