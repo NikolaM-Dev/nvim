6571e405b0f8b2f6c2eb24d7576970fda0bbc1bd
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-10 14:32:10 -0500

    feat(undotree): Add undotree mapping

---
+++ after/plugin/undotree.lua
@@ -0,0 +1,8 @@
+local keymap = require('core.keymap')
+
+local cmd = keymap.cmd
+local nmap = keymap.nmap
+local opts = keymap.new_opts
+local noremap = keymap.noremap
+
+nmap({ '<leader>u', cmd('UndotreeShow'), opts(noremap) })
