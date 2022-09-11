eb3b62841107d191cb3fc64c68c5164647f01268
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-11 09:33:30 -0500

    feat(null_ls): Add base setup

---
+++ after/plugin/null_ls.lua
@@ -0,0 +1,29 @@
+local status, null_ls = pcall(require, 'null-ls')
+if not status then
+	return
+end
+
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
+null_ls.setup({
+	sources = {
+		null_ls.builtins.code_actions.eslint_d,
+		null_ls.builtins.code_actions.gitsigns,
+		null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.formatting.eslint_d,
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.stylua,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method('textDocument/formatting') then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd('BufWritePre', {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
+})
