0daaad8a648f0419de5e5f350bde2ba3d18de288
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-18 10:50:50 -0500

    feat(undotree): Change from show to toggle mapping

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -5,4 +5,4 @@ local nmap = keymap.nmap
 local opts = keymap.new_opts
 local noremap = keymap.noremap
 
-nmap({ '<leader>u', cmd('UndotreeShow'), opts(noremap) })
+nmap({ '<leader>u', cmd('UndotreeToggle'), opts(noremap) })
