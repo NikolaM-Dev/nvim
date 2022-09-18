375c6d11ec54cf5c75ad563a5b9617fae92b1439
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-17 21:01:10 -0500

    feat(null_ls): Add prettierd config

--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -7,11 +7,14 @@ local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
 null_ls.setup({
 	sources = {
+		null_ls.builtins.formatting.prettierd.with({
+			extra_args = { '--single-quote', '--trailing-comma', 'all' },
+			extra_filetypes = { 'toml' },
+		}),
 		null_ls.builtins.code_actions.eslint_d,
 		null_ls.builtins.code_actions.gitsigns,
 		null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.formatting.eslint_d,
-		null_ls.builtins.formatting.prettierd,
 		null_ls.builtins.formatting.stylua,
 	},
 	on_attach = function(client, bufnr)
