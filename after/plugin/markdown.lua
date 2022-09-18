2e2d183e3efb1c34ab1beef458a4a831420de0b8
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-18 10:50:08 -0500

    refactor(markdown): Change to lua script global variables

--- after/plugin/markdown.lua
+++ after/plugin/markdown.lua
@@ -5,13 +5,7 @@ local nmap = keymap.nmap
 local noremap = keymap.noremap
 local opts = keymap.new_opts
 
-vim.cmd([[ 
-" use a custom port to start server or empty for random
-let g:mkdp_port = '5000'
-
-" set default theme (dark or light)
-" By default the theme is define according to the preferences of the system
-let g:mkdp_theme = 'dark'
-]])
+vim.g.mkdp_port = '5000'
+vim.g.mkdp_theme = 'dark'
 
 nmap({ '<leader>md', cmd('MarkdownPreviewToggle'), opts(noremap) })
