0cfcf6bccd2ede1c13ae3d8bca1d583a9c68a996
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-22 02:55:21 -0500

    feat(null_ls): Change prettierd & eslintd by prettier & eslint

--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -7,14 +7,14 @@ local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
 null_ls.setup({
 	sources = {
-		null_ls.builtins.formatting.prettierd.with({
+		null_ls.builtins.formatting.prettier.with({
 			extra_args = { '--single-quote', '--trailing-comma', 'all' },
 			extra_filetypes = { 'toml' },
 		}),
-		null_ls.builtins.code_actions.eslint_d,
+		null_ls.builtins.code_actions.eslint,
 		null_ls.builtins.code_actions.gitsigns,
-		null_ls.builtins.diagnostics.eslint_d,
-		null_ls.builtins.formatting.eslint_d,
+		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.formatting.eslint,
 		null_ls.builtins.formatting.stylua,
 	},
 	on_attach = function(client, bufnr)
