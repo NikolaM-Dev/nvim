240c267e82ffaf0e58e107b67f4cd1daa4963260
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-23 03:04:19 -0500

    fix(explorer): Remove height view setting

--- after/plugin/explorer.lua
+++ after/plugin/explorer.lua
@@ -34,7 +34,6 @@ nvim_tree.setup({
 		adaptive_size = false,
 		centralize_selection = false,
 		width = 30,
-		height = 30,
 		hide_root_folder = false,
 		side = 'left',
 		preserve_window_proportions = false,
