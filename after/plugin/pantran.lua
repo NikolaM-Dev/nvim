e45dbdbbceb04544956473fc53b720a5d54001f9
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-25 18:21:35 -0500

    feat(pantran): Add base setup

---
+++ after/plugin/pantran.lua
@@ -0,0 +1,34 @@
+local status_ok, pantran = pcall(require, 'pantran')
+if not status_ok then
+	return
+end
+
+local keymap = require('core.keymap')
+
+local cmd = keymap.cmd
+local expr, noremap, silent = keymap.expr, keymap.noremap, keymap.silent
+local nmap, xmap = keymap.nmap, keymap.xmap
+local opts = keymap.new_opts
+
+pantran.setup({
+	default_engine = 'google',
+	engines = {
+		google = {
+			default_target = 'es',
+		},
+	},
+})
+
+nmap({
+	{
+		'<leader>tll',
+		function()
+			return pantran.motion_translate() .. '_'
+		end,
+		opts(noremap, silent),
+	},
+	{ '<leader>t', cmd('Pantran'), opts(noremap, silent) },
+	{ '<leader>tl', pantran.motion_translate, opts(expr, noremap, silent) },
+})
+
+xmap({ '<leader>tl', pantran.motion_translate, opts(expr, noremap, silent) })
