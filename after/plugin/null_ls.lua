2fe4c81f40ced61cd0f769a2909be63d536894df
Juan David Merchan Torres <juan1014166@gmail.com>
2022-09-28 18:23:02 -0500

    feat(null_ls): Add yamllint diagnostics source

--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -13,6 +13,7 @@ null_ls.setup({
 		}),
 		null_ls.builtins.code_actions.eslint_d,
 		null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.diagnostics.yamllint,
 		null_ls.builtins.formatting.eslint_d,
 		null_ls.builtins.formatting.stylua,
 	},
