c15c139855b1eed5dc0ed8708f2726a02b132c8f
Juan David Merchan Torres <juan1014166@gmail.com>
2022-10-17 17:57:56 -0500

    feat(comment): Add base setup

---
+++ after/plugin/comment.lua
@@ -0,0 +1,26 @@
+local status_ok, comment = pcall(require, 'mini.comment')
+if not status_ok then
+	return
+end
+
+comment.setup({
+	-- Module mappings. Use `''` (empty string) to disable one.
+	mappings = {
+		-- Toggle comment (like `gcip` - comment inner paragraph) for both
+		-- Normal and Visual modes
+		comment = 'gc',
+
+		-- Toggle comment on current line
+		comment_line = 'gcc',
+
+		-- Define 'comment' textobject (like `dgc` - delete whole comment block)
+		textobject = 'gc',
+	},
+	-- Hook functions to be executed at certain stage of commenting
+	hooks = {
+		-- Before successful commenting. Does nothing by default.
+		pre = function() end,
+		-- After successful commenting. Does nothing by default.
+		post = function() end,
+	},
+})
