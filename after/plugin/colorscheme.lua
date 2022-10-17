cd92afabc9d38f9efeff7b4de4a911f2a6588294
Juan David Merchan Torres <juan1014166@gmail.com>
2022-10-17 17:30:34 -0500

    feat(colorscheme): Change setup to current tokyo version

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -8,16 +8,17 @@ end
 tokyonight.setup({
 	-- your configuration comes here
 	-- or leave it empty to use the default settings
-	style = 'storm', -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+	style = 'storm', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+	light_style = 'day', -- The theme is used when the background is set to light
 	transparent = false, -- Enable this to disable setting the background color
 	terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
 	styles = {
 		-- Style to be applied to different syntax groups
-		-- Value is any valid attr-list value `:help attr-list`
-		comments = 'italic',
-		keywords = 'italic',
-		functions = 'bold',
-		variables = 'NONE',
+		-- Value is any valid attr-list value for `:help nvim_set_hl`
+		comments = { italic = true },
+		keywords = { italic = true },
+		functions = { bold = true },
+		variables = {},
 		-- Background styles. Can be "dark", "transparent" or "normal"
 		sidebars = 'dark', -- style for sidebars, see below
 		floats = 'dark', -- style for floating windows
@@ -29,12 +30,12 @@ tokyonight.setup({
 	lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 
 	--- You can override specific color groups to use other groups or a hex color
-	--- fucntion will be called with a ColorScheme table
+	--- function will be called with a ColorScheme table
 	---@param colors ColorScheme
 	on_colors = function(colors) end,
 
 	--- You can override specific highlights to use other groups or a hex color
-	--- fucntion will be called with a Highlights and ColorScheme table
+	--- function will be called with a Highlights and ColorScheme table
 	---@param highlights Highlights
 	---@param colors ColorScheme
 	on_highlights = function(highlights, colors) end,
