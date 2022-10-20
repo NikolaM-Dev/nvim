d1aa2f204333365117ce6fc86aa654ace6193152
Juan David Merchan Torres <juan1014166@gmail.com>
2022-10-20 08:31:03 -0500

    feat(neoclip): Add continuous sync

--- after/plugin/neoclip.lua
+++ after/plugin/neoclip.lua
@@ -14,7 +14,7 @@ neoclip.setup({
 	history = 1000,
 	enable_persistent_history = true,
 	length_limit = 1048576,
-	continuous_sync = false,
+	continuous_sync = true,
 	db_path = vim.fn.stdpath('data') .. '/databases/neoclip.sqlite3',
 	filter = nil,
 	preview = true,
