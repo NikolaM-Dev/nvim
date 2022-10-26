a6f1ca5ac8bc7419281c4377e98b43f9ae0b0a9a
Juan David Merchan Torres <juan1014166@gmail.com>
2022-10-26 09:29:11 -0500

    feat(null_ls): Add formatting only if null-ls server exists

--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -17,14 +17,20 @@ null_ls.setup({
 		null_ls.builtins.formatting.eslint_d,
 		null_ls.builtins.formatting.stylua,
 	},
-	on_attach = function(client, bufnr)
-		if client.supports_method('textDocument/formatting') then
+	on_attach = function(current_client, bufnr)
+		if current_client.supports_method('textDocument/formatting') then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 			vim.api.nvim_create_autocmd('BufWritePre', {
 				group = augroup,
 				buffer = bufnr,
 				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
+					vim.lsp.buf.format({
+						filter = function(client)
+							--  only use null-ls for formatting instead of lsp server
+							return client.name == 'null-ls'
+						end,
+						bufnr = bufnr,
+					})
 				end,
 			})
 		end
