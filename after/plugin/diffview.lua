c27b4025d4b96291bf8a26c98b7028a6eb5a9f85
Juan David Merchan Torres <juan1014166@gmail.com>
2022-12-18 09:23:11 -0500

    feat(diffview): Change flatten_dirs option

--- after/plugin/diffview.lua
+++ after/plugin/diffview.lua
@@ -54,7 +54,7 @@ diffview.setup({
 	file_panel = {
 		listing_style = 'tree', -- One of 'list' or 'tree'
 		tree_options = { -- Only applies when listing_style is 'tree'
-			flatten_dirs = true, -- Flatten dirs that only contain one single dir
+			flatten_dirs = false, -- Flatten dirs that only contain one single dir
 			folder_statuses = 'only_folded', -- One of 'never', 'only_folded' or 'always'.
 		},
 		win_config = { -- See ':h diffview-config-win_config'
