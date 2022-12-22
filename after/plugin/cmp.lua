b06de3901a9faa2fd7e43ff89945740010ab7951
Juan David Merchan Torres <juan1014166@gmail.com>
2022-12-22 10:37:05 -0500

    feat(cmp): Add emmet source

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -116,6 +116,7 @@ cmp.setup({
 		{ name = 'nvim_lsp' },
 		{ name = 'nvim_lua' },
 		{ name = 'luasnip' },
+		{ name = 'emmet' },
 		{ name = 'nvim_lsp_signature_help' },
 		{ name = 'buffer' },
 		{ name = 'path' },
