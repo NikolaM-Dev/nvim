cdd8e45a189ee60d95c120d049141f65e1adf771
Juan David Merchan Torres <juan1014166@gmail.com>
2022-12-24 14:25:49 -0500

    feat(cursorworld): Remove setup

--- after/plugin/cursorworld.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, cursorworld = pcall(require, 'mini.cursorword')
-if not status_ok then
-	return
-end
-
-cursorworld.setup({
-	-- Delay (in ms) between when cursor moved and when highlighting appeared
-	delay = 100,
-})
