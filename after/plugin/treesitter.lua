4d9287b38fb504c96032b6eed7eb5eb5c92d80cf
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-08 19:44:46 -0500

    feat(treesitter): Add disable utility for large files

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -23,11 +23,14 @@ configs.setup({
 		-- `false` will disable the whole extension
 		enable = true,
 
-		-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-		-- the name of the parser)
-		-- list of language that will be disabled
-		disable = {},
+		-- Disable slow treesitter highlight for large files
+		disable = function(_, buf)
+			local max_filesize = 100 * 1024 -- 100 KB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
 
 		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
