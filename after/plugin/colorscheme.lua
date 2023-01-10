c32d1bb32df1db0b8f75213ea10732fd930829cc
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-09 20:28:02 -0500

    style(colorscheme): Change nomenclature

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -37,13 +37,11 @@ tokyonight.setup({
 
 	--- You can override specific highlights to use other groups or a hex color
 	--- function will be called with a Highlights and ColorScheme table
-	---@param highlights Highlights
-	---@param colors ColorScheme
+	---@param hl Highlights
+	---@param c ColorScheme
 	---@diagnostic disable: undefined-field
-	on_highlights = function(highlights, colors)
-		highlights.TelescopeBorder = {
-			fg = colors.comment,
-		}
+	on_highlights = function(hl, c)
+		hl.TelescopeBorder = { fg = c.comment }
 	end,
 })
 
