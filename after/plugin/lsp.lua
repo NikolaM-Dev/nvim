b7236f2cd4e72ce4fa957ed4cc6600f94f8d7f77
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-13 18:47:04 -0500

    feat(lsp): Add definition mapping

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -38,6 +38,7 @@ local on_attach = function(client, bufnr)
 
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
 	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
 
 	if client.name == 'tsserver' then
