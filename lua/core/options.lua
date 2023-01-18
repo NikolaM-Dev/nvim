fbdb45f47e549081b2d2daeb87940a9174294f32
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-17 22:44:13 -0500

    feat(options): Add fsync option

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -39,12 +39,13 @@ local options = {
 	splitbelow = true, -- split window below
 	splitright = true, -- split window right
 	swapfile = false, -- creates a swapfile
+	fsync = false, -- This flushes the file to disk, ensuring that it is safely written
 	tabstop = 4, -- insert <x> spaces for a tab
 	termguicolors = true, -- set term gui colors (most terminals support this)
 	timeoutlen = 300, -- time to wait for a mapped sequence to complete (in milliseconds)
 	undodir = os.getenv('HOME') .. '/.cache/nvim/undodir', -- undodir path
 	undofile = true, -- make undo file
-	updatetime = 50, -- improve user experience updating more frequently
+	updatetime = 40, -- improve user experience updating more frequently
 	wrap = false, -- wrap lines
 	writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 }
