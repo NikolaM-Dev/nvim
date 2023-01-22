2e6cd2c31d3ce92523a7947f9c1ba386cb60e378
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-22 10:29:01 -0500

    feat(comment): Add base setup

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,15 @@
+return {
+	'echasnovski/mini.comment',
+	dependencies = 'JoosepAlviste/nvim-ts-context-commentstring',
+	event = 'VeryLazy',
+	opts = {
+		hooks = {
+			pre = function()
+				require('ts_context_commentstring.internal').update_commentstring({})
+			end,
+		},
+	},
+	config = function(_, opts)
+		require('mini.comment').setup(opts)
+	end,
+}
