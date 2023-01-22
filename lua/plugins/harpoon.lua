83e4952c10bb0d5d3f897c44b3ed1105f5279ba0
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-22 10:47:33 -0500

    feat(harpoon): Add base setup

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,56 @@
+return {
+	'ThePrimeagen/harpoon',
+	keys = function()
+		return {
+			{
+				'<leader>a',
+				function()
+					require('harpoon.mark').add_file()
+				end,
+				desc = 'Add file to harpoon',
+			},
+			{
+				'<C-e>',
+				function()
+					require('harpoon.ui').toggle_quick_menu()
+				end,
+				desc = 'Show harpoon marks',
+			},
+			{
+				'<leader>1',
+				function()
+					require('harpoon.ui').nav_file(1)
+				end,
+				'Navigation to file 1',
+			},
+			{
+				'<leader>2',
+				function()
+					require('harpoon.ui').nav_file(2)
+				end,
+				'Navigation to file 2',
+			},
+			{
+				'<leader>3',
+				function()
+					require('harpoon.ui').nav_file(3)
+				end,
+				'Navigation to file 3',
+			},
+			{
+				'<leader>4',
+				function()
+					require('harpoon.ui').nav_file(4)
+				end,
+				'Navigation to file 4',
+			},
+			{
+				'<leader>5',
+				function()
+					require('harpoon.ui').nav_file(5)
+				end,
+				'Navigation to file 5',
+			},
+		}
+	end,
+}
