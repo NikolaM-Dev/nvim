0406a55712d294eb227624b4e423ab4efac25c1c
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-24 11:35:08 -0500

    feat(after): Remove undotree setup

--- after/plugin/undotree.lua
+++
@@ -1,8 +0,0 @@
-local keymap = require('core.keymap')
-
-local cmd = keymap.cmd
-local nmap = keymap.nmap
-local noremap = keymap.noremap
-local opts = keymap.new_opts
-
-nmap({ '<leader>u', cmd('UndotreeToggle'), opts(noremap) })
