b9d438151d96986a84f34eac0ff9c1328df3796e
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-26 15:22:05 -0500

    feat(after): Remove packer setup

--- after/plugin/packer.lua
+++
@@ -1,8 +0,0 @@
-local keymap = require('core.keymap')
-
-local cmd = keymap.cmd
-local nmap = keymap.nmap
-local noremap, silent = keymap.noremap, keymap.silent
-local opts = keymap.new_opts
-
-nmap({ '<leader>ps', cmd('PackerSync'), opts(noremap, silent) })
