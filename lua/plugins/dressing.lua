6ab03bb33aae84721ebc1882995bff29f5a1e2ae
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-29 12:08:51 -0500

    style(dressing): Change formatting

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,14 +1,15 @@
 return {
 	'stevearc/dressing.nvim',
 	init = function()
-		---@diagnostic disable-next-line: duplicate-set-field
+		local lazy = require('lazy')
+
 		vim.ui.select = function(...)
-			require('lazy').load({ plugins = { 'dressing.nvim' } })
+			lazy.load({ plugins = { 'dressing.nvim' } })
 			return vim.ui.select(...)
 		end
-		---@diagnostic disable-next-line: duplicate-set-field
+
 		vim.ui.input = function(...)
-			require('lazy').load({ plugins = { 'dressing.nvim' } })
+			lazy.load({ plugins = { 'dressing.nvim' } })
 			return vim.ui.input(...)
 		end
 
