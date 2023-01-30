115b12b84dd43225c292455cc5fc2cb8693f6dc3
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-30 14:40:28 -0500

    feat(pantran): Change to config

--- lua/plugins/pantran.lua
+++ lua/plugins/pantran.lua
@@ -2,16 +2,26 @@ return {
 	'potamides/pantran.nvim',
 	cmd = 'Pantran',
 	keys = { { '<leader>tl', '<cmd>Pantran<cr>', desc = 'Translator' } },
-	opts = {
-		default_engine = 'google',
-		engines = {
-			google = {
-				fallback = {
-					default_source = 'es',
-					default_target = 'en',
+	config = function()
+		require('pantran').setup({
+			default_engine = 'google',
+			engines = {
+				google = {
+					fallback = {
+						default_source = 'es',
+						default_target = 'en',
+					},
 				},
 			},
-		},
-		window = { window_config = { border = 'rounded' } },
-	},
+			ui = {
+				height_percentage = 0.6,
+				width_percentage = 0.9,
+			},
+			select = {
+				prompt_prefix = ' ',
+				selection_caret = '  ',
+			},
+			window = { window_config = { border = 'rounded' } },
+		})
+	end,
 }
