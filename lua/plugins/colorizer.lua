3a918ad4e6b5bce5bd990241301376c615846a2b
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-30 14:12:46 -0500

    feat(colorizer): Change to config

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,19 +1,21 @@
 return {
 	'NvChad/nvim-colorizer.lua',
 	event = 'BufReadPre',
-	opts = {
-		buftype = { '*', '!prompt', '!nofile' },
-		filetypes = { '*', '!lazy' },
-		user_default_options = {
-			AARRGGBB = true,
-			css = true,
-			css_fn = true,
-			mode = 'background',
-			rgb_fn = true,
-			RRGGBBAA = true,
-			sass = { enable = true, parsers = { css } },
-			tailwind = 'both',
-			virtualtext = '■',
-		},
-	},
+	config = function()
+		require('colorizer').setup({
+			buftype = { '*', '!prompt', '!nofile' },
+			filetypes = { '*', '!lazy' },
+			user_default_options = {
+				AARRGGBB = true,
+				css = true,
+				css_fn = true,
+				mode = 'background',
+				rgb_fn = true,
+				RRGGBBAA = true,
+				sass = { enable = true, parsers = { css } },
+				tailwind = 'both',
+				virtualtext = '■',
+			},
+		})
+	end,
 }
