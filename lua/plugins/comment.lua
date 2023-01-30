83ded2d3ede63e3676fb9a3eb2d57ce6e140c8cb
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-30 11:53:53 -0500

    feat(comment): Remove initial table setup for pre hook

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -5,7 +5,7 @@ return {
 	opts = {
 		hooks = {
 			pre = function()
-				require('ts_context_commentstring.internal').update_commentstring({})
+				require('ts_context_commentstring.internal').update_commentstring()
 			end,
 		},
 	},
