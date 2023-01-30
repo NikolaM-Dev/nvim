5071d69a1bafdbdc71aed99ef625f29079b07614
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-30 14:41:55 -0500

    feat(pairs): Change to config

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -1,7 +1,7 @@
 return {
 	'echasnovski/mini.pairs',
 	event = 'VeryLazy',
-	config = function(_, opts)
-		require('mini.pairs').setup(opts)
+	config = function()
+		require('mini.pairs').setup()
 	end,
 }
