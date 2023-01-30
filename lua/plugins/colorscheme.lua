b24dce95f713ba9f74cc2bdfb9bd1aa3d345a8e4
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-30 18:21:05 -0500

    feat(colorscheme): Change to config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,31 +1,27 @@
 return {
-  'folke/tokyonight.nvim',
-  -- lazy = false,
-  event = 'VimEnter',
-  priority = 1000,
-  config = function()
-    local tokyonight = require('tokyonight')
+	'folke/tokyonight.nvim',
+	event = 'VimEnter',
+	priority = 1000,
+	config = function()
+		local tokyonight = require('tokyonight')
 
-    tokyonight.setup({
-      dim_inactive = true,
-      styles = {
-        -- Value is any valid attr-list value for `:help nvim_set_hl`
-        functions = { bold = true },
-      },
-      sidebars = {
-        'help',
-        'outline',
-        'qf',
-        'spectre_panel',
-        'startuptime',
-        'terminal',
-        'vista_kind',
-      },
-      on_highlights = function(hl, c)
-        hl.TelescopeBorder = { fg = c.comment }
-      end,
-    })
+		tokyonight.setup({
+			dim_inactive = true,
+			styles = { functions = { bold = true } },
+			sidebars = {
+				'help',
+				'outline',
+				'qf',
+				'spectre_panel',
+				'startuptime',
+				'terminal',
+				'vista_kind',
+			},
+			on_highlights = function(hl, c)
+				hl.TelescopeBorder = { fg = c.comment }
+			end,
+		})
 
-    tokyonight.load()
-  end,
+		tokyonight.load()
+	end,
 }
