7cb51eb13e9558da7b2c62d64fad7e3de11e0c45
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-30 14:35:54 -0500

    feat(move): Change to config

--- lua/plugins/move.lua
+++ lua/plugins/move.lua
@@ -1,19 +1,18 @@
 return {
 	'echasnovski/mini.move',
 	event = 'VeryLazy',
-	opts = {
-		mappings = {
-			down = '<M-J>',
-			left = '<M-H>',
-			line_down = '<M-J>',
-			line_left = '<M-H>',
-			line_right = '<M-L>',
-			line_up = '<M-K>',
-			right = '<M-L>',
-			up = '<M-K>',
-		},
-	},
-	config = function(_, opts)
-		require('mini.move').setup(opts)
+	config = function()
+		require('mini.move').setup({
+			mappings = {
+				down = '<M-J>',
+				left = '<M-H>',
+				line_down = '<M-J>',
+				line_left = '<M-H>',
+				line_right = '<M-L>',
+				line_up = '<M-K>',
+				right = '<M-L>',
+				up = '<M-K>',
+			},
+		})
 	end,
 }
