f27a6bf48acc119f1d5653b566a19995a4362496
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-30 14:36:34 -0500

    feat(neoclip): Change to config

--- lua/plugins/neoclip.lua
+++ lua/plugins/neoclip.lua
@@ -1,9 +1,11 @@
 return {
 	'AckslD/nvim-neoclip.lua',
 	keys = { { '<leader>c', '<cmd>Telescope neoclip<cr>', desc = 'Clipboard' } },
-	opts = {
-		continuous_sync = true,
-		db_path = os.getenv('HOME') .. '/.cache/nvim/neoclip/neoclip.sqlite3',
-		enable_persistent_history = true,
-	},
+	config = function()
+		require('neoclip').setup({
+			continuous_sync = true,
+			db_path = os.getenv('HOME') .. '/.cache/nvim/neoclip/neoclip.sqlite3',
+			enable_persistent_history = true,
+		})
+	end,
 }
