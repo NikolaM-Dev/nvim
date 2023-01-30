76299cc57166f4878e82677489f804bc52e321d3
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-30 11:51:46 -0500

    feat(hop): Fix invalid keymap

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,6 +1,6 @@
 return {
 	'phaazon/hop.nvim',
 	event = 'BufRead',
-	keys = { { 'f', '<cmd>Telescope buffers<cr>', desc = 'Jump to word', mode = { 'n', 'v' } } },
+	keys = { { 'f', '<cmd>HopWord<cr>', desc = 'Jump to word', mode = { 'n', 'v' } } },
 	opts = { keys = 'asdghklqwertyuiopzxcvbnmfj' },
 }
