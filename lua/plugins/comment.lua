41564d8bde15aa7772741375eff56fc92541f97e
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-30 14:22:56 -0500

    feat(comment): Change to config

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -2,14 +2,13 @@ return {
 	'echasnovski/mini.comment',
 	dependencies = 'JoosepAlviste/nvim-ts-context-commentstring',
 	event = 'VeryLazy',
-	opts = {
-		hooks = {
-			pre = function()
-				require('ts_context_commentstring.internal').update_commentstring()
-			end,
-		},
-	},
-	config = function(_, opts)
-		require('mini.comment').setup(opts)
+	config = function()
+		require('mini.comment').setup({
+			hooks = {
+				pre = function()
+					require('ts_context_commentstring.internal').update_commentstring()
+				end,
+			},
+		})
 	end,
 }
