28be847cd69e834531333ed0b926782665364a11
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-30 18:16:50 -0500

    feat(ufo): Change to config

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -2,16 +2,14 @@ return {
 	'kevinhwang91/nvim-ufo',
 	dependencies = 'kevinhwang91/promise-async',
 	event = 'BufReadPost',
-	opts = {
-		open_fold_hl_timeout = 40,
-	},
 	init = function()
-		-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 		vim.keymap.set('n', 'zR', function()
 			require('ufo').openAllFolds()
 		end)
 		vim.keymap.set('n', 'zM', function()
 			require('ufo').closeAllFolds()
 		end)
+
+		require('ufo').setup({ open_fold_hl_timeout = 40 })
 	end,
 }
