53d54de2cf2365d607282ccb5ae179d459445894
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-31 16:47:07 -0500

    fix(trailspace): Fix MiniTrailspace highlight

--- lua/plugins/trailspace.lua
+++ lua/plugins/trailspace.lua
@@ -1,11 +1,10 @@
-local c = require('tokyonight.colors').setup()
-
-vim.api.nvim_command('highlight! MiniTrailspace guibg=' .. c.bg .. 'guifg=' .. c.terminal_black)
-
 return {
 	'echasnovski/mini.trailspace',
 	event = 'VeryLazy',
-	config = function(_, opts)
-		require('mini.trailspace').setup(opts)
+	config = function()
+		require('mini.trailspace').setup()
+
+		local c = require('tokyonight.colors').setup()
+		vim.api.nvim_command('highlight! MiniTrailspace guibg=' .. c.bg .. 'guifg=' .. c.terminal_black)
 	end,
 }
