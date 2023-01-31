5ed190c80c76f05efbd20fa848210551ce4f5b2c
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-30 20:25:43 -0500

    feat(luasnip): Add base setup

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,42 +1,14 @@
 return {
-
-	-- snippets
 	{
 		'L3MON4D3/LuaSnip',
-		dependencies = {
-			'rafamadriz/friendly-snippets',
-			config = function()
-				require('luasnip.loaders.from_vscode').lazy_load()
-			end,
-		},
-		opts = {
-			history = true,
-			delete_check_events = 'TextChanged',
-		},
-		keys = {
-			{
-				'<tab>',
-				function()
-					return require('luasnip').jumpable(1) and '<Plug>luasnip-jump-next' or '<tab>'
-				end,
-				expr = true,
-				silent = true,
-				mode = 'i',
-			},
-			{
-				'<tab>',
-				function()
-					require('luasnip').jump(1)
-				end,
-				mode = 's',
-			},
-			{
-				'<s-tab>',
-				function()
-					require('luasnip').jump(-1)
-				end,
-				mode = { 'i', 's' },
-			},
-		},
+		build = 'make install_jsregexp',
+		config = function()
+			require('luasnip').config.set_config({
+				history = true,
+				delete_check_events = 'TextChanged',
+			})
+
+			require('luasnip.loaders.from_vscode').lazy_load({ paths = { './snippets/' } })
+		end,
 	},
 }
