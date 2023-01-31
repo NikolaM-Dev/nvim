8287fe1551fb8c9a42d01a831badfe9149078499
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-30 19:33:16 -0500

    feat(treesitter): Add base setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,82 +1,36 @@
 return {
 	{
-		'CKolkey/ts-node-action',
-		keys = {
-			{
-				mode = { 'n', 'x' },
-				'<leader>cn',
-				function()
-					require('ts-node-action').node_action()
-				end,
-				desc = 'Trigger Node Action',
-			},
-		},
-		config = true,
-	},
-
-	-- Show code context as top line
-	{
-		'nvim-treesitter/nvim-treesitter-context',
-		event = 'BufReadPre',
-		config = true,
-	},
-
-	-- Code tree based highlighting and other features
-	{
 		'nvim-treesitter/nvim-treesitter',
 		build = ':TSUpdate',
 		event = 'BufReadPost',
+		dependencies = {
+			'mrjones2014/nvim-ts-rainbow',
+			'nvim-treesitter/nvim-treesitter-context',
+			'windwp/nvim-ts-autotag',
+		},
 		keys = {
 			{ '<C-space>', desc = 'Increment selection' },
 			{ '<bs>', desc = 'Schrink selection', mode = 'x' },
 		},
-		dependencies = {
-			'mrjones2014/nvim-ts-rainbow',
-		},
-		opts = {
-			highlight = { enable = true },
-			indent = { enable = true },
-			context_commentstring = { enable = true, enable_autocmd = false },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = '<C-space>',
-					node_incremental = '<C-space>',
-					scope_incremental = '<nop>',
-					node_decremental = '<bs>',
+		config = function()
+			require('nvim-treesitter.configs').setup({
+				highlight = {
+					enable = true,
+					-- Disable slow treesitter highlight for large files
+					disable = function(_, buf)
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							return true
+						end
+					end,
 				},
-			},
-			autopairs = {
-				enable = true,
-			},
-			rainbow = {
-				enable = true,
-				extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-				max_file_lines = nil, -- Do not enable for files with more than n lines, int
-			},
-			matchup = { enable = true },
-			ensure_installed = {
-				'bash',
-				'comment',
-				'diff',
-				'git_rebase',
-				'gitignore',
-				'help',
-				'html',
-				'json',
-				'lua',
-				'markdown',
-				'markdown_inline',
-				'python',
-				'query',
-				'r',
-				'regex',
-				'vim',
-				'yaml',
-			},
-		},
-		config = function(_, opts)
-			require('nvim-treesitter.configs').setup(opts)
+				auto_install = true,
+				context_commentstring = { enable = true, enable_autocmd = false },
+				ensure_installed = 'all',
+				indent = { enable = true },
+				rainbow = { enable = true, extended_mode = false },
+			})
 		end,
 	},
 }
