a0de9d01855bdbd2652e9a5cdc264ed25a964c38
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-30 21:53:55 -0500

    feat(explorer): Add base setup

---
+++ lua/plugins/explorer.lua
@@ -0,0 +1,54 @@
+return {
+	'nvim-tree/nvim-tree.lua',
+	cmd = 'NvimTreeToggle',
+	keys = {
+		{ '<leader>e', '<cmd>NvimTreeToggle<CR>', desc = 'Explorer' },
+		{ '<leader><leader>e', '<cmd>NvimTreeCollapse<CR>', desc = 'Collapse recursively' },
+	},
+	config = function()
+		require('nvim-tree').setup({
+			view = {
+				mappings = {
+					list = {
+						{ key = 'd', action = 'trash' },
+						{ key = 'f', action = '' },
+						{ key = 'F', action = '' },
+						{ key = 'o', action = 'cd' },
+					},
+				},
+			},
+			renderer = {
+				icons = {
+					glyphs = {
+						bookmark = '',
+						default = '',
+						symlink = '',
+						modified = '●',
+						folder = {
+							arrow_closed = '',
+							arrow_open = '',
+							default = '',
+							empty = '󰉖',
+							empty_open = '󰷏',
+							open = 'ﱮ',
+							symlink = '󰉒',
+							symlink_open = '󰉒',
+						},
+						git = {
+							deleted = '',
+							ignored = '',
+							renamed = '',
+							staged = '',
+							unmerged = '',
+							unstaged = '',
+							untracked = 'ﲉ',
+						},
+					},
+				},
+			},
+			filters = {
+				custom = { '^.git$', '^node_modules$' },
+			},
+		})
+	end,
+}
