39e17fc55ed484b85d35368f8057f69a40b3b269
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-31 16:22:42 -0500

    feat(mason): Change to config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,26 +3,27 @@ return {
 	'williamboman/mason.nvim',
 	cmd = 'Mason',
 	keys = { { '<leader>cM', '<cmd>Mason<cr>', desc = 'Mason' } },
-	opts = {
-		ensure_installed = {
+	config = function()
+		local sources = {
 			'eslint_d',
 			'gofumpt',
 			'goimports',
 			'prettier',
 			'staticcheck',
 			'stylua',
-		},
-		ui = {
-			border = 'rounded',
-			icons = { package_installed = ' ', package_pending = ' ', package_uninstalled = ' ﮊ' },
-		},
-	},
-	---@diagnostic disable-next-line: unused-local
-	config = function(plugin, opts)
-		require('mason').setup(opts)
+		}
+
+		require('mason').setup({
+			ensure_installed = sources,
+			ui = {
+				border = 'rounded',
+				icons = { package_installed = ' ', package_pending = ' ', package_uninstalled = ' ﮊ' },
+			},
+		})
+
 		local mr = require('mason-registry')
-		for _, tool in ipairs(opts.ensure_installed) do
-			local p = mr.get_package(tool)
+		for _, s in ipairs(sources) do
+			local p = mr.get_package(s)
 			if not p:is_installed() then
 				p:install()
 			end
