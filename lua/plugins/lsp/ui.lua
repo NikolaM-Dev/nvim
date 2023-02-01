f02b519c0c168f0a6d9e056f5d5d5e9e6c814aea
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-01 18:19:34 -0500

    feat(lsp): Add setup_signs method

--- lua/plugins/lsp/ui.lua
+++ lua/plugins/lsp/ui.lua
@@ -1,5 +1,13 @@
 local M = {}
 
+function M.setup_signs()
+	local signs = { Error = '', Hint = '', Info = '', Warn = '' }
+
+	for name, icon in pairs(signs) do
+		local hl = 'DiagnosticSign' .. name
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	end
+end
 
 function M.setup_ui()
 	require('lspconfig.ui.windows').default_options.border = 'rounded'
