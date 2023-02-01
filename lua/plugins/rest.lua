f14bfa119b68310293686e8968c55ab0bcf0953d
Juan David Merchan Torres <juan1014166@gmail.com>
2023-01-31 20:36:53 -0500

    fix(rest): Fix invalid mappings

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -2,9 +2,27 @@ return {
 	'rest-nvim/rest.nvim',
 	ft = 'http',
 	keys = {
-		{ '<leader>rn', '<cmd>RestNvim<cr>', desc = 'Run the request under the cursor' },
-		{ '<leader>rp', '<cmd>RestNvimPreview<cr>', desc = 'Preview the request cURL command' },
-		{ '<leader>rl', '<cmd>RestNvimLast<cr>', desc = 'Re-Run the last request' },
+		{
+			'<leader>rn',
+			function()
+				require('rest-nvim').run()
+			end,
+			desc = 'Run the request under the cursor',
+		},
+		{
+			'<leader>rp',
+			function()
+				require('rest_nvim').last()
+			end,
+			desc = 'Preview the request cURL command',
+		},
+		{
+			'<leader>rl',
+			function()
+				require('rest_nvim').run(true)
+			end,
+			desc = 'Re-Run the last request',
+		},
 	},
 	config = function()
 		require('rest-nvim').setup()
