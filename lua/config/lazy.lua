eb1788cf63cac391d33031f35ba7fe072b56247a
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-02 09:55:53 -0500

    feat(lazy): Add typescript import

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
 	spec = {
 		{ import = 'plugins' },
+		{ import = 'plugins.langs.typescript' },
 	},
 	change_detection = { enabled = false, notify = false },
 	checker = { enabled = true, notify = false },
