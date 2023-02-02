76862c9a5b0d7b1b89d600e28934259aa2b0b872
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-01 21:14:55 -0500

    refactor(lsp): Change to more declative code

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -36,49 +36,17 @@ return {
 		config = function(_, opts)
 			local config = require('plugins.lsp.config')
 
+			config.servers = opts.servers
+			config.setup = opts.setup
+
 			config.on_attach(function(client, buffer)
 				require('plugins.lsp.keymaps').on_attach(client, buffer)
 			end)
 
-			config.setup_stuffs()
-
-			local servers = opts.servers
-			local capabilities =
-				require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-			local function setup(server)
-				local server_opts = servers[server] or {}
-				server_opts.capabilities = capabilities
-				if opts.setup[server] then
-					if opts.setup[server](server, server_opts) then
-						return
-					end
-				elseif opts.setup['*'] then
-					if opts.setup['*'](server, server_opts) then
-						return
-					end
-				end
-				require('lspconfig')[server].setup(server_opts)
-			end
-
-			local mlsp = require('mason-lspconfig')
-			local available = mlsp.get_available_servers()
-
-			local ensure_installed = {} ---@type string[]
-			for server, server_opts in pairs(servers) do
-				if server_opts then
-					server_opts = server_opts == true and {} or server_opts
-					-- run manual setup if "mason = false" or if this is a server that cannot be installed with mason-lspconfig
-					if server_opts.mason == false or not vim.tbl_contains(available, server) then
-						setup(server)
-					else
-						ensure_installed[#ensure_installed + 1] = server
-					end
-				end
-			end
-
-			mlsp.setup({ ensure_installed = ensure_installed })
-			mlsp.setup_handlers({ setup })
+			config.signs()
+			config.lsp_handlers()
+			config.diagnostics()
+			config.setup_servers()
 		end,
 	},
 }
