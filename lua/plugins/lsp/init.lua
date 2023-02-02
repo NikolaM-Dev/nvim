7371b6c7a1caef1d66a7542f102edb8bd9a610bb
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-01 20:42:14 -0500

    docs(lsp): Add setup docs

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -89,6 +89,9 @@ return {
 					},
 				},
 			},
+			-- you can do any additional lsp server setup here
+			-- return true if you don't want this server to be setup with lspconfig
+			---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
 			setup = {
 				-- example to setup with typescript.nvim
 				-- tsserver = function(_, opts)
