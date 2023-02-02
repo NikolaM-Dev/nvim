97369cd56ee84fc90f439fd1eb660855387b681b
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-02 09:58:40 -0500

    feat(lsp): Add folding range to capabilities

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -20,6 +20,14 @@ return {
 				tailwindcss = {},
 				vimls = {},
 			},
+			capabilities = {
+				textDocument = {
+					foldingRange = {
+						dynamicRegistration = false,
+						lineFoldingOnly = true,
+					},
+				},
+			},
 			-- you can do any additional lsp server setup here
 			-- return true if you don't want this server to be setup with lspconfig
 			---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
