c025625220a575c594594451467f35ba72604f13
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-02 09:50:24 -0500

    feat(keymaps): Add move lines mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -24,6 +24,14 @@ map('n', '<A-j>', '<cmd>resize +2<cr>', { desc = 'Increase window height' })
 map('n', '<A-k>', '<cmd>resize -2<cr>', { desc = 'Decrease window height' })
 map('n', '<A-l>', '<cmd>vertical resize +2<cr>', { desc = 'Increase window width' })
 
+-- Move Lines
+map('i', '<A-J>', '<Esc>:m .+1<cr>==gi', { desc = 'Move down' })
+map('i', '<A-K>', '<Esc>:m .-2<cr>==gi', { desc = 'Move up' })
+map('n', '<A-J>', ':m .+1<cr>==', { desc = 'Move down' })
+map('n', '<A-K>', ':m .-2<cr>==', { desc = 'Move up' })
+map('v', '<A-J>', ':m \'>+1<cr>gv=gv', { desc = 'Move down' })
+map('v', '<A-K>', ':m \'<-2<cr>gv=gv', { desc = 'Move up' })
+
 -- Clear search with <esc>
 map({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and clear hlsearch' })
 
