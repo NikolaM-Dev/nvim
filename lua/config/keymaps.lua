7c76952d8d0655b345cdc1a9e0d04eff3ee72db2
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-06 16:28:23 -0500

    feat(keymaps): Add silent by default

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,11 @@
 local function map(mode, lhs, rhs, opts)
 	local keys = require('lazy.core.handler').handlers.keys
 
+	---@cast keys LazyKeysHandler
 	-- do not create the keymap if a lazy keys handler exists
-	---@diagnostic disable-next-line: undefined-field
 	if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+		opts = opts or {}
+		opts.silent = opts.silent ~= false
 		vim.keymap.set(mode, lhs, rhs, opts)
 	end
 end
