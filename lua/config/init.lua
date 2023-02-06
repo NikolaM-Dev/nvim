64af28915fec52c9542d34092c4d684edca4ac84
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-06 16:23:29 -0500

    feat(config): Add better conditional autocmds & keymaps

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -2,7 +2,8 @@ require('config.globals')
 require('config.options')
 require('config.lazy')
 
-if vim.fn.argc() == 0 then
+if vim.fn.argc(-1) == 0 then
+	-- autocmds and keymaps can wait to load
 	vim.api.nvim_create_autocmd('User', {
 		group = vim.api.nvim_create_augroup('LazyVim', { clear = true }),
 		pattern = 'VeryLazy',
@@ -11,4 +12,8 @@ if vim.fn.argc() == 0 then
 			require('config.keymaps')
 		end,
 	})
+else
+	-- load them now so they affect the opened buffers
+	require('config.autocmds')
+	require('config.keymaps')
 end
