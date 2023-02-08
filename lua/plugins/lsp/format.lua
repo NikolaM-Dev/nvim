32667372ccc05a0e01cd165fbf411603a4ef2f95
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-08 16:39:39 -0500

    feat(lsp): Add format method to format module

--- lua/plugins/lsp/format.lua
+++ lua/plugins/lsp/format.lua
@@ -9,4 +9,23 @@ local function has_nls(buff)
 
 	return #require('null-ls.sources').get_available(ft, 'NULL_LS_FORMATTING') > 0
 end
+
+local function format()
+	local buf = vim.api.nvim_get_current_buf()
+
+	vim.lsp.buf.format(vim.tbl_deep_extend('force', {
+		bufnr = buf,
+		filter = function(client)
+			if is_go_tmpl(client) then
+				return true
+			end
+
+			if has_nls(buf) then
+				return client.name == 'null-ls'
+			end
+
+			return client.name ~= 'null-ls'
+		end,
+	}, {}))
+end
 return M
