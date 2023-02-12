08c3b66f0496bdfc0d43b43d68bebde7ad78bec2
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-12 15:31:54 -0500

    feat(luasnip): Remove unnecessary indent lvl

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,14 +1,11 @@
 return {
-	{
-		'L3MON4D3/LuaSnip',
-		build = 'make install_jsregexp',
-		config = function()
-			require('luasnip').config.set_config({
-				history = true,
-				delete_check_events = 'TextChanged',
-			})
+	'L3MON4D3/LuaSnip',
+	config = function()
+		require('luasnip').config.set_config({
+			history = true,
+			delete_check_events = 'TextChanged',
+		})
 
-			require('luasnip.loaders.from_vscode').lazy_load({ paths = { './snippets/' } })
-		end,
-	},
+		require('luasnip.loaders.from_vscode').lazy_load({ paths = { './snippets/' } })
+	end,
 }
