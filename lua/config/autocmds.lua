a3093acc714dee9b9c2766c739d873f127f82d82
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-18 17:10:21 -0500

    feat(autocmds): Add go to last loc autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -112,3 +112,20 @@ autocmd({ 'VimResized' }, {
 		vim.cmd('tabdo wincmd =')
 	end,
 })
+
+-- Go to last loc when opening a buffer
+autocmd('BufReadPre', {
+	group = _G.augroup('go_last_loc'),
+	pattern = '*',
+	callback = function()
+		vim.api.nvim_create_autocmd('filetype', {
+			pattern = '<buffer>',
+			once = true,
+			callback = function()
+				vim.cmd(
+					[[if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif]]
+				)
+			end,
+		})
+	end,
+})
