0ac8e4708e7df3ffbea61e997fb3d920ce4b610c
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-25 09:34:42 -0500

    feat(cmp): Remove "unnecessary" options

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -14,17 +14,6 @@ return {
 		{ 'jackieaskins/cmp-emmet', build = 'npm run release' },
 	},
 	config = function()
-		local check_backspace = function()
-			local col = vim.fn.col('.') - 1
-			return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
-		end
-
-		local has_words_before = function()
-			---@diagnostic disable-next-line: deprecated
-			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-		end
-
 		local kind_icons = {
 			Class = '',
 			Color = '',
@@ -68,11 +57,9 @@ return {
 				['<Tab>'] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_next_item()
-					elseif luasnip.expand_or_locally_jumpable() then
+					elseif luasnip.expand_or_jumpable() then
 						luasnip.expand_or_jump()
-					elseif has_words_before() then
-						cmp.complete()
-					elseif check_backspace() then
+					else
 						fallback()
 					end
 				end, { 'i', 's' }),
@@ -87,7 +74,7 @@ return {
 				end, { 'i', 's' }),
 				['<C-c>'] = cmp.mapping.abort(),
 				['<C-d>'] = cmp.mapping.scroll_docs(4),
-				['<C-Space>'] = cmp.mapping.complete(),
+				['<C-Space>'] = cmp.mapping.complete({}),
 				['<C-u>'] = cmp.mapping.scroll_docs(-4),
 				['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 			}),
