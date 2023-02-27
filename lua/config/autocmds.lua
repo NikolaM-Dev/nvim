d034e42f89f645138f3afaf4353200d3914febf9
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-27 08:40:21 -0500

    feat(autocmds): Change to only gitcommit ft open in vertical split

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -93,7 +93,7 @@ autocmd({ 'FileType' }, {
 -- Open in vertical split
 autocmd({ 'FileType' }, {
 	group = _G.augroup('open_in_right_vertical_split'),
-	pattern = { 'gitcommit', 'help', 'man' },
+	pattern = { 'gitcommit' },
 	callback = function()
 		vim.cmd.wincmd('L')
 	end,
