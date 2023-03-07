a52f28fa2a23008b587d8ce695c5cdc83e39fbfb
Juan David Merchan Torres <juan1014166@gmail.com>
2023-03-07 05:36:49 -0500

    feat(colorscheme): Add catppuccin colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,27 +1,54 @@
 return {
-	'folke/tokyonight.nvim',
-	event = 'VimEnter',
-	priority = 1000,
-	config = function()
-		local tokyonight = require('tokyonight')
+	{
+		'folke/tokyonight.nvim',
+		enabled = false,
+		lazy = false,
+		priority = 1000,
+		config = function()
+			local tokyonight = require('tokyonight')
 
-		tokyonight.setup({
-			dim_inactive = true,
-			styles = { functions = { bold = true } },
-			sidebars = {
-				'help',
-				'outline',
-				'qf',
-				'spectre_panel',
-				'startuptime',
-				'terminal',
-				'vista_kind',
-			},
-			on_highlights = function(hl, c)
-				hl.TelescopeBorder = { fg = c.comment }
-			end,
-		})
+			tokyonight.setup({
+				dim_inactive = true,
+				styles = { functions = { bold = true } },
+				sidebars = {
+					'help',
+					'outline',
+					'qf',
+					'spectre_panel',
+					'startuptime',
+					'terminal',
+					'vista_kind',
+				},
+				on_highlights = function(hl, c)
+					hl.TelescopeBorder = { fg = c.comment }
+				end,
+			})
 
-		tokyonight.load()
-	end,
+			tokyonight.load()
+		end,
+	},
+	{
+		'catppuccin/nvim',
+		event = 'VimEnter',
+		lazy = false,
+		priority = 1000,
+		config = function()
+			local catppuccin = require('catppuccin')
+
+			catppuccin.setup({
+				integrations = {
+					harpoon = true,
+					hop = true,
+					illuminate = true,
+					lsp_trouble = true,
+					mason = true,
+					mini = true,
+					ts_rainbow = true,
+				},
+				styles = { functions = { 'bold' } },
+			})
+
+			catppuccin.load()
+		end,
+	},
 }
