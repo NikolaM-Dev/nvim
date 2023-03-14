175bea714c7817ea79dd42eb1a6f58b31b4dfc41
Juan David Merchan Torres <juan1014166@gmail.com>
2023-03-14 06:48:08 -0500

    feat(cursorword): Add base setup

---
+++ lua/plugins/cursorword.lua
@@ -0,0 +1,10 @@
+return {
+	'echasnovski/mini.cursorword',
+	event = { 'BufNewFile', 'BufRead', 'BufAdd' },
+	config = function()
+		require('mini.cursorword').setup()
+
+		vim.api.nvim_command('highlight! MiniCursorwordCurrent guifg=NONE guibg=#3b4261 gui=NONE cterm=NONE')
+		vim.api.nvim_command('highlight! MiniCursorword guifg=NONE guibg=#3b4261 gui=NONE cterm=NONE')
+	end,
+}
