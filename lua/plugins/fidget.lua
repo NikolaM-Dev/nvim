70a013a0cb262fdff8a0148c7185f1d39ced51ce
Juan David Merchan Torres <juan1014166@gmail.com>
2023-03-26 12:02:37 -0500

    feat(fidget): Add ignore null ls source

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -3,6 +3,7 @@ return {
 	event = 'VeryLazy',
 	config = function()
 		require('fidget').setup({
+			sources = { ['null-ls'] = { ignore = true } },
 			text = { done = '', spinner = 'dots' },
 			window = { blend = 0 },
 		})
