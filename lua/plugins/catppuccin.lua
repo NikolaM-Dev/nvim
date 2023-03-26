30c1f04cf49175bf7b9c743c936252942cdb0158
Juan David Merchan Torres <juan1014166@gmail.com>
2023-03-26 13:04:38 -0500

    refactor(plugins): Change colorscheme structure

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,25 @@
+return {
+	'catppuccin/nvim',
+	event = 'VimEnter',
+	lazy = false,
+	priority = 1000,
+	config = function()
+		local catppuccin = require('catppuccin')
+
+		catppuccin.setup({
+			integrations = {
+				harpoon = true,
+				hop = true,
+				illuminate = true,
+				lsp_trouble = true,
+				mason = true,
+				mini = true,
+				notify = true,
+				ts_rainbow = true,
+			},
+			styles = { functions = { 'bold' } },
+		})
+
+		catppuccin.load()
+	end,
+}
