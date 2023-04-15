9d090e9f6a4619f2ed22d00fc39fb099d427f4b2
Juan David Merchan Torres <juan1014166@gmail.com>
2023-04-14 22:11:57 -0500

    feat(cmp): Remove occurrences limit

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -79,9 +79,9 @@ return {
 				['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 			}),
 			sources = cmp.config.sources({
-				{ name = 'nvim_lsp', max_item_count = 10 },
+				{ name = 'nvim_lsp' },
 				{ name = 'nvim_lua' },
-				{ name = 'luasnip', max_item_count = 3 },
+				{ name = 'luasnip' },
 				{ name = 'emmet' },
 				{ name = 'nvim_lsp_signature_help' },
 				{ name = 'buffer' },
