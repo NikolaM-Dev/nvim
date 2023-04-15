bf60e46b0d9a853f2745f9e1eed8370495d96b6f
Juan David Merchan Torres <juan1014166@gmail.com>
2023-04-14 22:12:26 -0500

    feat(cmp): Add tailwind colors support

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -89,8 +89,30 @@ return {
 				{ name = 'calc' },
 			}),
 			formatting = {
-				format = function(_, vim_item)
+				format = function(entry, vim_item)
+					if vim_item.kind == 'Color' and entry.completion_item.documentation then
+						local _, _, r, g, b =
+							string.find(entry.completion_item.documentation, '^rgb%((%d+), (%d+), (%d+)')
+
+						if r and g and b then
+							local color = string.format('%02x', r)
+								.. string.format('%02x', g)
+								.. string.format('%02x', b)
+							local group = 'Tw_' .. color
+
+							if vim.api.nvim_call_function('hlID', { group }) < 1 then
+								vim.api.nvim_command('highlight' .. ' ' .. group .. ' ' .. 'guifg=#' .. color)
+							end
+
+							vim_item.kind = '󱓻󰝤󰝤󰝤󰝤󰝤󰝤󰝤󱓻'
+							vim_item.kind_hl_group = group
+
+							return vim_item
+						end
+					end
+
 					vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
+
 					return vim_item
 				end,
 			},
