a1c8b6b49122cb27dbdc4c7e1661cb1de274786d
Juan David Merchan Torres <juan1014166@gmail.com>
2023-04-23 20:00:31 -0500

    feat(indent_blankline): Add buftype list to exclude

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -2,7 +2,8 @@ return {
 	'lukas-reineke/indent-blankline.nvim',
 	event = { 'BufReadPost', 'BufNewFile' },
 	opts = {
-		filetype_exclude = { 'help', 'Trouble', 'lazy' },
+		buftype_exclude = { 'help', 'loclist', 'nofile', 'prompt', 'quickfix', 'terminal' },
+		filetype_exclude = { 'help', 'Trouble', 'lazy', 'list' },
 		show_current_context_start = true,
 		show_current_context = true,
 		space_char_blankline = ' ',
