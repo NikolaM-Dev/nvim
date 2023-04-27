cff71aa55a7b965db70cd8bbe786d573b21b7bbf
Juan David Merchan Torres <juan1014166@gmail.com>
2023-04-27 13:49:22 -0500

    feat(catppuccin): Remove unknown properties

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -23,13 +23,13 @@ return {
 					PmenuSel = { bg = colors.surface0, fg = '' },
 
 					-- TODO: IDK 󱞤
-					CursorLineNr = { fg = colors.mauve },
-					GitSignsChange = { fg = colors.peach },
-					IndentBlanklineChar = { fg = colors.surface0 },
-					IndentBlanklineContextChar = { fg = colors.surface2 },
-					NormalFloat = { bg = colors.crust },
-					NvimTreeExecFile = { fg = colors.text },
-					VertSplit = { bg = colors.base, fg = colors.surface0 },
+					-- CursorLineNr = { fg = colors.mauve },
+					-- GitSignsChange = { fg = colors.peach },
+					-- IndentBlanklineChar = { fg = colors.surface0 },
+					-- IndentBlanklineContextChar = { fg = colors.surface2 },
+					-- NormalFloat = { bg = colors.crust },
+					-- NvimTreeExecFile = { fg = colors.text },
+					-- VertSplit = { bg = colors.base, fg = colors.surface0 },
 				}
 			end,
 		})
