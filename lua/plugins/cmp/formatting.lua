2dbf3d3e17315e164b8c6c5e6003caf0962c5af8
Juan David Merchan Torres <juan1014166@gmail.com>
2023-04-27 13:50:57 -0500

    feat(cmp): Add format function

--- lua/plugins/cmp/formatting.lua
+++ lua/plugins/cmp/formatting.lua
@@ -1,3 +1,5 @@
+local M = {}
+
 local kind_icons = {
 	Class = '',
 	Color = '',
@@ -26,3 +28,31 @@ local kind_icons = {
 	Value = '',
 	Variable = '',
 }
+
+function M.format(entry, vim_item)
+	if vim_item.kind == 'Color' and entry.completion_item.documentation then
+		local _, _, r, g, b =
+			---@diagnostic disable-next-line: param-type-mismatch
+			string.find(entry.completion_item.documentation, '^rgb%((%d+), (%d+), (%d+)')
+
+		if r and g and b then
+			local color = string.format('%02x', r) .. string.format('%02x', g) .. string.format('%02x', b)
+			local group = 'Tw_' .. color
+
+			if vim.api.nvim_call_function('hlID', { group }) < 1 then
+				vim.api.nvim_command('highlight' .. ' ' .. group .. ' ' .. 'guifg=#' .. color)
+			end
+
+			vim_item.kind = '󱓻󰝤󰝤󰝤󰝤󰝤󰝤󰝤󱓻'
+			vim_item.kind_hl_group = group
+
+			return vim_item
+		end
+	end
+
+	vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
+
+	return vim_item
+end
+
+return M
