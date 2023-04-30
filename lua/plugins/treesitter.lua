f50dab60c24b06b91a60ea1836fc1bc062dfbbfc
Juan David Merchan Torres <juan1014166@gmail.com>
2023-04-30 14:42:11 -0500

    feat(treesitter): Change rainbow by rainbow2

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,36 +1,40 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	build = ':TSUpdate',
-	event = { 'BufReadPost', 'BufNewFile' },
-	version = false,
-	dependencies = {
-		'mrjones2014/nvim-ts-rainbow',
-		'windwp/nvim-ts-autotag',
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ':TSUpdate',
+		event = { 'BufAdd', 'BufNewFile', 'BufReadPre' },
+		dependencies = { 'windwp/nvim-ts-autotag' },
+		keys = {
+			{ '<C-space>', desc = 'Increment selection' },
+			{ '<bs>', desc = 'Schrink selection', mode = 'x' },
+		},
+		config = function()
+			local enable = { enable = true }
+			require('nvim-treesitter.configs').setup({
+				highlight = {
+					enable = true,
+					-- Disable slow treesitter highlight for large files
+					disable = function(_, buf)
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							return true
+						end
+					end,
+				},
+				auto_install = true,
+				autotag = { enable = true },
+				context_commentstring = { enable = true, enable_autocmd = false },
+				ensure_installed = 'all',
+				indent = { enable = true },
+				matchup = { enable = true },
+				rainbow = enable,
+			})
+		end,
 	},
-	keys = {
-		{ '<C-space>', desc = 'Increment selection' },
-		{ '<bs>', desc = 'Schrink selection', mode = 'x' },
+	{
+		'HiPhish/nvim-ts-rainbow2',
+		dependencies = 'nvim-treesitter/nvim-treesitter',
+		event = { 'BufAdd', 'BufNewFile', 'BufReadPre' },
 	},
-	config = function()
-		require('nvim-treesitter.configs').setup({
-			highlight = {
-				enable = true,
-				-- Disable slow treesitter highlight for large files
-				disable = function(_, buf)
-					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
-			},
-			auto_install = true,
-			autotag = { enable = true },
-			context_commentstring = { enable = true, enable_autocmd = false },
-			ensure_installed = 'all',
-			indent = { enable = true },
-			matchup = { enable = true },
-			rainbow = { enable = true, extended_mode = false },
-		})
-	end,
 }
