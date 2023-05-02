d665a316dc54dee963b14ef015a963ec35926481
Juan David Merchan Torres <juan1014166@gmail.com>
2023-05-02 15:52:39 -0500

    feat(treesitter): Remove indent

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,46 +1,33 @@
 return {
-	{
-		'nvim-treesitter/nvim-treesitter',
-		build = ':TSUpdate',
-		event = { 'BufAdd', 'BufNewFile', 'BufReadPre' },
-		keys = {
-			{ '<C-space>', desc = 'Increment selection' },
-			{ '<bs>', desc = 'Schrink selection', mode = 'x' },
-		},
-		config = function()
-			local enable = { enable = true }
-			require('nvim-treesitter.configs').setup({
-				highlight = {
-					enable = true,
-					-- Disable slow treesitter highlight for large files
-					disable = function(_, buf)
-						local max_filesize = 100 * 1024 -- 100 KB
-						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-						if ok and stats and stats.size > max_filesize then
-							return true
-						end
-					end,
-				},
-				auto_install = true,
-				context_commentstring = { enable = true, enable_autocmd = false },
-				ensure_installed = 'all',
+	'nvim-treesitter/nvim-treesitter',
+	build = ':TSUpdate',
+	event = { 'BufAdd', 'BufNewFile', 'BufReadPre' },
+	keys = {
+		{ '<C-space>', desc = 'Increment selection' },
+		{ '<bs>', desc = 'Schrink selection', mode = 'x' },
+	},
+	config = function()
+		local enable = { enable = true }
+		require('nvim-treesitter.configs').setup({
+			highlight = {
+				enable = true,
+				-- Disable slow treesitter highlight for large files
+				disable = function(_, buf)
+					local max_filesize = 100 * 1024 -- 100 KB
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+				end,
+			},
+			auto_install = true,
+			context_commentstring = { enable = true, enable_autocmd = false },
+			ensure_installed = 'all',
 
-				autotag = enable,
-				indent = enable,
-				matchup = enable,
-				rainbow = enable,
-			})
-		end,
-	},
-	{
-		'HiPhish/nvim-ts-rainbow2',
-		dependencies = 'nvim-treesitter/nvim-treesitter',
-		event = { 'BufAdd', 'BufNewFile', 'BufReadPre' },
-	},
-
-	{
-		'windwp/nvim-ts-autotag',
-		dependencies = 'nvim-treesitter/nvim-treesitter',
-		event = { 'BufAdd', 'BufNewFile', 'BufReadPre' },
-	},
+			autotag = enable,
+			indent = enable,
+			matchup = enable,
+			rainbow = enable,
+		})
+	end,
 }
