2ac3f85c82615f5b625c0d44a2438b54b6b54b4a
Juan David Merchan Torres <juan1014166@gmail.com>
2023-05-02 15:51:55 -0500

    refactor(plugins): Remove color scheme abstraction

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,40 @@
+return {
+	'catppuccin/nvim',
+	lazy = false,
+	priority = 1000,
+	config = function()
+		local catppuccin = require('catppuccin')
+
+		catppuccin.setup({
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				harpoon = true,
+				hop = true,
+				illuminate = true,
+				lsp_trouble = true,
+				mason = true,
+				mini = true,
+				notify = true,
+				ts_rainbow2 = true,
+			},
+			styles = { functions = { 'bold' } },
+			custom_highlights = function(colors)
+				return {
+					PmenuSel = { bg = colors.surface0, fg = '' },
+
+					-- TODO: IDK 󱞤
+					-- CursorLineNr = { fg = colors.mauve },
+					-- GitSignsChange = { fg = colors.peach },
+					-- IndentBlanklineChar = { fg = colors.surface0 },
+					-- IndentBlanklineContextChar = { fg = colors.surface2 },
+					-- NormalFloat = { bg = colors.crust },
+					-- NvimTreeExecFile = { fg = colors.text },
+					-- VertSplit = { bg = colors.base, fg = colors.surface0 },
+				}
+			end,
+		})
+
+		catppuccin.load()
+	end,
+}
---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,28 @@
+return {
+	'folke/tokyonight.nvim',
+	enabled = false,
+	lazy = false,
+	priority = 1000,
+	config = function()
+		local tokyonight = require('tokyonight')
+
+		tokyonight.setup({
+			dim_inactive = true,
+			styles = { functions = { bold = true } },
+			sidebars = {
+				'help',
+				'outline',
+				'qf',
+				'spectre_panel',
+				'startuptime',
+				'terminal',
+				'vista_kind',
+			},
+			on_highlights = function(hl, c)
+				hl.TelescopeBorder = { fg = c.comment }
+			end,
+		})
+
+		tokyonight.load()
+	end,
+}
