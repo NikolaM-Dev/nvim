4566e49f3ac6f3bbf8fb5e2ca69fc0aa7aa7533b
Juan David Merchan Torres <juan1014166@gmail.com>
2023-05-03 16:21:37 -0500

    feat(fsread): Remove unnecessary mapping

--- lua/plugins/fsread.lua
+++ lua/plugins/fsread.lua
@@ -3,6 +3,5 @@ return {
 	cmd = { 'FSToggle', 'FSRead', 'FSClear' },
 	keys = {
 		{ '<leader>fs', '<cmd>FSToggle<cr>', desc = 'Toggle [F][S]Toggle' },
-		{ '<leader><leader>fs', '<cmd>FSRead<cr>', desc = 'Toggle [F][S][R]ead' },
 	},
 }
