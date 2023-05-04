8e576641960b0bd37d586c0367a4964a45ce5c66
Juan David Merchan Torres <juan1014166@gmail.com>
2023-05-04 17:04:50 -0500

    feat(indent_blankline): Add more events & toggle current context

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -1,11 +1,11 @@
 return {
 	'lukas-reineke/indent-blankline.nvim',
-	event = { 'BufReadPost', 'BufNewFile' },
+	event = { 'BufNewFile', 'BufReadPre', 'BufAdd' },
 	opts = {
 		buftype_exclude = { 'help', 'loclist', 'nofile', 'prompt', 'quickfix', 'terminal' },
 		filetype_exclude = { 'help', 'Trouble', 'lazy', 'list' },
 		show_current_context_start = true,
-		show_current_context = true,
+		show_current_context = false,
 		space_char_blankline = ' ',
 	},
 }
