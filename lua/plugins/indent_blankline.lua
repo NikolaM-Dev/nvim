abcc508dc45442d6b6d34abfb4089d0b8545db65
Juan David Merchan Torres <juan1014166@gmail.com>
2023-05-04 18:27:22 -0500

    feat(indent_blankline): Remove unused config

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -4,8 +4,5 @@ return {
 	opts = {
 		buftype_exclude = { 'help', 'loclist', 'nofile', 'prompt', 'quickfix', 'terminal' },
 		filetype_exclude = { 'help', 'Trouble', 'lazy', 'list' },
-		show_current_context_start = true,
-		show_current_context = false,
-		space_char_blankline = ' ',
 	},
 }
