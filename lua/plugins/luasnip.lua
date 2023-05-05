a0c2a36dfa082908c36398a18e14aa2b5aa13fab
Juan David Merchan Torres <juan1014166@gmail.com>
2023-05-04 19:10:10 -0500

    feat(luasnip): Fix problems

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,11 +1,13 @@
 return {
 	'L3MON4D3/LuaSnip',
+	build = 'make install_jsregexp',
 	config = function()
-		require('luasnip').config.set_config({
+		require('luasnip').setup({
 			history = true,
-			delete_check_events = 'TextChanged',
+			delete_check_events = { 'TextChanged', 'TextChangedI' },
+			region_check_events = { 'CursorMoved', 'CursorMovedI' },
 		})
 
-		require('luasnip.loaders.from_vscode').lazy_load({ paths = { './snippets/' } })
+		require('luasnip.loaders.from_vscode').lazy_load({ paths = { './snippets' } })
 	end,
 }
