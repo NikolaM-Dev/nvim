9f62f886566d3c69ba87a962144e8031cbac40ee
Juan David Merchan Torres <juan1014166@gmail.com>
2023-05-05 19:53:59 -0500

    feat(neoclip): Change paste mappings

--- lua/plugins/neoclip.lua
+++ lua/plugins/neoclip.lua
@@ -6,6 +6,12 @@ return {
 			continuous_sync = true,
 			db_path = os.getenv('HOME') .. '/.cache/nvim/neoclip/neoclip.sqlite3',
 			enable_persistent_history = true,
+			keys = {
+				telescope = {
+					i = { paste = '<cr>' },
+					n = { paste = '<cr>' },
+				},
+			},
 		})
 	end,
 }
