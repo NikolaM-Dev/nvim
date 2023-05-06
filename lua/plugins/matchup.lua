34929ac9cefe278a1130446f4e067a978f99c34d
Juan David Merchan Torres <juan1014166@gmail.com>
2023-05-05 19:46:08 -0500

    feat(matchup): Change method

--- lua/plugins/matchup.lua
+++ lua/plugins/matchup.lua
@@ -2,6 +2,6 @@ return {
 	'andymass/vim-matchup',
 	event = 'BufReadPost',
 	config = function()
-		vim.g.matchup_matchparen_offscreen = { method = 'status_manual' }
+		vim.g.matchup_matchparen_offscreen = { method = 'popup' }
 	end,
 }
