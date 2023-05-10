6304d4bb10b3e81a45af97c9930a0b76ccaa7193
Juan David Merchan Torres <juan1014166@gmail.com>
2023-05-10 10:52:11 -0500

    docs(trouble): Change mappings doc

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,12 +2,12 @@ return {
 	'folke/trouble.nvim',
 	cmd = { 'TroubleToggle', 'Trouble' },
 	keys = {
-		{ '<leader>;d', '<cmd>TroubleToggle document_diagnostics<cr>', desc = 'Document Diagnostics (Trouble)' },
-		{ '<leader>;l', '<cmd>TroubleToggle loclist<cr>', desc = 'Loclist (Trouble)' },
-		{ '<leader>;q', '<cmd>TroubleToggle quickfix<cr>', desc = 'Quickfix (Trouble)' },
-		{ '<leader>;w', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Workspace Diagnostics (Trouble)' },
-		{ '<leader>;x', '<cmd>TroubleToggle<cr>', desc = 'Toggle trouble' },
-		{ 'gR', '<cmd>TroubleToggle lsp_references<cr>', desc = 'Lsp References (Trouble)' },
+		{ 'gR', '<cmd>TroubleToggle lsp_references<cr>', desc = 'Trouble Lsp [R]eferences' },
+		{ '<leader>;d', '<cmd>TroubleToggle document_diagnostics<cr>', desc = 'Trouble [D]ocument [D]iagnostics' },
+		{ '<leader>;l', '<cmd>TroubleToggle loclist<cr>', desc = 'Trouble [L]oclist' },
+		{ '<leader>;q', '<cmd>TroubleToggle quickfix<cr>', desc = 'Trouble [Q]uickfix ' },
+		{ '<leader>;w', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Trouble [W]orkspace Diagnostics ' },
+		{ '<leader>;x', '<cmd>TroubleToggle<cr>', desc = 'Toggle Trouble' },
 	},
 	opts = {
 		auto_close = true,
