0843330520adaed8800d05c81ca663ac57445069
Juan David Merchan Torres <juan1014166@gmail.com>
2023-05-10 10:45:19 -0500

    feat(trailspace): Change highlight

--- lua/plugins/trailspace.lua
+++ lua/plugins/trailspace.lua
@@ -4,7 +4,6 @@ return {
 	config = function()
 		require('mini.trailspace').setup()
 
-		local c = require('catppuccin.palettes').get_palette('mocha')
-		vim.api.nvim_command('highlight! MiniTrailspace guibg=' .. c.base .. 'guifg=' .. c.red)
+		vim.api.nvim_command('highlight! MiniTrailspace guibg=NONE guifg=#45475a')
 	end,
 }
