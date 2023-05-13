119f88ee168ca51dc3a513abe3dadd249ac96929
Juan David Merchan Torres <juan1014166@gmail.com>
2023-05-13 09:27:58 -0500

    feat(indent_blankline): Change to disabled

--- lua/plugins/indent_blankline.lua
+++ lua/plugins/indent_blankline.lua
@@ -1,5 +1,6 @@
 return {
 	'lukas-reineke/indent-blankline.nvim',
+	enabled = false,
 	event = { 'BufNewFile', 'BufReadPre', 'BufAdd' },
 	opts = {
 		buftype_exclude = { 'help', 'loclist', 'nofile', 'prompt', 'quickfix', 'terminal' },
