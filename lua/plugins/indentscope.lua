1237357b96bcfd88de50ecad7dd470fc8a0c7b6c
Juan David Merchan Torres <juan1014166@gmail.com>
2023-05-13 09:43:04 -0500

    feat(indentscope): Change highlight to indent scope symbol

--- lua/plugins/indentscope.lua
+++ lua/plugins/indentscope.lua
@@ -3,5 +3,7 @@ return {
 	event = 'VeryLazy',
 	config = function()
 		require('mini.indentscope').setup({ symbol = '│' })
+
+		vim.api.nvim_command('highlight! MiniIndentscopeSymbol guibg=NONE guifg=#45475a')
 	end,
 }
