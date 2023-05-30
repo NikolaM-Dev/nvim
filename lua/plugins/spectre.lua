3bc040ffc2fe319d26153c17579ec2557df5eb46
Juan David Merchan Torres <juan1014166@gmail.com>
2023-05-30 13:52:11 -0500

    style(spectre): Change formatting

--- lua/plugins/spectre.lua
+++ lua/plugins/spectre.lua
@@ -3,23 +3,23 @@ return {
 	keys = {
 		{
 			'<leader>S',
-			'<cmd>lua require(\'spectre\').open()<CR>',
+			'<cmd>lua require(\'spectre\').open()<cr>',
 			desc = 'Open [S]pectre',
 		},
 		{
 			'<leader>sw',
-			'<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
+			'<cmd>lua require("spectre").open_visual({select_word=true})<cr>',
 			desc = '[S]earch Current [W]ord',
 		},
 		{
-			'<esc><cmd>lua require("spectre").open_visual()<CR>',
+			'<esc><cmd>lua require("spectre").open_visual()<cr>',
 			'<leader>sw',
 			desc = '[S]earch Current [W]ord',
 			mode = 'v',
 		},
 		{
 			'<leader>swp',
-			'<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+			'<cmd>lua require("spectre").open_file_search({select_word=true})<cr>',
 			desc = '[S]earch On Current File',
 			mode = 'n',
 		},
@@ -33,12 +33,12 @@ return {
 			mapping = {
 				run_replace = {
 					map = 'R',
-					cmd = '<cmd>lua require(\'spectre.actions\').run_replace()<CR>',
+					cmd = '<cmd>lua require(\'spectre.actions\').run_replace()<cr>',
 					desc = '[R]eplace All',
 				},
 				run_current_replace = {
 					map = 'C',
-					cmd = '<cmd>lua require(\'spectre.actions\').run_current_replace()<CR>',
+					cmd = '<cmd>lua require(\'spectre.actions\').run_current_replace()<cr>',
 					desc = 'Replace [C]urrent',
 				},
 			},
