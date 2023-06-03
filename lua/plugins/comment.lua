32eaae46d8630beffc22a132253b168d74937a51
Juan David Merchan Torres <juan1014166@gmail.com>
2023-06-03 16:25:22 -0500

    feat(comment): Add event

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,6 +1,6 @@
 return {
 	'numToStr/Comment.nvim',
-	keys = { { 'gc', mode = { 'n', 'x' } } },
+	event = 'BufReadPost',
 	dependencies = 'JoosepAlviste/nvim-ts-context-commentstring',
 	config = function()
 		require('Comment').setup({
