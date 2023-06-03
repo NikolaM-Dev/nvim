2dabd4fa3802bb54ff77c458ccf01be565878844
Juan David Merchan Torres <juan1014166@gmail.com>
2023-06-03 16:41:41 -0500

    feat(null_ls): Remove eslint use

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -5,17 +5,11 @@ return {
 	config = function()
 		local nls = require('null-ls')
 
-		local ca = nls.builtins.code_actions
 		local d = nls.builtins.diagnostics
 		local f = nls.builtins.formatting
 
 		nls.setup({
 			sources = {
-				d.eslint_d.with({
-					condition = function(utils)
-						return utils.root_has_file({ '.eslintrc.cjs', '.eslintrc.js' })
-					end,
-				}),
 				f.prettier.with({
 					extra_args = { '--single-quote', '--trailing-comma', 'all' },
 					condition = function(utils)
@@ -28,9 +22,7 @@ return {
 						return not utils.root_has_file({ '.eslintrc.cjs' })
 					end,
 				}),
-				ca.eslint_d,
 				d.staticcheck,
-				f.eslint_d,
 				f.gofumpt,
 				f.goimports,
 				f.stylua,
