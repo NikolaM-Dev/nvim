8347b9a0235a2218e0ae86a4e4d42852147f5faf
Juan David Merchan Torres <juan1014166@gmail.com>
2023-06-04 10:38:04 -0500

    feat(matchup): Add more configs

--- lua/plugins/matchup.lua
+++ lua/plugins/matchup.lua
@@ -2,6 +2,11 @@ return {
 	'andymass/vim-matchup',
 	event = 'BufReadPost',
 	config = function()
-		vim.g.matchup_matchparen_offscreen = { method = 'popup' }
+		vim.api.nvim_set_var('matchup_matchparen_deferred_hide_delay', 0)
+		vim.api.nvim_set_var('matchup_matchparen_deferred_show_delay', 42)
+		vim.api.nvim_set_var('matchup_matchparen_insert_timeout', 284)
+		vim.api.nvim_set_var('matchup_matchparen_offscreen', { method = 'popup' })
+		vim.api.nvim_set_var('matchup_matchparen_timeout', 284)
+		vim.api.nvim_set_var('matchup_text_obj_enabled', 0)
 	end,
 }
