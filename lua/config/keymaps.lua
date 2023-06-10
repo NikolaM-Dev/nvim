3d29e6fec02bdbbfd25d4ea55b7ec72b1a07da4a
Juan David Merchan Torres <juan1014166@gmail.com>
2023-06-10 08:29:35 -0500

    feat(keymaps): Add x mode to better up & down

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,8 +11,8 @@ local function map(mode, lhs, rhs, opts)
 end
 
 -- Better up/down
-map('n', 'k', 'v:count == 0 ? \'gk\' : \'k\'', { expr = true, silent = true })
-map('n', 'j', 'v:count == 0 ? \'gj\' : \'j\'', { expr = true, silent = true })
+map({ 'n', 'x' }, 'j', 'v:count == 0 ? \'gj\' : \'j\'', { expr = true, silent = true })
+map({ 'n', 'x' }, 'k', 'v:count == 0 ? \'gk\' : \'k\'', { expr = true, silent = true })
 
 -- Move Lines
 map('i', '<A-J>', '<Esc>:m .+1<cr>==gi', { desc = 'Move down' })
