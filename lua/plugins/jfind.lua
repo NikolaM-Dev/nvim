500fd9d989f7f0cd2c6ad66f8610e79784394589
Juan David Merchan Torres <juan1014166@gmail.com>
2023-06-09 21:46:14 -0500

    feat(jfind): Add base setup

---
+++ lua/plugins/jfind.lua
@@ -0,0 +1,28 @@
+return {
+	'jake-stewart/jfind.nvim',
+	keys = {
+		{ '<leader>ff', desc = 'j[J]find' },
+	},
+	config = function()
+		require('jfind').setup({
+			exclude = {
+				'build',
+				'.class',
+				'dist',
+				'.git',
+				'.idea',
+				'*.iml',
+				'*.meta',
+				'node_modules',
+				'__pycache__',
+				'.sass-cache',
+				'target',
+				'tmp',
+			},
+			border = 'rounded',
+			tmux = true,
+			formatPaths = true,
+			key = '<leader>ff',
+		})
+	end,
+}
