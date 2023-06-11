411285e8260e73c256425e2cea12ead04fc176dd
Juan David Merchan Torres <juan1014166@gmail.com>
2023-06-11 10:00:59 -0500

    feat(context): Add go to context mapping

--- lua/plugins/context.lua
+++ lua/plugins/context.lua
@@ -1,6 +1,15 @@
 return {
 	'nvim-treesitter/nvim-treesitter-context',
 	event = 'BufReadPre',
+	keys = {
+		{
+			';gc',
+			function()
+				require('treesitter-context').go_to_context()
+			end,
+			desc = '[G]oto [C]ontext',
+		},
+	},
 	config = function()
 		require('treesitter-context').setup()
 	end,
