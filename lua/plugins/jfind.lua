cdc306af3b12c41b10790c1db6e0afe85998009d
Juan David Merchan Torres <juan1014166@gmail.com>
2023-06-11 20:05:57 -0500

    feat(jfind): Add split mappings

--- lua/plugins/jfind.lua
+++ lua/plugins/jfind.lua
@@ -1,7 +1,22 @@
 return {
 	'jake-stewart/jfind.nvim',
+	branch = '1.0',
 	keys = {
-		{ '<leader>ff', desc = 'j[J]find' },
+		{
+			'<leader>ff',
+			function()
+				local Key = require('jfind.key')
+
+				require('jfind').findFile({
+					formatPaths = true,
+					callback = {
+						[Key.CTRL_V] = vim.cmd.vsplit,
+						[Key.CTRL_X] = vim.cmd.split,
+						[Key.DEFAULT] = vim.cmd.edit,
+					},
+				})
+			end,
+		},
 	},
 	config = function()
 		require('jfind').setup({
