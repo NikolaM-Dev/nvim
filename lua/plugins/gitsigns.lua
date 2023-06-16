159b6138be20065a841e4b0eac477e31b8c003c0
Juan David Merchan Torres <juan1014166@gmail.com>
2023-06-16 03:32:43 -0500

    fix(gitsigns): Fix deprecated nerfont icons

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,8 +6,8 @@ return {
 			signs = {
 				add = { text = '▎' },
 				change = { text = '▎' },
-				delete = { text = '契' },
-				topdelete = { text = '契' },
+				delete = { text = '󰐊 ' },
+				topdelete = { text = '󰐊 ' },
 				changedelete = { text = '▎' },
 				untracked = { text = '▎' },
 			},
