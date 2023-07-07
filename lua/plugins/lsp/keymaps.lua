1b7d38c03f89c4674fac3ed309c3c090fc015666
Juan David Merchan Torres <juan1014166@gmail.com>
2023-07-07 08:32:43 -0500

    style(lsp): Change formatting

--- lua/plugins/lsp/keymaps.lua
+++ lua/plugins/lsp/keymaps.lua
@@ -6,6 +6,7 @@ M._keys = nil
 local function diagnostic_goto(next, severity)
 	local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
 	severity = severity and vim.diagnostic.severity[severity] or nil
+
 	return function()
 		go({ severity = severity })
 		vim.diagnostic.open_float()
