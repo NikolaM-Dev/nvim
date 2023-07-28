85d00c401c9a3b4be7cca1314cd6b7f3201df284
Juan David Merchan Torres <juan1014166@gmail.com>
2023-07-28 11:00:05 -0500

    feat(browse): Add refold bookmark

--- lua/plugins/browse.lua
+++ lua/plugins/browse.lua
@@ -28,6 +28,7 @@ local bookmarks = {
 	null_ls = 'https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md',
 	prime = 'https://github.com/ThePrimeagen/init.lua',
 	reddit = 'https://www.reddit.com/r/neovim',
+	refold = 'https://academy.refold.la/library',
 	status_codes = 'https://developer.mozilla.org/es/docs/Web/HTTP/Status',
 	telescope = 'https://github.com/nvim-telescope/telescope.nvim/blob/master/doc/telescope.txt',
 	tokyonight = 'https://github.com/folke/tokyonight.nvim/blob/main/lua/tokyonight/colors.lua',
