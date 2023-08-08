93231efdf7e18ae3c12fabe67bf38db0d99e0e11
Juan David Merchan Torres <juan1014166@gmail.com>
2023-08-08 16:18:59 -0500

    feat(fsread): Change to disabled

--- lua/plugins/fsread.lua
+++ lua/plugins/fsread.lua
@@ -1,5 +1,6 @@
 return {
 	'nullchilly/fsread.nvim',
+	enabled = false,
 	cmd = { 'FSToggle', 'FSRead', 'FSClear' },
 	keys = {
 		{ '<leader>fs', '<cmd>FSToggle<cr>', desc = 'Toggle [F][S]Toggle' },
