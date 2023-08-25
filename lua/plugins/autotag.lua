012a6da672f972c4710466eb5fb5954fa4da59b4
Juan David Merchan Torres <juan1014166@gmail.com>
2023-08-24 19:34:34 -0500

    feat(autotag): Add base config

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -2,4 +2,9 @@ return {
 	'windwp/nvim-ts-autotag',
 	dependencies = 'nvim-treesitter/nvim-treesitter',
 	event = { 'BufAdd', 'BufNewFile', 'BufReadPre' },
+	config = function()
+		require('nvim-ts-autotag').setup({
+			enable = true,
+		})
+	end,
 }
