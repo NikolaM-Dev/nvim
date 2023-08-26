14c0b7fabda23d779bbe0653a88af1da034e1dd0
Juan David Merchan Torres <juan1014166@gmail.com>
2023-08-25 21:03:06 -0500

    feat(icons): Change yarn lock icon

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -3,7 +3,12 @@ return {
 	config = function()
 		local icons = require('nvim-web-devicons')
 
-		icons.setup()
+		icons.setup({
+			['yarn.lock'] = {
+				color = '#0288d1',
+				icon = '',
+				name = 'YarnLock',
+			},
 
 		icons.set_default_icon('󰈙', '#42a5f5')
 	end,
