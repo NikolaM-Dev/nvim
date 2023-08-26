307a4227bb1d7ff07aba67de09eacedf4b939e33
Juan David Merchan Torres <juan1014166@gmail.com>
2023-08-25 21:01:54 -0500

    feat(icons): Change default icon

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -1,6 +1,10 @@
 return {
 	'nvim-tree/nvim-web-devicons',
 	config = function()
-		require('nvim-web-devicons').setup()
+		local icons = require('nvim-web-devicons')
+
+		icons.setup()
+
+		icons.set_default_icon('󰈙', '#42a5f5')
 	end,
 }
