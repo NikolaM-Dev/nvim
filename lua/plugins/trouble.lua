c64e171def85a000c8bd136e77f29c6bd0352799
Juan David Merchan Torres <juan1014166@gmail.com>
2023-11-01 17:48:54 -0500

    feat(trouble): Add next & prev trouble/quickfix keymaps

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -8,6 +8,36 @@ return {
 		{ '<leader>;q', '<cmd>TroubleToggle quickfix<cr>', desc = 'Trouble [Q]uickfix ' },
 		{ '<leader>;w', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Trouble [W]orkspace Diagnostics ' },
 		{ '<leader>;x', '<cmd>TroubleToggle<cr>', desc = 'Toggle Trouble' },
+		{
+			'<leader>xk',
+
+			function()
+				if require('trouble').is_open() then
+					require('trouble').previous({ skip_groups = true, jump = true })
+				else
+					local ok, err = pcall(vim.cmd.cprev)
+					if not ok then
+						vim.notify(err, vim.log.levels.ERROR)
+					end
+				end
+			end,
+			desc = 'Previous trouble/quickfix item',
+		},
+		{
+			'<leader>xj',
+			function()
+				if require('trouble').is_open() then
+					require('trouble').next({ skip_groups = true, jump = true })
+				else
+					local ok, err = pcall(vim.cmd.cnext)
+					if not ok then
+						---@diagnostic disable-next-line: param-type-mismatch
+						vim.notify(err, vim.log.levels.ERROR)
+					end
+				end
+			end,
+			desc = 'Next trouble/quickfix item',
+		},
 	},
 	opts = {
 		auto_close = true,
