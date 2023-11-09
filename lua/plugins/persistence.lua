ae7f90995700f2e6952f557611c365bb75d494dd
Juan David Merchan Torres <juan1014166@gmail.com>
2023-11-09 13:01:07 -0500

    feat(persistence): Add base setup

---
+++ lua/plugins/persistence.lua
@@ -0,0 +1,21 @@
+return {
+	'folke/persistence.nvim',
+	event = 'BufReadPre',
+	keys = {
+		{
+			'<leader>rs',
+			function()
+				require('persistence').load()
+			end,
+			desc = '[R]estore [S]ession',
+		},
+	},
+	config = function()
+		require('persistence').setup({
+			options = vim.opt.sessionoptions:get(),
+			pre_save = function()
+				vim.cmd.mkview()
+			end,
+		})
+	end,
+}
