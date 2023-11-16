9f2067836cb47e93d4de8aa7729782146ee95660
Juan David Merchan Torres <juan1014166@gmail.com>
2023-11-15 19:22:39 -0500

    feat(trouble): Change trouble quickfix keymap

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -5,7 +5,7 @@ return {
 		{ 'gR', '<cmd>TroubleToggle lsp_references<cr>', desc = 'Trouble Lsp [R]eferences' },
 		{ '<leader>;d', '<cmd>TroubleToggle document_diagnostics<cr>', desc = 'Trouble [D]ocument [D]iagnostics' },
 		{ '<leader>;l', '<cmd>TroubleToggle loclist<cr>', desc = 'Trouble [L]oclist' },
-		{ '<leader>;q', '<cmd>TroubleToggle quickfix<cr>', desc = 'Trouble [Q]uickfix ' },
+		{ '<leader>;Q', '<cmd>TroubleToggle quickfix<cr>', desc = 'Trouble [Q]uickfix ' },
 		{ '<leader>;w', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Trouble [W]orkspace Diagnostics ' },
 		{ '<leader>;x', '<cmd>TroubleToggle<cr>', desc = 'Toggle Trouble' },
 		{
