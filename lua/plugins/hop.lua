7ad17af329da1e1315cfc5959d9b06bf2e2e0b0b
Juan David Merchan Torres <juan1014166@gmail.com>
2023-11-20 10:24:49 -0500

    style(hop): Change formatting

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,8 +1,17 @@
 return {
 	'smoka7/hop.nvim',
 	event = 'BufRead',
-	keys = { { 'f', '<cmd>HopWord<cr>', desc = 'Jump to word', mode = { 'n', 'v' } } },
+	keys = {
+		{
+			'f',
+			'<cmd>HopWord<cr>',
+			desc = 'Jump to word',
+			mode = { 'n', 'v' },
+		},
+	},
 	config = function()
-		require('hop').setup({ keys = 'asdghklqwertyuiopzxcvbnmfj' })
+		require('hop').setup({
+			keys = 'asdghklqwertyuiopzxcvbnmfj',
+		})
 	end,
 }
