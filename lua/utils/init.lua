6cb06e1ce359a52a6db78ef7b47ea4a26b924145
Juan David Merchan Torres <juan1014166@gmail.com>
2024-01-13 15:17:07 -0500

    feat(utils): Add go_to_window utility

---
+++ lua/utils/init.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+function M.go_to_window(num)
+	local nrs = {}
+	local ids = {}
+
+	---@type table<integer>
+	local windows = vim.tbl_filter(function(id)
+		return vim.api.nvim_win_get_config(id).relative == ''
+	end, vim.api.nvim_tabpage_list_wins(0))
+
+	for _, win in ipairs(windows) do
+		local number = vim.api.nvim_win_get_number(win)
+
+		table.insert(nrs, number)
+		ids[number] = win
+	end
+
+	if ids[num] then
+		vim.api.nvim_set_current_win(ids[num])
+	else
+		vim.cmd('G commit')
+	end
+end
+
+return M
