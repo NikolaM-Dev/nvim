f1faf0ca9687b2cd56c6873e1dae833dc1260669
Juan David Merchan Torres <juan1014166@gmail.com>
2024-02-01 14:27:47 -0500

    feat(ftplugin-markdown): Add go to link keyamp

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,4 +1,11 @@
-vim.opt_local.spell = false
 vim.opt_local.conceallevel = 2
 vim.opt_local.spell = false
 vim.opt_local.wrap = true
+
+vim.keymap.set('n', 'gd', function()
+	if require('obsidian').util.cursor_on_markdown_link() then
+		return '<cmd>ObsidianFollowLink<CR>'
+	else
+		return 'gd'
+	end
+end, { noremap = false, expr = true })
