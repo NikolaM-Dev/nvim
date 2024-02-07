86217ba3264738e8201ed7004a1dd2280945f20f
Juan David Merchan Torres <juan1014166@gmail.com>
2024-02-07 16:41:19 -0500

    refactor(harpoon): Change to use a function to setup keys

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,35 +1,36 @@
-local binds = 9
-local keys = {}
-
-table.insert(keys, {
-	'<C-e>',
-	function()
-		require('harpoon.ui').toggle_quick_menu()
-	end,
-	desc = 'Show harpoon marks',
-})
-
-table.insert(keys, {
-	'<leader>a',
-	function()
-		require('harpoon.mark').add_file()
-	end,
-	desc = '[A]dd file to harpoon',
-})
-
-for i = 1, binds do
-	table.insert(keys, {
-		string.format('<leader>%s', i),
-		function()
-			require('harpoon.ui').nav_file(i)
-		end,
-		desc = 'Harpoon go to file ' .. i,
-	})
-end
-
 return {
 	'ThePrimeagen/harpoon',
-	keys = keys,
+	keys = function()
+		local keys = {}
+
+		table.insert(keys, {
+			'<C-e>',
+			function()
+				require('harpoon.ui').toggle_quick_menu()
+			end,
+			desc = 'Show harpoon marks',
+		})
+
+		table.insert(keys, {
+			'<leader>a',
+			function()
+				require('harpoon.mark').add_file()
+			end,
+			desc = '[A]dd file to harpoon',
+		})
+
+		for i = 1, 9 do
+			table.insert(keys, {
+				string.format('<leader>%s', i),
+				function()
+					require('harpoon.ui').nav_file(i)
+				end,
+				desc = 'Harpoon go to file ' .. i,
+			})
+		end
+
+		return keys
+	end,
 	config = function()
 		require('harpoon').setup({
 			menu = {
