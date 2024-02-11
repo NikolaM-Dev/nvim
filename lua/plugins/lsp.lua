d764347ff1457207fd3931313a1a6f289e1357d7
Juan David Merchan Torres <juan1014166@gmail.com>
2024-02-11 15:29:50 -0500

    feat(lsp): Change gd keymap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -82,7 +82,13 @@ return {
 
 			nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation', 'i')
 			nmap('ga', vim.lsp.buf.code_action, '[C]ode [A]ction')
-			nmap('gd', '<cmd>Telescope lsp_definitions<cr>', '[G]o to [D]efinition')
+			nmap('gd', function()
+				if require('obsidian').util.cursor_on_markdown_link() then
+					return vim.cmd('ObsidianFollowLink')
+				else
+					vim.cmd('Telescope lsp_definitions')
+				end
+			end, '[G]o to [D]efinition')
 			nmap(';gd', '<cmd>vsplit | lua vim.lsp.buf.definition()<cr>', '[G]o to [D]efinition in vsplit')
 			nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 			nmap('gI', '<cmd>Telescope lsp_implementations<cr>', '[G]oto [I]mplementation')
