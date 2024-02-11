3c43ad7a285488553cf41a1beb8d4c3ba31be52d
Juan David Merchan Torres <juan1014166@gmail.com>
2024-02-11 15:29:32 -0500

    feat(ftplugin-markdwon): Remove go to link keymap

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,11 +1,3 @@
 vim.opt_local.conceallevel = 2
 vim.opt_local.spell = false
 vim.opt_local.wrap = true
-
-vim.keymap.set('n', 'gd', function()
-	if require('obsidian').util.cursor_on_markdown_link() then
-		return '<cmd>ObsidianFollowLink<CR>'
-	else
-		return 'gd'
-	end
-end, { noremap = false, expr = true, desc = '[G]o to Link' })
