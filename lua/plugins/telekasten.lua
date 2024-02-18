667e3e0c0fe0d755dc50f82a44a65c5b48ec8726
Juan David Merchan Torres <juan1014166@gmail.com>
2024-02-18 11:17:45 -0500

    feat(telekasten): Add bases setup

---
+++ lua/plugins/telekasten.lua
@@ -0,0 +1,11 @@
+return {
+	'renerocksai/telekasten.nvim',
+	keys = {
+		{ '<leader>fo', '<cmd>Telekasten panel<cr>', desc = 'uwu' },
+	},
+	config = function()
+		require('telekasten').setup({
+			home = vim.fn.expand('~/zettelkasten'), -- Put the name of your notes directory here
+		})
+	end,
+}
