f3cb7bb04b414ba5e193bb9b591138324b1f5939
Juan David Merchan Torres <juan1014166@gmail.com>
2024-02-19 11:19:49 -0500

    feat(obsidian): Add daily_notes settings

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -11,6 +11,12 @@ return {
 					path = '~/Documents/second-brain.md',
 				},
 			},
+			daily_notes = {
+				folder = '001-calendar/dailies',
+				date_format = '%Y-%m-%d',
+				alias_format = '%A, %d %B %Y',
+				template = nil,
+			},
 			follow_url_func = function(url)
 				vim.fn.jobstart({ 'google-chrome-stable', url })
 			end,
