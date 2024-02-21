b4ccf7b2c7850d801b99d2bd8a02ce28288e37bc
Juan David Merchan Torres <juan1014166@gmail.com>
2024-02-21 11:22:39 -0500

    feat(obsidian): Add keymaps

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -2,6 +2,15 @@ return {
 	'epwalsh/obsidian.nvim',
 	version = '*',
 	ft = 'markdown',
+	keys = {
+		{ '<leader>ob', '<cmd>ObsidianBacklinks<cr>', desc = '[O]bisidan [B]acklinks' },
+		{ '<leader>oo', '<cmd>ObsidianOpen<cr>', desc = '[O]pen in [O]bsidian' },
+		-- d = { "<cmd>ObsidianToday<cr>", "Daily Note" },
+		-- p = { '<cmd>ObsidianPasteImg<cr>', 'Paste image' },
+		{ '<leader>oq', '<cmd>ObsidianQuickSwitch<cr>', '[O]sidian [Q]uick Swith' },
+		{ '<leader>os', '<cmd>ObsidianSearch<cr>', '[O]sidian [S]earch' },
+		{ '<leader>ot', '<cmd>ObsidianTags<cr>', '[O]sidian [T]ags' },
+	},
 	config = function()
 		---@diagnostic disable: missing-fields
 		require('obsidian').setup({
