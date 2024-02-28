21ffa04b42f93e6205b8b2cca0044d588c919a84
Juan David Merchan Torres <juan1014166@gmail.com>
2024-02-28 18:03:38 -0500

    feat(lualine-components): Add n_lsp_servers component

---
+++ lua/lualine/components/n_lsp_servers.lua
@@ -0,0 +1,30 @@
+local M = require('lualine.component'):extend()
+
+function M:init(options)
+	options.icon = options.icon or ' '
+	options.split = options.split or ' '
+
+	M.super.init(self, options)
+end
+
+function M:update_status()
+	local buf_client_names = {}
+	local buf_clients = vim.lsp.get_active_clients()
+	local null_ls_installed, null_ls = pcall(require, 'null-ls')
+
+	for _, client in pairs(buf_clients) do
+		if client.name == 'null-ls' then
+			if null_ls_installed then
+				for _, source in ipairs(null_ls.get_source({ filetype = vim.bo.filetype })) do
+					table.insert(buf_client_names, source.name)
+				end
+			end
+		else
+			table.insert(buf_client_names, client.name)
+		end
+	end
+
+	return table.concat(buf_client_names, self.options.split)
+end
+
+return M
