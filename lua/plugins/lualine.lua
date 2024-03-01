7ba622a61797ca5a57764c9870164ad25dfc0bbc
Juan David Merchan Torres <juan1014166@gmail.com>
2024-03-01 14:56:07 -0500

    feat(lualine): Add base options

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,5 +4,19 @@ return {
 	event = 'VeryLazy',
 	config = function()
 		require('lualine').setup({})
+		require('lualine').setup({
+			options = {
+				component_separators = { left = '', right = '' },
+				section_separators = { left = '', right = '' },
+				globalstatus = true,
+				refresh = { statusline = 284 },
+				disabled_filetypes = {
+					statusline = {
+						'',
+						'TelescopePrompt',
+					},
+				},
+			},
+		})
 	end,
 }
