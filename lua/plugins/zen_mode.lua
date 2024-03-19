598af19303584ea1877cb3b28080f0b216f81661
Juan David Merchan Torres <juan1014166@gmail.com>
2024-03-19 10:16:01 -0500

    feat(zen_mode): Add dynamic alacritty offset
    When zen mode is on

--- lua/plugins/zen_mode.lua
+++ lua/plugins/zen_mode.lua
@@ -19,6 +19,30 @@ return {
 				tmux = { enabled = true },
 				twilight = { enabled = false },
 			},
+			-- Change alacritty font y offset when zen mode is activated
+			on_open = function()
+				if not vim.fn.executable('alacritty') then
+					return
+				end
+
+				local cmd = 'alacritty msg config -w %s font.offset.y=6'
+				local win_id = vim.fn.expand('$ALACRITTY_WINDOW_ID')
+
+				vim.fn.system(cmd:format(win_id))
+				vim.cmd([[redraw]])
+			end,
+			-- Reset alacritty offset when zen mode is deactivated
+			on_close = function()
+				if not vim.fn.executable('alacritty') then
+					return
+				end
+
+				local cmd = 'alacritty msg config -w %s --reset'
+				local win_id = vim.fn.expand('$ALACRITTY_WINDOW_ID')
+
+				vim.fn.system(cmd:format(win_id))
+				vim.cmd([[redraw]])
+			end,
 		})
 	end,
 }
