85fc396a090655d4c2a4adc50a325993e055cd61
Juan David Merchan Torres <juan1014166@gmail.com>
2024-04-05 10:45:39 -0500

    feat(config-globals): Change to disable language providers
    - node
    - perl
    - python3
    - ruby

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -1,6 +1,11 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 _G.P = function(v)
 	print(vim.inspect(v))
 	return v
