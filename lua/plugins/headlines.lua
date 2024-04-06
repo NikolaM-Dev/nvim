94c2c5a5da7d30b59b148d3d1b535a39cf79fa37
Juan David Merchan Torres <juan1014166@gmail.com>
2024-04-06 11:02:20 -0500

    feat(headlines): Change to use single quotes

--- lua/plugins/headlines.lua
+++ lua/plugins/headlines.lua
@@ -3,19 +3,19 @@ return {
 	event = 'BufRead',
 	dependencies = 'nvim-treesitter/nvim-treesitter',
 	config = function()
-		vim.cmd([[highlight Headline1 guibg=#f38ba8 guifg=#09090d]])
-		vim.cmd([[highlight Headline2 guibg=#fab387 guifg=#09090d]])
-		vim.cmd([[highlight Headline3 guibg=#a6e3a1 guifg=#09090d]])
-		vim.cmd([[highlight Headline4 guibg=#94e2d5 guifg=#09090d]])
-		vim.cmd([[highlight Headline5 guibg=#b4befe guifg=#09090d]])
-		vim.cmd([[highlight Headline6 guibg=#cba6f7 guifg=#09090d]])
+		vim.cmd('highlight Headline1 guibg=#f38ba8 guifg=#09090d')
+		vim.cmd('highlight Headline2 guibg=#fab387 guifg=#09090d')
+		vim.cmd('highlight Headline3 guibg=#a6e3a1 guifg=#09090d')
+		vim.cmd('highlight Headline4 guibg=#94e2d5 guifg=#09090d')
+		vim.cmd('highlight Headline5 guibg=#b4befe guifg=#09090d')
+		vim.cmd('highlight Headline6 guibg=#cba6f7 guifg=#09090d')
 
-		vim.cmd([[highlight CodeBlock guibg=#11111b]])
+		vim.cmd('highlight CodeBlock guibg=#11111b')
 
-		vim.cmd([[highlight Dash guifg=#6c7086]]) -- dashes
-		vim.cmd([[highlight Quote guifg=#9399b2]]) -- quotes
-		vim.cmd([[highlight @markup.italic guifg=#a6e3a1]]) -- italic
-		vim.cmd([[highlight @markup.strong guifg=#eba0ac]]) -- bold
+		vim.cmd('highlight Dash guifg=#89b4fa')
+		vim.cmd('highlight @markup.italic guifg=#a6e3a1')
+		vim.cmd('highlight @markup.strong guifg=#eba0ac')
+		vim.cmd('highlight Quote guifg=#9399b2')
 
 		-- Setup headlines.nvim with the newly defined highlight groups
 		require('headlines').setup({
