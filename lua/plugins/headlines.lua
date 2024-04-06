1d2716206edd0bac6baca3ccd97dff9639d1ee76
Juan David Merchan Torres <juan1014166@gmail.com>
2024-04-06 11:54:04 -0500

    feat(headlines): Change to use nvim_set_hl & catppuccin palette

--- lua/plugins/headlines.lua
+++ lua/plugins/headlines.lua
@@ -3,31 +3,31 @@ return {
 	ft = 'markdown',
 	dependencies = { 'catppuccin/nvim', 'nvim-treesitter/nvim-treesitter' },
 	config = function()
-		vim.cmd('highlight Headline1 guibg=#f38ba8 guifg=#09090d')
-		vim.cmd('highlight Headline2 guibg=#fab387 guifg=#09090d')
-		vim.cmd('highlight Headline3 guibg=#a6e3a1 guifg=#09090d')
-		vim.cmd('highlight Headline4 guibg=#94e2d5 guifg=#09090d')
-		vim.cmd('highlight Headline5 guibg=#b4befe guifg=#09090d')
-		vim.cmd('highlight Headline6 guibg=#cba6f7 guifg=#09090d')
+		local PALETTE = require('catppuccin.palettes').get_palette('mocha')
 
-		vim.cmd('highlight CodeBlock guibg=#11111b')
+		local headline_fg = '#09090d'
+		local marker_fg = PALETTE.surface2
 
-		vim.cmd('highlight Dash guifg=#89b4fa')
-		vim.cmd('highlight @markup.italic guifg=#a6e3a1')
-		vim.cmd('highlight @markup.strong guifg=#eba0ac')
-		vim.cmd('highlight Quote guifg=#9399b2')
+		vim.api.nvim_set_hl(0, 'Headline1', { bg = PALETTE.red, fg = headline_fg })
+		vim.api.nvim_set_hl(0, 'Headline2', { bg = PALETTE.peach, fg = headline_fg })
+		vim.api.nvim_set_hl(0, 'Headline3', { bg = PALETTE.green, fg = headline_fg })
+		vim.api.nvim_set_hl(0, 'Headline4', { bg = PALETTE.teal, fg = headline_fg })
+		vim.api.nvim_set_hl(0, 'Headline5', { bg = PALETTE.lavender, fg = headline_fg })
+		vim.api.nvim_set_hl(0, 'Headline6', { bg = PALETTE.mauve, fg = headline_fg })
 
-		-- Setup headlines.nvim with the newly defined highlight groups
-		require('headlines').setup({
-			markdown = {
-				bullets = { '', '', '', '', '', '' },
-				codeblock_highlight = 'CodeBlock',
-				dash_highlight = 'Dash',
-				dash_string = '',
-				fat_headline_lower_string = '▀',
-				fat_headlines = false,
-				fat_headline_upper_string = '▀',
-				headline_highlights = { 'Headline1', 'Headline2', 'Headline3', 'Headline4', 'Headline5', 'Headline6' },
+		vim.api.nvim_set_hl(0, 'CodeBlock', { bg = PALETTE.crust })
+
+		vim.api.nvim_set_hl(0, 'Dash', { fg = PALETTE.blue })
+		vim.api.nvim_set_hl(0, '@markup.italic', { bg = PALETTE.green })
+		vim.api.nvim_set_hl(0, '@markup.strong', { bg = PALETTE.red })
+		vim.api.nvim_set_hl(0, 'Quote', { bg = PALETTE.overlay2 })
+
+		vim.api.nvim_set_hl(0, '@text.title.1.marker.markdown', { fg = marker_fg })
+		vim.api.nvim_set_hl(0, '@text.title.2.marker.markdown', { fg = marker_fg })
+		vim.api.nvim_set_hl(0, '@text.title.3.marker.markdown', { fg = marker_fg })
+		vim.api.nvim_set_hl(0, '@text.title.4.marker.markdown', { fg = marker_fg })
+		vim.api.nvim_set_hl(0, '@text.title.5.marker.markdown', { fg = marker_fg })
+		vim.api.nvim_set_hl(0, '@text.title.6.marker.markdown', { fg = marker_fg })
 				quote_highlight = 'Quote',
 				quote_string = '',
 			},
