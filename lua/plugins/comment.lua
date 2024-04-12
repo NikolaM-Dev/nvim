9610a91e51b107deab981c64021383f346e3948f
Juan David Merchan Torres <juan1014166@gmail.com>
2024-04-12 07:34:15 -0500

    feat(comment): Change to use config function

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,12 +1,14 @@
 return {
 	'echasnovski/mini.comment',
 	event = 'VeryLazy',
-	opts = {
-		options = {
-			custom_commentstring = function()
-				---@diagnostic disable-next-line: missing-parameter
-				return require('ts_context_commentstring.internal').calculate_commentstring() or vim.bo.commentstring
-			end,
-		},
-	},
+	config = function()
+		require('mini.comment').setup({
+			options = {
+				custom_commentstring = function()
+					return require('ts_context_commentstring.internal').calculate_commentstring()
+						or vim.bo.commentstring
+				end,
+			},
+		})
+	end,
 }
