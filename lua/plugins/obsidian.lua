d4ae28405a3ca11672b99b93b1aaa79b9361eb0e
Juan David Merchan Torres <juan1014166@gmail.com>
2024-06-04 23:19:38 -0500

    feat(obsidian): Remove commit setting

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -21,7 +21,6 @@ return {
 	'epwalsh/obsidian.nvim',
 	version = '*',
 	ft = 'markdown',
-	commit = '7b59d907a3ee6952c58e07139cf021a205692338', -- TODO: Remove after new release
 	cmd = { 'ObsidianBacklinks', 'ObsidianLinks', 'ObsidianQuickSwitch' },
 	enabled = true,
 	keys = {
