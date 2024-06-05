a438dbb5b842322a8678f1321059d2a6349d259c
Juan David Merchan Torres <juan1014166@gmail.com>
2024-06-05 01:25:43 -0500

    style(treesitter): Change formatting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -20,8 +20,10 @@ return {
 					if not is_markdown_ft then
 						return false
 					end
+
 					local max_filesize = 100 * 1024 -- 100 KB
 					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+
 					if ok and stats and stats.size > max_filesize then
 						return true
 					end
