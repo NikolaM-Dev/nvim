09a1d393e0cb0ab2e120ecb739daf226c80a0b5d
Juan David Merchan Torres <juan1014166@gmail.com>
2024-06-10 19:48:49 -0500

    feat(telescope-frecency): Add base setup

---
+++ lua/plugins/telescope/frecency.lua
@@ -0,0 +1,11 @@
+return {
+	'nvim-telescope/telescope-frecency.nvim',
+	enabled = false,
+
+	keys = {
+		{ '<leader>ff', '<cmd>Telescope frecency workspace=CWD<cr>', desc = '[F]ind [F]recency' },
+	},
+	config = function()
+		require('telescope').load_extension('frecency')
+	end,
+}
