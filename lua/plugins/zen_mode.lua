395824909318efa720ce436e5b23719195c842c9
Juan David Merchan Torres <juan1014166@gmail.com>
2024-06-18 07:44:37 -0500

    feat(zen_mode): Change setup

--- lua/plugins/zen_mode.lua
+++ lua/plugins/zen_mode.lua
@@ -8,42 +8,25 @@ return {
 	config = function()
 		require('zen-mode').setup({
 			window = {
-				backdrop = 0.95,
 				height = 1,
-				options = { colorcolumn = '', signcolumn = 'no' },
-				width = 80,
+				width = 110,
+				options = {
+					cursorcolumn = false,
+					cursorline = true,
+					foldcolumn = '0',
+					list = true,
+					number = true,
+					relativenumber = true,
+					signcolumn = 'yes',
+					colorcolumn = '80',
+				},
 			},
 			plugins = {
-				alacritty = { enabled = true, font = '13' },
 				gitsigns = { enabled = false },
-				options = { enabled = true, laststatus = 0, ruler = false, showcmd = false },
-				tmux = { enabled = true },
+				options = { enabled = false, laststatus = 3, ruler = true, showcmd = true },
+				tmux = { enabled = false },
 				twilight = { enabled = false },
 			},
-			-- Change alacritty font y offset when zen mode is activated
-			on_open = function()
-				if not vim.fn.executable('alacritty') then
-					return
-				end
-
-				local cmd = 'alacritty msg config -w %s font.offset.y=6'
-				local win_id = vim.fn.expand('$ALACRITTY_WINDOW_ID')
-
-				vim.fn.system(cmd:format(win_id))
-				vim.cmd([[redraw]])
-			end,
-			-- Reset alacritty offset when zen mode is deactivated
-			on_close = function()
-				if not vim.fn.executable('alacritty') then
-					return
-				end
-
-				local cmd = 'alacritty msg config -w %s --reset'
-				local win_id = vim.fn.expand('$ALACRITTY_WINDOW_ID')
-
-				vim.fn.system(cmd:format(win_id))
-				vim.cmd([[redraw]])
-			end,
 		})
 	end,
 }
