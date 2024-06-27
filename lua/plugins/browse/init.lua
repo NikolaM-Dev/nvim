7449cb017404d5aa7b05f3b91e45deb4fb02ed80
Juan David Merchan Torres <juan1014166@gmail.com>
2024-06-27 08:49:48 -0500

    refactor(browse): Change architecture

--- lua/plugins/browse/init.lua
+++ lua/plugins/browse/init.lua
@@ -1,16 +1,22 @@
+---@type table
 local bookmarks = {
 	nvim_configs = {
 		adalessa = 'https://github.com/adalessa/nvim',
 		adibhanna = 'https://github.com/adibhanna/nvim',
+		b0o = 'https://github.com/b0o/nvim-conf',
 		cris_at_machine = 'https://github.com/ChristianChiarulli/nvim',
+		dcrag = 'https://github.com/CRAG666/dotfiles/tree/main/config/nvim',
+		epwalsh = 'https://github.com/epwalsh/dotfiles/tree/main/neovim',
 		fbearoff = 'https://github.com/fbearoff/dotfiles/tree/master/nvim/.config/nvim',
 		folke = 'https://github.com/folke/dot/tree/master/nvim',
 		glepnir = 'https://github.com/glepnir/nvim',
 		kickstart = 'https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua',
 		lazy_vim = 'https://github.com/LazyVim/LazyVim',
+		mrs4ndman = 'https://github.com/mrs4ndman/nvim-conf',
 		prime = 'https://github.com/ThePrimeagen/neovimrc',
 		wuelner = 'https://github.com/wuelnerdotexe/nvim',
 	},
+
 	parq = {
 		operator = 'https://main.d2aol8f6bio8ep.amplifyapp.com',
 		passes = 'https://main.d2xuop51ekcwbf.amplifyapp.com',
@@ -18,6 +24,7 @@ local bookmarks = {
 		payvalida_doc = 'https://docs.payvalida.com/documentacion',
 		payvalida_sandbox = 'https://sandbox-merchant.payvalida.com/signin',
 	},
+
 	awesome = 'https://github.com/rockerBOO/awesome-neovim',
 	catppuccin = 'https://github.com/catppuccin/catppuccin',
 	dots = 'https://github.com/NikolaM-Dev/dotfiles',
@@ -33,8 +40,11 @@ local bookmarks = {
 	new_repository = 'https://github.com/new',
 	nikola = 'https://github.com/NikolaM-Dev',
 	none_ls = 'https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md',
+	obsidian = 'https://github.com/epwalsh/obsidian.nvim',
+	random_wheel = 'https://es.piliapp.com/random/wheel/',
 	reddit = 'https://www.reddit.com/r/neovim',
 	refold = 'https://academy.refold.la/library',
+	sesh = 'https://github.com/joshmedeski/sesh',
 	status_codes = 'https://developer.mozilla.org/es/docs/Web/HTTP/Status#errores_de_cliente',
 	telescope = 'https://github.com/nvim-telescope/telescope.nvim/blob/master/doc/telescope.txt',
 	tokyonight = 'https://github.com/folke/tokyonight.nvim/blob/main/lua/tokyonight/colors.lua',
@@ -43,9 +53,11 @@ local bookmarks = {
 	youtube = 'https://www.youtube.com',
 }
 
+---@type LazySpec
 return {
 	'lalitmee/browse.nvim',
-	enabled = false,
+	enabled = true,
+
 	keys = {
 		{
 			'<leader>bb',
