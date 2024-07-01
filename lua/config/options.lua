2ac4635ef147d705e0711ba7b8a36e62a1aeb6f8
Juan David Merchan Torres <juan1014166@gmail.com>
2024-07-01 07:35:42 -0500

    feat(options): Remove for loop

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,74 +1,80 @@
-local options = {
-	listchars = {
-		-- eol = '¬',
-		extends = '»',
-		lead = ' ',
-		nbsp = '␣',
-		precedes = '«',
-		space = ' ',
-		tab = '  ', -- │
-		trail = '·',
-	},
-	fillchars = {
-		diff = '╱',
-		eob = ' ',
-		fold = ' ',
-		foldclose = '',
-		foldopen = '',
-		foldsep = ' ',
-	},
-	backup = false, -- create backup files
-	clipboard = 'unnamedplus', -- use system clipboard
-	colorcolumn = '80', -- maximum column length for color highlighting
-	completeopt = 'menu,menuone,noinsert', -- cmp complete options
-	cursorline = true, -- highlight current line
-	expandtab = true, -- convert tabs to spaces
-	fileencoding = 'utf-8', -- file encoding for the current buffer
-	foldcolumn = '0', -- dont show fold column
-	foldenable = true, -- enable folds
-	foldlevel = 99, -- using ufo provider need a large value, feel free to decrease the value
-	foldlevelstart = 99, -- does not start with all folds closed
-	foldmethod = 'expr', -- the kind of folding used for the current window
-	fsync = false, -- This flushes the file to disk, ensuring that it is safely written
-	hlsearch = true, -- highlight all matches on previous search pattern
-	ignorecase = true, -- ignore case in search patterns
-	inccommand = 'split', -- Incrementally show effects of commands, opens split
-	incsearch = true, -- while typing a search command, show where the pattern,
-	joinspaces = false, -- No double spaces with join after a dot
-	laststatus = 3, -- always and ONLY the last window a status line
-	linebreak = true, -- wrap long lines at characters, not in the middle of a word
-	list = true, -- useful to see the difference between tabs and spaces and for trailing blanks
-	mouse = 'a', -- mouse support
-	number = true, -- show line numbers
-	relativenumber = true, -- show relative numbers
-	scrolloff = 8, -- leave 8 lines above and below cursor
-	sessionoptions = { 'buffers', 'curdir', 'folds', 'globals', 'help', 'skiprtp', 'tabpages', 'winsize' },
-	shiftwidth = 4, -- the number of spaces inserted for each indentation
-	showbreak = '', -- string to put at the start of lines that have been wrapped
-	showmode = false, -- removes the text indicating the mode
-	showtabline = 0, -- remove tab page label
-	signcolumn = 'yes', -- always show the sign column, otherwise it would shift the text each time
-	smartcase = true, -- difference between lower and upper case letters
-	smartindent = true, -- make indenting smarter again
-	softtabstop = 4, -- number of spaces that a tab
-	spelllang = 'en_us',
-	splitbelow = true, -- split window below
-	splitkeep = 'screen', -- stabilize screen position on split
-	splitright = true, -- split window right
-	swapfile = false, -- creates a swapfile
-	tabstop = 4, -- insert <x> spaces for a tab
-	termguicolors = true, -- set term gui colors (most terminals support this)
-	timeoutlen = 300, -- time to wait for a mapped sequence to complete (in milliseconds)
-	undodir = os.getenv('HOME') .. '/.cache/nvim/undodir', -- undodir path
-	undofile = true, -- make undo file
-	updatetime = 40, -- improve user experience updating more frequently
-	whichwrap = 'b,s,<,>,[,]',
-	wildmode = 'longest:full,full', -- Command-line completion mode
-	winbar = '%=%m %f', -- the window bar is a bar that's shown at the top of every window with it enabled
-	wrap = false, -- wrap lines
-	writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.opt.listchars = {
+	-- eol = '¬',
+	extends = '»',
+	lead = ' ',
+	nbsp = '␣',
+	precedes = '«',
+	space = ' ',
+	tab = '  ', -- │
+	trail = '·',
 }
 
-for o, v in pairs(options) do
-	vim.opt[o] = v
-end
+vim.opt.fillchars = {
+	diff = '╱',
+	eob = ' ',
+	fold = ' ',
+	foldclose = '',
+	foldopen = '',
+	foldsep = ' ',
+}
+
+vim.opt.sessionoptions = {
+	'buffers',
+	'curdir',
+	'folds',
+	'globals',
+	'help',
+	'skiprtp',
+	'tabpages',
+	'winsize',
+}
+
+vim.opt.backup = false -- create backup files
+vim.opt.clipboard = 'unnamedplus' -- use system clipboard
+vim.opt.colorcolumn = '80' -- maximum column length for color highlighting
+vim.opt.completeopt = 'menu,menuone,noinsert' -- cmp complete options
+vim.opt.cursorline = true -- highlight current line
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.fileencoding = 'utf8' -- file encoding for the current buffer
+vim.opt.foldcolumn = '0' -- dont show fold column
+vim.opt.foldenable = true -- enable folds
+vim.opt.foldlevel = 99 -- using ufo provider need a large value, feel free to decrease the value
+vim.opt.foldlevelstart = 99 -- does not start with all folds closed
+vim.opt.foldmethod = 'expr' -- the kind of folding used for the current window
+vim.opt.fsync = false -- This flushes the file to disk, ensuring that it is safely written
+vim.opt.hlsearch = true -- highlight all matches on previous search pattern
+vim.opt.ignorecase = true -- ignore case in search patterns
+vim.opt.inccommand = 'split' -- Incrementally show effects of commands, opens split
+vim.opt.incsearch = true -- while typing a search command, show where the pattern,
+vim.opt.joinspaces = false -- No double spaces with join after a dot
+vim.opt.laststatus = 3 -- always and ONLY the last window a status line
+vim.opt.linebreak = true -- wrap long lines at characters, not in the middle of a word
+vim.opt.list = true -- useful to see the difference between tabs and spaces and for trailing blanks
+vim.opt.mouse = 'a' -- mouse support
+vim.opt.number = true -- show line numbers
+vim.opt.relativenumber = true -- show relative numbers
+vim.opt.scrolloff = 8 -- leave 8 lines above and below cursor
+vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
+vim.opt.showbreak = '' -- strin to put at the start of lines that have been wrapped
+vim.opt.showmode = false -- removes the text indicating the mode
+vim.opt.showtabline = 0 -- remove tab page label
+vim.opt.signcolumn = 'yes' -- always show the sign column, otherwise it would shift the text each time
+vim.opt.smartcase = true -- difference between lower and upper case letters
+vim.opt.smartindent = true -- make indenting smarter again
+vim.opt.softtabstop = 4 -- number of spaces that a tab
+vim.opt.spelllang = 'en_us'
+vim.opt.splitbelow = true -- split window below
+vim.opt.splitkeep = 'screen' -- stabilize screen position on split
+vim.opt.splitright = true -- split window right
+vim.opt.swapfile = false -- creates a swapfile
+vim.opt.tabstop = 4 -- insert <x> spaces for a tab
+vim.opt.termguicolors = true -- set term gui colors (most terminals support this)
+vim.opt.timeoutlen = 300 -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.undodir = os.getenv('HOME') .. '/.cache/nvim/undodir' -- undodir path
+vim.opt.undofile = true -- make undo file
+vim.opt.updatetime = 40 -- improve user experience updating more frequently
+vim.opt.whichwrap = 'b,s,<,>,[,]'
+vim.opt.wildmode = 'longest:full,full' -- Command-line completion mode
+vim.opt.winbar = '%=%m %f' -- the window bar is a bar that's shown at the top of every window with it enabled
+vim.opt.wrap = false -- wrap lines
+vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
