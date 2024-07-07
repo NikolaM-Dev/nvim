92cfd044d6829190d19b6b2d2f3664c90844996d
Juan David Merchan Torres <juan1014166@gmail.com>
2024-07-07 09:15:50 -0500

    feat(bullets): Change lazy strategy

--- lua/plugins/bullets.lua
+++ lua/plugins/bullets.lua
@@ -3,7 +3,7 @@ return {
 	'bullets-vim/bullets.vim',
 	enabled = true,
 
-	event = 'VeryLazy',
+	ft = { 'gitcommit', 'markdown', 'text' },
 	init = function()
 		vim.g.bullets_outline_levels = {
 			'ROM',
@@ -14,6 +14,25 @@ return {
 			'std-',
 			-- 'std*',
 			-- 'std+'
+		}
+
+		vim.g.bullets_custom_mappings = {
+			{ 'imap', '<cr>', '<Plug>(bullets-newline)' },
+			{ 'inoremap', '<C-cr>', '<cr>' },
+
+			{ 'nmap', 'o', '<Plug>(bullets-newline)' },
+
+			{ 'vmap', 'gN', '<Plug>(bullets-renumber)' },
+			{ 'nmap', 'gN', '<Plug>(bullets-renumber)' },
+
+			{ 'nmap', '<leader>ch', '<Plug>(bullets-toggle-checkbox)' },
+
+			{ 'imap', '<C-t>', '<Plug>(bullets-demote)' },
+			{ 'nmap', '>>', '<Plug>(bullets-demote)' },
+			{ 'vmap', '>', '<Plug>(bullets-demote)' },
+			{ 'imap', '<C-d>', '<Plug>(bullets-promote)' },
+			{ 'nmap', '<<', '<Plug>(bullets-promote)' },
+			{ 'vmap', '<', '<Plug>(bullets-promote)' },
 		}
 	end,
 }
