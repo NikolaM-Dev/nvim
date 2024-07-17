6fa189ef9e557e7279a2ae2cb5a38c3be6f966db
Juan David Merchan Torres <juan1014166@gmail.com>
2024-07-17 07:23:07 -0500

    feat(supermaven): Change to use a specific commit

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -3,6 +3,8 @@ return {
 	'supermaven-inc/supermaven-nvim',
 	enabled = true,
 
+	-- TODO: Remove this when https://github.com/supermaven-inc/supermaven-nvim/issues/74 is fixed
+	commit = 'df3ecf7',
 	event = 'InsertEnter',
 	keys = {
 		{ '<leader>ai', '<cmd>SupermavenToggle<cr>', desc = 'Toggle Supermaven [A][I]' },
