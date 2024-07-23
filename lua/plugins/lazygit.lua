181a81ffe9b4903b0e3c97b6ef0dcfa802a4aca8
Juan David Merchan Torres <juan1014166@gmail.com>
2024-07-22 20:28:05 -0500

    feat(lazygit): Add lazygit keymap

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -3,4 +3,7 @@ return {
 	'kdheepak/lazygit.nvim',
 	enabled = true,
 	dependencies = 'nvim-lua/plenary.nvim',
+	keys = {
+		{ '<leader>lg', '<cmd>LazyGit<cr>', desc = 'Open [L]azy [G]it' },
+	},
 }
