d164ebf41dbce99751e38b4c6df04ef82580ac4f
Juan David Merchan Torres <juan1014166@gmail.com>
2024-07-22 20:28:26 -0500

    feat(lazygit): Add cmd's to lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -2,6 +2,8 @@
 return {
 	'kdheepak/lazygit.nvim',
 	enabled = true,
+
+	cmd = { 'LazyGit', 'LazyGitConfig', 'LazyGitCurrentFile', 'LazyGitFilter', 'LazyGitFilterCurrentFile' },
 	dependencies = 'nvim-lua/plenary.nvim',
 	keys = {
 		{ '<leader>lg', '<cmd>LazyGit<cr>', desc = 'Open [L]azy [G]it' },
