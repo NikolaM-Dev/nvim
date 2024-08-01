c1fd1fcdf0930a270f00da6ad8f3ec7d437e26ad
Juan David Merchan Torres <juan1014166@gmail.com>
2024-07-31 20:18:28 -0500

    feat(grug_far): Add cmd

--- lua/plugins/grup_far.lua
+++ lua/plugins/grup_far.lua
@@ -2,10 +2,7 @@
 return {
 	'MagicDuck/grug-far.nvim',
 	enabled = true,
-	config = function()
-		---@type GrugFarOptionsOverride
-		local setup_opts = {}
 
-		require('grug-far').setup(setup_opts)
+	cmd = 'GrugFar',
 	end,
 }
