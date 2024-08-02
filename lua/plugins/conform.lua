e62729aca3b12638c990f23551751cbbae3c35c9
Juan David Merchan Torres <juan1014166@gmail.com>
2024-08-02 08:22:51 -0500

    refactor(conform): Change to use opts

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -7,5 +7,8 @@ return {
 	event = 'BufWritePre',
 	config = function()
 		require('conform').setup()
+	---@param opts conform.setupOpts
+	config = function(_, opts)
+		require('conform').setup(opts)
 	end,
 }
