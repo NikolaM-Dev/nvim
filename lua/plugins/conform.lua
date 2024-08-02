b494a3359eabc867b83a3053d90a78dab35c8688
Juan David Merchan Torres <juan1014166@gmail.com>
2024-08-02 08:23:17 -0500

    feat(conform): Add base formatters_by_ft

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,8 +5,41 @@ return {
 
 	cmd = { 'ConformInfo' },
 	event = 'BufWritePre',
-	config = function()
-		require('conform').setup()
+
+	---@return conform.setupOpts
+	opts = function()
+		local prettier = { 'prettierd', 'prettier', stop_after_first = true }
+
+		return {
+			log_level = vim.log.levels.TRACE,
+			notify_on_error = true,
+			formatters_by_ft = {
+				css = prettier,
+				go = { 'gofumpt', 'goimports' },
+				graphql = prettier,
+				html = prettier,
+				javascript = prettier,
+				javascriptreact = prettier,
+				json5 = prettier,
+				jsonc = prettier,
+				json = prettier,
+				lua = { 'stylua' },
+				markdown = prettier,
+				svelte = prettier,
+				typescript = prettier,
+				typescriptreact = prettier,
+				yaml = prettier,
+			},
+
+			format_on_save = function()
+				return {
+					timeout_ms = 5000,
+					lsp_format = 'fallback',
+				}
+			end,
+		}
+	end,
+
 	---@param opts conform.setupOpts
 	config = function(_, opts)
 		require('conform').setup(opts)
