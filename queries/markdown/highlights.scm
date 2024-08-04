16e37fe0eb1010134bb425df3fdf9f6ef3c6bd1f
Juan David Merchan Torres <juan1014166@gmail.com>
2024-08-04 08:19:58 -0500

    feat(queries-markdown): Change to use markdown.nvim

--- queries/markdown/highlights.scm
+++ queries/markdown/highlights.scm
@@ -1,29 +1,30 @@
 ;; extends
 
-;; completed check-box
-(list
-  (list_item
-    (task_list_marker_checked) @text.todo.checked.markdown (#set! conceal "")))
-
-;; uncompleted check-box
-(list
-  (list_item
-    (task_list_marker_unchecked) @text.todo.unchecked.markdown (#set! conceal "")))
-
 ;; comment highlight for completed check-box
 (list_item (task_list_marker_checked)) @comment
-;; list markers/bullet points
-(
-  ([
-    (list_marker_star)
-    (list_marker_plus)
-    (list_marker_minus)
-  ]) @markdown_list_marker
-  (#offset! @markdown_list_marker 0 0 0 -1)
-  (#set! conceal "•")
-)
 
-;; NOTE: Commented because I am currently using headlines.nvim
+;; NOTE: Commented because I am currently using markdwon.nvim
+
+;; completed check-box
+; (list
+;   (list_item
+;     (task_list_marker_checked) @text.todo.checked.markdown (#set! conceal "")))
+
+;; uncompleted check-box
+; (list
+;   (list_item
+;     (task_list_marker_unchecked) @text.todo.unchecked.markdown (#set! conceal "")))
+
+;; list markers/bullet points
+; (
+;   ([
+;     (list_marker_star)
+;     (list_marker_plus)
+;     (list_marker_minus)
+;   ]) @markdown_list_marker
+;   ; (#offset! @markdown_list_marker 0 0 0 -1)
+;   (#set! conceal "•")
+; )
 
 ;; block quotes
 ; ((block_quote_marker) @markdown_quote_marker (#set! conceal "▍"))
