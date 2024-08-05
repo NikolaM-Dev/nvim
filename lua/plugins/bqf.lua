c1495f596300b49a61d76fdfa51f58ee0b66b374
Juan David Merchan Torres <juan1014166@gmail.com>
2024-08-05 18:48:45 -0500

    style(bqf): Change formatting

--- lua/plugins/bqf.lua
+++ lua/plugins/bqf.lua
@@ -1,6 +1,8 @@
 return {
 	'kevinhwang91/nvim-bqf',
+
 	event = 'VeryLazy',
+
 	dependencies = {
 		{
 			'junegunn/fzf',
@@ -9,12 +11,13 @@ return {
 			end,
 		},
 	},
+
 	keys = {
 		{ '<leader>;q', '<cmd>copen<cr>', desc = 'Open [Q]uickfix' },
 		{ '<leader>qj', '<C-w>jj', desc = 'Next Item in [Q]uickfix' },
 	},
+
 	config = function()
-		---@diagnostic disable: missing-fields
 		require('bqf').setup({
 			func_map = {
 				filter = ';c', -- choose
