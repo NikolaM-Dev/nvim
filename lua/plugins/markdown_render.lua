f155a9074746ceb365cef8a1023a2b0b06a4c5cd
Juan David Merchan Torres <juan1014166@gmail.com>
2024-08-14 19:57:38 -0500

    feat(markdown_render): Change highlights

--- lua/plugins/markdown_render.lua
+++ lua/plugins/markdown_render.lua
@@ -13,6 +13,12 @@ return {
 
 	---@param opts render.md.UserConfig
 	config = function(_, opts)
+		local PALETTE = require('catppuccin.palettes').get_palette('mocha')
+
+		vim.api.nvim_set_hl(0, 'RenderMarkdownCode', { bg = PALETTE.crust })
+		vim.api.nvim_set_hl(0, 'RenderMarkdownCodeInline', { fg = PALETTE.sky })
+		vim.api.nvim_set_hl(0, 'RenderMarkdownDash', { fg = PALETTE.blue })
+
 		require('render-markdown').setup(opts)
 	end,
 }
