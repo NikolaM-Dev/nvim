97bd1ab7123302a5a97ff1fd8cc2e487d39c42fc
Juan David Merchan Torres <juan1014166@gmail.com>
2024-08-14 19:54:58 -0500

    feat(markdown_render): Add base setup

---
+++ lua/plugins/markdown_render.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+return {
+	'MeanderingProgrammer/markdown.nvim',
+	enabled = true,
+
+	cmd = 'RenderMarkdown',
+	main = 'render-markdown',
+	name = 'render-markdown',
+	ft = { 'markdown' },
+}
