6f9e7cb9af14b556fc87eb3aef344b04f84ab6d8
Juan David Merchan Torres <juan1014166@gmail.com>
2024-08-29 21:41:03 -0500

    feat(stay_centered): Add notifications

--- lua/plugins/stay_centered.lua
+++ lua/plugins/stay_centered.lua
@@ -1,3 +1,5 @@
+local logger = require('lib.logger'):new('Stay Centered')
+
 ---Is stay centered enabled
 ---@type boolean
 local is_enable = false
@@ -14,8 +16,10 @@ return {
 			function()
 				if is_enable then
 					require('stay-centered').disable()
+					logger:warn('Stay Centered disabled')
 				else
 					require('stay-centered').enable()
+					logger:info('Stay Centered enabled')
 				end
 
 				is_enable = not is_enable
