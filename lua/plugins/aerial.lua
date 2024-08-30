0f593d0d88d8a33dc24c7e3c988cbbe4247c754a
Juan David Merchan Torres <juan1014166@gmail.com>
2024-08-30 13:40:57 -0500

    docs(aerial): Add type annotations

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -2,8 +2,10 @@
 return {
 	'stevearc/aerial.nvim',
 	enabled = true,
+	---@type table
 	opts = {},
 
+	---@param opts? table
 	config = function(_, opts)
 		require('aerial').setup(opts)
 		require('telescope').load_extension('aerial')
