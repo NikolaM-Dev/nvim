32ca00ebd68428200d7ac70a25a9c545ff345702
Juan David Merchan Torres <juan1014166@gmail.com>
2024-08-30 13:40:45 -0500

    feat(aerial): Add to telescope

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -2,4 +2,10 @@
 return {
 	'stevearc/aerial.nvim',
 	enabled = true,
+	opts = {},
+
+	config = function(_, opts)
+		require('aerial').setup(opts)
+		require('telescope').load_extension('aerial')
+	end,
 }
