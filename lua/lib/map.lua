bd3604743d7854d3cf6f5f62ff658159f99e1368
Juan David Merchan Torres <juan1014166@gmail.com>
2024-09-15 07:13:11 -0500

    feat(lib-map): Add cmd function

--- lua/lib/map.lua
+++ lua/lib/map.lua
@@ -10,4 +10,24 @@ function Map:new()
 end
 
 local new_map = Map:new()
+---@class MapCmdOpts
+---@field delay_cmd? boolean defaults to false
+
+---Constructs a command string based on the provided `cmd` and options.
+---@param cmd The command to be executed.
+---@param opts? MapCmdOpts table containing options for the command.
+--- The `delay_cmd` option determines whether the command is delayed (default is `false`).
+--- If `delay_cmd` is `true`, returns the command prefixed with a colon `:`.
+--- Otherwise, returns the command wrapped with `<cmd>` and `<cr>`.
+---@return string constructed command string based on the options provided.
+function Map:cmd(cmd, opts)
+	opts = opts or { delay_cmd = false }
+
+	if opts.delay_cmd then
+		return ':' .. cmd
+	end
+
+	return '<cmd>' .. cmd .. '<cr>'
+end
+
 return new_map
