370f14fad1e9ff77a9d2793df0427893740c828f
Juan David Merchan Torres <juan1014166@gmail.com>
2024-09-19 07:23:06 -0500

    refactor(keymaps): Change to use lib.map

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,49 +1,38 @@
----Adds a new |mapping|. By default with silent mode active
----@param mode string|string[] Mode short-name, see |nvim_set_keymap()|.
----							   Can also be list of modes to create mapping on multiple modes.
----@param lhs string           Left-hand side |{lhs}| of the mapping.
----@param rhs string|function  Right-hand side |{rhs}| of the mapping, can be a Lua function.
----@param opts? vim.keymap.set.Opts
-local function map(mode, lhs, rhs, opts)
-	opts = opts or {}
-	opts.silent = opts.silent ~= false
-
-	return vim.keymap.set(mode, lhs, rhs, opts)
-end
+local map = require('lib.map')
 
 -- Better up/down
-map({ 'n', 'x' }, 'j', 'v:count == 0 ? \'gj\' : \'j\'', { expr = true, silent = true })
-map({ 'n', 'x' }, 'k', 'v:count == 0 ? \'gk\' : \'k\'', { expr = true, silent = true })
+map:set({ 'n', 'x' }, 'j', 'v:count == 0 ? \'gj\' : \'j\'', { expr = true, silent = true })
+map:set({ 'n', 'x' }, 'k', 'v:count == 0 ? \'gk\' : \'k\'', { expr = true, silent = true })
 
 -- Move Lines
-map('i', '<A-J>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move down' })
-map('i', '<A-K>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move up' })
-map('n', '<A-J>', '<cmd>m .+1<cr>==', { desc = 'Move down' })
-map('n', '<A-K>', '<cmd>m .-2<cr>==', { desc = 'Move up' })
-map('v', '<A-J>', ':m \'>+1<cr>gv=gv', { desc = 'Move down' })
-map('v', '<A-K>', ':m \'<-2<cr>gv=gv', { desc = 'Move up' })
+map:set('i', '<A-J>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move down' })
+map:set('i', '<A-K>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move up' })
+map:set('n', '<A-J>', '<cmd>m .+1<cr>==', { desc = 'Move down' })
+map:set('n', '<A-K>', '<cmd>m .-2<cr>==', { desc = 'Move up' })
+map:set('v', '<A-J>', ':m \'>+1<cr>gv=gv', { desc = 'Move down' })
+map:set('v', '<A-K>', ':m \'<-2<cr>gv=gv', { desc = 'Move up' })
 
 -- Insert line below
-map('i', '<C-o>', '<esc>o')
+map:set('i', '<C-o>', '<esc>o')
 
 -- Clear search with <esc>
-map({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and clear hlsearch' })
+map:set({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and clear hlsearch' })
 
 -- Better indenting
-map('v', '<', '<gv', { desc = 'Indent Left' })
-map('v', '>', '>gv', { desc = 'Indent right' })
+map:set('v', '<', '<gv', { desc = 'Indent Left' })
+map:set('v', '>', '>gv', { desc = 'Indent right' })
 
 -- Sort
-map('v', '<leader>s', ':!sort<cr>', { desc = 'Sort' })
+map:set('v', '<leader>s', ':!sort<cr>', { desc = 'Sort' })
 
 -- Save
-map({ 'i', 'n', 's', 'v' }, '<C-s>', '<esc><cmd>w<cr>', { desc = '[S]ave file' })
+map:set({ 'i', 'n', 's', 'v' }, '<C-s>', '<esc><cmd>w<cr>', { desc = '[S]ave file' })
 
 -- Save all
-map('n', '<leader>w', '<cmd>wall<cr>', { desc = 'Write all' })
+map:set('n', '<leader>w', map:cmd('wall'), { desc = 'Write all' })
 
 -- Quit
-map('n', '<C-q>', function()
+map:set('n', '<C-q>', function()
 	if vim.bo.filetype == 'gitcommit' then
 		return vim.cmd.quit()
 	end
@@ -76,90 +65,91 @@ map('n', '<C-q>', function()
 end, { desc = 'Quit' })
 
 -- Quit & save
-map('i', '<C-q>', '<esc><cmd>wq<cr>', { desc = 'Quit' })
+map:set('i', '<C-q>', '<esc><cmd>wq<cr>', { desc = 'Quit' })
 
 -- Quit all
-map('n', '<leader>q', '<cmd>qall<cr>', { desc = '[Q]uit all' })
-map('n', '<leader><leader>q', '<cmd>qall!<cr>', { desc = '[Q]uit all forced' })
+map:set('n', '<leader>q', map:cmd('qall'), { desc = '[Q]uit all' })
+map:set('n', '<leader><leader>q', map:cmd('qall!'), { desc = '[Q]uit all forced' })
 
 -- Quit without save
-map('n', '<leader>z', '<cmd>q!<cr>', { desc = 'Quit without save' })
+map:set('n', '<leader>z', map:cmd('q!'), { desc = 'Quit without save' })
 
 -- Centralized scroll
-map('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up centralized' })
-map('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down centralized' })
+map:set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up centralized' })
+map:set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down centralized' })
 
 -- Centralized search
-map('n', 'N', 'Nzzzv', { desc = 'Previous centralized' })
-map('n', 'n', 'nzzzv', { desc = 'Next search centralized' })
+map:set('n', 'N', 'Nzzzv', { desc = 'Previous centralized' })
+map:set('n', 'n', 'nzzzv', { desc = 'Next search centralized' })
 
 -- Centralized history
-map('n', '<C-o>', '<C-o>zz', { desc = 'Previous History centralized' })
-map('n', '<C-i>', '<C-i>zz', { desc = 'Next History centralized' })
+map:set('n', '<C-o>', '<C-o>zz', { desc = 'Previous History centralized' })
+map:set('n', '<C-i>', '<C-i>zz', { desc = 'Next History centralized' })
 
 -- Lazy
-map('n', '<leader>lp', '<cmd>Lazy profile<cr>', { desc = 'Lazy profile' })
-map('n', '<leader>lu', '<cmd>Lazy update<cr>', { desc = 'Lazy update' })
+map:set('n', '<leader>lp', map:cmd('Lazy profile'), { desc = 'Lazy profile' })
+map:set('n', '<leader>lu', map:cmd('Lazy update'), { desc = 'Lazy update' })
 
 -- H and L go to begining/end of line
-map('n', 'H', '^', { desc = 'Go to Beginning of Line' })
-map('n', 'L', '$', { desc = 'Go to End of Line' })
+map:set('n', 'H', '^', { desc = 'Go to Beginning of Line' })
+map:set('n', 'L', '$', { desc = 'Go to End of Line' })
 
 -- Add undo break-points
-map('i', '!', '!<c-g>u')
-map('i', ',', ',<c-g>u')
-map('i', '-', '-<c-g>u')
-map('i', '.', '.<c-g>u')
-map('i', ';', ';<c-g>u')
-map('i', '?', '?<c-g>u')
-map('i', '_', '_<c-g>u')
+map:set('i', '!', '!<c-g>u')
+map:set('i', ',', ',<c-g>u')
+map:set('i', '-', '-<c-g>u')
+map:set('i', '.', '.<c-g>u')
+map:set('i', ';', ';<c-g>u')
+map:set('i', '?', '?<c-g>u')
+map:set('i', '_', '_<c-g>u')
 
 -- Paste & replace without copy
-map('n', '<leader>Y', [["+Y]])
-map({ 'n', 'v' }, '<leader>d', [["_d]])
-map({ 'n', 'v' }, '<leader>y', [["+y]])
-map('x', '<leader>p', [["_dP]])
+map:set('n', '<leader>Y', [["+Y]])
+map:set({ 'n', 'v' }, '<leader>d', [["_d]])
+map:set({ 'n', 'v' }, '<leader>y', [["+y]])
+map:set('x', '<leader>p', [["_dP]])
 
 -- Change to right vertial split
-map('n', '<leader><leader>l', function()
+map:set('n', '<leader><leader>l', function()
 	vim.cmd.wincmd('L')
 end, { desc = 'Change to vertial split' })
 
 -- Rename all occurrences in the file
-map(
+map:set(
 	'n',
 	'<leader><leader>s',
-	':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>',
+
+	map:cmd('%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', { delay_cmd = true }),
 	{ desc = 'Rename all occurrences in the file', silent = false }
 )
-map(
+map:set(
 	'v',
 	'<leader><leader>s',
-	':s///gI<Left><Left><Left><Left>',
+	map:cmd('s///gI<Left><Left><Left><Left>', { delay_cmd = true }),
 	{ desc = 'Rename all occurrences in the file (v)', silent = false }
 )
 
-map('n', '<leader><leader>;', function()
+map:set('n', '<leader><leader>;', function()
 	require('utils').go_to_window(4)
 end, { desc = 'Go to window 4' })
 
-map('n', '<leader><leader>', '<cmd>luafile %<cr>', { desc = 'Reload Lua File' })
+map:set('n', '<leader><leader>', map:cmd('luafile %'), { desc = 'Reload Lua File' })
 
-map(
+map:set(
 	'n',
 	'<leader>rf',
-	'<cmd>e<cr>',
+	map:cmd('e'),
 	{ desc = '[R]eloads the [F]ile from disk, but will fail if there are unsaved changes' }
 )
 
-map(
+map:set(
 	'n',
 	'<leader>;rf',
-	'<cmd>e!<cr>',
+	map:cmd('e!'),
 	{ desc = '[R]eloads the [F]ile from disk, but forces the reload, discarding any unsaved changes in the buffer' }
 )
 
-map('n', '<leader>ip', function()
+map:set('n', '<leader>ip', function()
 	local line_number = vim.api.nvim_win_get_cursor(0)[1]
 	local line_content = vim.api.nvim_buf_get_lines(0, line_number - 1, line_number, false)[1]
 
@@ -177,4 +167,4 @@ map('n', '<leader>ip', function()
 	os.execute(command)
 end, { desc = '[I]mage [P]review' })
 
-map('n', '<leader>yp', '<cmd>let @+=@%<cr>', { desc = '[Y]ank [P]ath from current file' })
+map:set('n', '<leader>yp', map:cmd('let @+=expand("%:p:~")'), { desc = '[Y]ank [P]ath from current file' })
