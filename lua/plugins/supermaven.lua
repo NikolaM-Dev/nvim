88e0f02bc2c5a4929683a8ab82ccd34f4e9d974c
Juan David Merchan Torres <juan1014166@gmail.com>
2024-10-02 07:00:38 -0500

    feat(supermaven): Change to specific commit

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -4,6 +4,7 @@ return {
 	enabled = true,
 
 	event = 'InsertEnter',
+	commit = '40bde487fe31723cdd180843b182f70c6a991226',
 	keys = {
 		{ '<leader>ai', '<cmd>SupermavenToggle<cr>', desc = 'Toggle Supermaven [A][I]' },
 	},
