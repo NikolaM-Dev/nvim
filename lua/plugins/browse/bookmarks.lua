e0b438a940c4f384ab59dc60e36a84a7eec5479e
Juan David Merchan Torres <juan1014166@gmail.com>
2024-11-03 20:10:17 -0500

    feat(browse-bookmarks): Change deprecated lsp link

--- lua/plugins/browse/bookmarks.lua
+++ lua/plugins/browse/bookmarks.lua
@@ -50,7 +50,7 @@ local bookmarks = {
 	juan_david = 'https://mail.google.com/mail/u/0/#inbox',
 	juan = 'https://mail.google.com/mail/u/1/#inbox',
 	lazy = 'https://lazy.folke.io/spec',
-	lsp = 'https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md',
+	lsp = 'https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md',
 	motivation = 'https://www.youtube.com/watch?v=ja75uBnzHS0&list=PLyjmPWCWfM3KZENvXrLjGz5r-c1wMvN3e',
 	music = 'https://www.youtube.com/watch?v=1gSe78TIEEk&list=PLyjmPWCWfM3LJDpmmnJj6U7ot4Q1grdPO',
 	new_repository = 'https://github.com/new',
