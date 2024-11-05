77754e7eb8ac987ec340de14370bccb019f7b25a
Juan David Merchan Torres <juan1014166@gmail.com>
2024-11-05 08:29:37 -0500

    feat(lsp): Remove unnecessary on_attach call

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -142,9 +142,7 @@ return {
 		})
 
 		lspconfig.eslint.setup({
-			on_attach = function(client, bufnr)
-				on_attach(client, bufnr)
-
+			on_attach = function(_, bufnr)
 				vim.api.nvim_create_autocmd('BufWritePre', {
 					buffer = bufnr,
 					command = 'EslintFixAll',
