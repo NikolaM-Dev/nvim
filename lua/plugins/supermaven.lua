0e3e29ce479767617288eb329789757748e1b0cd
Juan David Merchan Torres <juan1014166@gmail.com>
2024-11-11 08:07:45 -0500

    feat(supermaven): Change to use default config

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -5,18 +5,10 @@ return {
 
 	event = 'InsertEnter',
 	commit = '40bde487fe31723cdd180843b182f70c6a991226',
+
 	keys = {
 		{ '<leader>ai', '<cmd>SupermavenToggle<cr>', desc = 'Toggle Supermaven [A][I]' },
 	},
 
-	config = function()
-		require('supermaven-nvim').setup({
-			condition = function()
-				-- Determine if current buffer is in calendar path
-				local is_calendar_path = string.match(vim.fn.expand('%:p:~'), 'calendar')
-
-				return is_calendar_path ~= nil
-			end,
-		})
-	end,
+	config = true,
 }
