5e79a63316e843e0eeb1905e4734bf38bba5ccef
Juan David Merchan Torres <juan1014166@gmail.com>
2024-12-01 20:39:23 -0500

    feat(lib): Add NString.is_empty function

--- lua/lib/string.lua
+++ lua/lib/string.lua
@@ -1,10 +1,16 @@
 ---@class NString
 ---@field trim fun(payload: string): string
+---@field is_empty fun(payload: string | nil): boolean
 local NString = {}
 
 ---@param payload string
 function NString.trim(payload)
 	return payload:match('^%s*(.-)%s*$')
+end
+
+---@param payload string | nil
+function NString.is_empty(payload)
+	return payload == nil or payload == ''
 end
 
 return NString
