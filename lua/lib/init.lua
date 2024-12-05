93be30e249c2ee9379d42714c85e1a7921025601
Juan David Merchan Torres <juan1014166@gmail.com>
2024-12-05 18:57:54 -0500

    feat(lib): Add entry point for lib

---
+++ lua/lib/init.lua
@@ -0,0 +1,9 @@
+local n = {
+	filetype = require('lib.filetype'),
+	logger = require('lib.logger'),
+	map = require('lib.map'),
+	string = require('lib.string'),
+	table = require('lib.table'),
+}
+
+return n
