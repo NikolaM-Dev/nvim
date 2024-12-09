2e1bc8195f766ecbe266aec28544cb2a9b6eba92
Juan David Merchan Torres <juan1014166@gmail.com>
2024-12-09 18:01:42 -0500

    feat(supermaven): Add notify support

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,3 +1,6 @@
+local is_enabled = true
+local logger = n.logger:new('Supermaven')
+
 ---@type LazySpec
 return {
 	'supermaven-inc/supermaven-nvim',
@@ -9,7 +12,20 @@ return {
 	cmd = { 'SupermavenToggle' },
 
 	keys = {
-		{ '<leader>ai', '<cmd>SupermavenToggle<cr>', desc = 'Toggle Supermaven [A][I]' },
+		{
+			'<leader>ai',
+			function()
+				vim.cmd('SupermavenToggle')
+				is_enabled = not is_enabled
+
+				if is_enabled then
+					logger:info('Supermaven is enabled')
+				else
+					logger:warn('Supermaven is disabled')
+				end
+			end,
+			desc = 'Toggle Supermaven [A][I]',
+		},
 	},
 
 	config = true,
