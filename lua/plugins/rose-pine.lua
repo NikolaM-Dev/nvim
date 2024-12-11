0c93fc0f3ee4778ef1577963ce754914b262827b
Juan David Merchan Torres <juan1014166@gmail.com>
2024-12-11 07:56:22 -0500

    refactor(rose-pine): Change to use opts

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -6,23 +6,14 @@ return {
 	lazy = false,
 	priority = 1000,
 	name = 'rose-pine',
-	config = function()
-		require('rose-pine').setup({
-			highlight_groups = {
-				TelescopeBorder = { fg = 'highlight_high', bg = 'none' },
-				TelescopeNormal = { bg = 'none' },
-				TelescopePromptNormal = { bg = 'none' },
-				TelescopeResultsNormal = { fg = 'subtle', bg = 'none' },
-				TelescopeSelectionCaret = { fg = 'rose', bg = 'rose' },
-				TelescopeSelection = { fg = 'text', bg = 'none' },
-			},
 
-			styles = {
-				bold = true,
-				italic = true,
-				transparency = true,
-			},
-		})
+	---@type table
+	opts = {
+	},
+
+	---@param opts table
+	config = function(_, opts)
+		require('rose-pine').setup(opts)
 
 		vim.cmd.colorscheme('rose-pine-moon')
 	end,
