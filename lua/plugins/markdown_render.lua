b2aa1c887e8b1e6fd756d150c1a1b00ce64060aa
Juan David Merchan Torres <juan1014166@gmail.com>
2024-12-16 13:16:00 -0500

    feat(markdwon_render): Remove unnecessary config

--- lua/plugins/markdown_render.lua
+++ lua/plugins/markdown_render.lua
@@ -54,36 +54,4 @@ return {
 			},
 		}
 	end,
-
-	---@param opts render.md.UserConfig
-	config = function(_, opts)
-		if theme == 'catppuccin' then
-			local PALETTE = require('catppuccin.palettes').get_palette('mocha')
-
-			vim.api.nvim_set_hl(0, 'RenderMarkdownCode', { bg = PALETTE.crust })
-			vim.api.nvim_set_hl(0, 'RenderMarkdownCodeInline', { fg = PALETTE.sky })
-			vim.api.nvim_set_hl(0, 'RenderMarkdownDash', { fg = PALETTE.blue })
-
-			local colors = {
-				PALETTE.red,
-				PALETTE.peach,
-				PALETTE.green,
-				PALETTE.teal,
-				PALETTE.lavender,
-				PALETTE.mauve,
-			}
-
-			for i = 1, 6, 1 do
-				local background_highlight = 'RenderMarkdownH' .. i .. 'Bg'
-				local foreground_highlight = 'RenderMarkdownH' .. i
-
-				local color = colors[i]
-
-				vim.api.nvim_set_hl(0, background_highlight, { fg = color })
-				vim.api.nvim_set_hl(0, foreground_highlight, { fg = color })
-			end
-		end
-
-		require('render-markdown').setup(opts)
-	end,
 }
