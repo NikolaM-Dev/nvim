099131cbd036ca52319009e9360b2dd56d3b3a91
Juan David Merchan Torres <juan1014166@gmail.com>
2024-12-20 14:15:45 -0500

    feat(zk): Add keymaps
    - ZkNotes
    - ZkTags

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -9,7 +9,11 @@ return {
 
 		return string.find(cwd, 'second%-brain%.md') ~= nil
 	end,
+
 	keys = {
 		{ '<leader>zb', '<cmd>ZkBacklinks<cr>', desc = '[Z]k [B]acklinks' },
+		{ '<leader>ze', '<cmd>ZkNotes { sort = { \'modified\' } }<cr>', desc = '[Z]k [E]dit' },
 		{ '<leader>zl', '<cmd>ZkLinks<cr>', desc = '[Z]k [L]inks' },
+		{ '<leader>zt', '<cmd>ZkTags<cr>', desc = '[Z]k [T]ags' },
+	},
 }
