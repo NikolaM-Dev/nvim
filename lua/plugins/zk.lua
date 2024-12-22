5106326b7a23bb7032c5f54160f0cdf15ef45a43
Juan David Merchan Torres <juan1014166@gmail.com>
2024-12-21 21:18:21 -0500

    chore(zk): Add interesting comments

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -38,5 +38,48 @@ return {
 	config = function(_, opts)
 		require('zk').setup(opts)
 			},
+		-- local cmd = require('zk.commands')
+		-- local Map = require('lib.map')
+		--
+		-- map.set('n', '<cr>', vim.lsp.buf.definition)
+		-- map.set('n', 'zn', ':ZkNew {title = vim.fn.input(\'Title: \'), dir = vim.fn.input(\'Dir: \')}<cr>')
+		-- map.set('n', 'st', ':ZkTags<cr>')
+		-- map.set('n', 'sf', ':ZkNotes {excludeHrefs = {\'d\'}}<cr>')
+		-- map.set('n', 'so', ':ZkLinks<cr>')
+		-- map.set('n', 'si', ':ZkBacklinks<cr>')
+		-- map.set('i', '<C-r>', function()
+		-- 	-- vim.cmd('norm! i') -- otherwise link gets inserted after cursor
+		-- 	cmd.get('ZkInsertLink')()
+		-- 	-- vim.cmd('normA')
+		-- end)
+		--
+		-- map.set('n', 'zd', function()
+		-- 	local year = vim.fn.strftime('%Y')
+		-- 	local dateISO = vim.fn.strftime('%Y-%m-%d')
+		-- 	local diary_dir = 'd/' .. year
+		--
+		-- 	-- local notebook_root = util.notebook_root(vim.fn.expand("%:p"))
+		-- 	local filename = dateISO
+		-- 	-- local path_table = { notebook_root, diary_dir, filename }
+		-- 	-- local filepath = table.concat(path_table, "/") .. ".md"
+		-- 	local title = ''
+		-- 	-- if tjutil.file_exists(filepath) then
+		-- 	-- 	vim.cmd(":e " .. filepath)
+		-- 	-- else
+		-- 	local subheading = vim.fn.input('Subheading: ')
+		-- 	if subheading == '' then
+		-- 		title = filename
+		-- 	else
+		-- 		title = '\'' .. filename .. ':' .. ' ' .. subheading .. '\''
+		-- 	end
+		-- 	cmd.get('ZkNew')({ dir = 'calendar/daily', title = title })
+		-- 	-- end
+		-- end)
+		--
+		-- map.set(
+		-- 	'v',
+		-- 	'ze',
+		-- 	':ZkNewFromContentSelection {title = vim.fn.input(\'Title: \'), dir = vim.fn.input(\'Dir: \')}<cr>'
+		-- )
 	end,
 }
