cdd39490d3451b230c7b4cc335bbee78009b9e4a
Juan David Merchan Torres <juan1014166@gmail.com>
2024-12-26 07:49:18 -0500

    refactor(telescope): Add a function wrap to keys

--- lua/plugins/telescope/telescope.lua
+++ lua/plugins/telescope/telescope.lua
@@ -9,29 +9,33 @@ return {
 		{ 'natecraddock/telescope-zf-native.nvim' },
 		{ 'nvim-telescope/telescope-fzy-native.nvim' },
 	},
-	keys = {
-		{
-			'<leader>/',
-			function()
-				require('telescope.builtin').current_buffer_fuzzy_find()
-			end,
-			desc = '[/] Fuzzily search in currrent buffer',
-		},
-		{ '<leader>?', '<cmd>Telescope command_history<cr>', desc = 'Command History' },
-		{ '<leader>fa', '<cmd>Telescope live_grep<cr>', desc = 'Live Grep' },
-		{ '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = '[F]ind [H]elp Tags' },
-		{ '<leader>fk', '<cmd>Telescope keymaps<cr>', desc = 'Find [K]eymaps' },
-		{ '<leader>fr', '<cmd>Telescope registers<cr>', desc = '[F]ind [R]egisters' },
-		{ '<leader>fw', '<cmd>Telescope grep_string<cr>', mode = { 'n', 'v' }, desc = 'Grep String' },
-		{ '<leader>gb', '<cmd>Telescope git_branches<cr>', desc = '[G]it [B]ranches' },
-		{ '<leader>gc', '<cmd>Telescope git_commits<cr>', desc = '[G]it [C]ommits' },
-		{ '<leader>;gc', '<cmd>Telescope git_bcommits<cr>', desc = '[G]it B[C]ommits' },
-		{ '<leader>;gs', '<cmd>Telescope git_stash<cr>', desc = '[G]it [S]tash' },
-		{ '<leader>gs', '<cmd>Telescope git_status<cr>', desc = '[G]it [S]tatus' },
-		{ '<leader>man', '<cmd>Telescope man_pages<cr>', desc = '[MAN] pages' },
-		{ '<leader>sp', '<cmd>Telescope spell_suggest<cr>', desc = '[S][p]ell Suggest' },
-		{ '<leader>t', '<cmd>Telescope<cr>', desc = '[T]elescope' },
-	},
+	keys = function()
+		local builtin = require('telescope.builtin')
+
+		return {
+			{
+				'<leader>/',
+				function()
+					builtin.current_buffer_fuzzy_find()
+				end,
+				desc = '[/] Fuzzily search in currrent buffer',
+			},
+			{ '<leader>?', '<cmd>Telescope command_history<cr>', desc = 'Command History' },
+			{ '<leader>fa', '<cmd>Telescope live_grep<cr>', desc = 'Live Grep' },
+			{ '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = '[F]ind [H]elp Tags' },
+			{ '<leader>fk', '<cmd>Telescope keymaps<cr>', desc = 'Find [K]eymaps' },
+			{ '<leader>fr', '<cmd>Telescope registers<cr>', desc = '[F]ind [R]egisters' },
+			{ '<leader>fw', '<cmd>Telescope grep_string<cr>', mode = { 'n', 'v' }, desc = 'Grep String' },
+			{ '<leader>gb', '<cmd>Telescope git_branches<cr>', desc = '[G]it [B]ranches' },
+			{ '<leader>gc', '<cmd>Telescope git_commits<cr>', desc = '[G]it [C]ommits' },
+			{ '<leader>;gc', '<cmd>Telescope git_bcommits<cr>', desc = '[G]it B[C]ommits' },
+			{ '<leader>;gs', '<cmd>Telescope git_stash<cr>', desc = '[G]it [S]tash' },
+			{ '<leader>gs', '<cmd>Telescope git_status<cr>', desc = '[G]it [S]tatus' },
+			{ '<leader>man', '<cmd>Telescope man_pages<cr>', desc = '[MAN] pages' },
+			{ '<leader>sp', '<cmd>Telescope spell_suggest<cr>', desc = '[S][p]ell Suggest' },
+			{ '<leader>t', '<cmd>Telescope<cr>', desc = '[T]elescope' },
+		}
+	end,
 	config = function()
 		local actions = require('telescope.actions')
 		local layout = require('telescope.actions.layout')
