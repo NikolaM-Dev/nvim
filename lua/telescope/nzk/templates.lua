2572f87d8944febcfd39362aa2033547c8b7b403
Juan David Merchan Torres <juan1014166@gmail.com>
2025-02-11 19:27:04 -0500

    feat(nzk): Add base run templates picker scaffolding

--- lua/telescope/nzk/templates.lua
+++ lua/telescope/nzk/templates.lua
@@ -15,3 +15,27 @@
 ---@field new fun(opts: NZKOpts)
 ---@field edit fun(opts: NZKOpts)
 local zk = require('zk')
+local templates = {}
+function templates.run_templates_picker(opts)
+	local new_opts = themes.get_dropdown(opts)
+
+	pickers
+		.new(new_opts, {
+			finder = finders.new_table({
+				results = {
+				},
+			}),
+			sorter = conf.generic_sorter(opts),
+			---@diagnostic disable-next-line: unused-local
+			attach_mappings = function(prompt_bufnr, _map)
+				actions.select_default:replace(function()
+					actions.close(prompt_bufnr)
+				end)
+
+				return true
+			end,
+		})
+		:find()
+end
+
+return templates
