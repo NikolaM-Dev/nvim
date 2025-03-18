5d2fbdad2389d02ed04e55ae134b063b0b53b323
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-18 15:54:00 -0500

    feat(keymaps): Add basic window navigation

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+local map = vim.keymap.set
+
+-- Navigation
+-- TODO: Remove and use tmux plugin
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
