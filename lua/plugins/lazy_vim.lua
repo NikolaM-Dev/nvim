fc58f8bddceaac16089461fd69c76696ee90582a
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-18 10:08:09 -0500

    feat(plugins): Remove default LazyVim autocmds & keymaps

---
+++ lua/plugins/lazy_vim.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+  "LazyVim/LazyVim",
+
+  ---@type LazyVimConfig
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    defaults = {
+      autocmds = false,
+      keymaps = false,
+    },
+  },
+}
