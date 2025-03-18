546b4916f80e4465809408f3db3900a77c2934a4
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-17 21:46:44 -0500

    feat(zk): Add nzk templates keymap

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -15,6 +15,7 @@ return {
 		{ '<leader>zb', '<cmd>ZkBacklinks<cr>', desc = '[Z]k [B]acklinks' },
 		{ '<leader>ze', '<cmd>ZkNotes { sort = { \'modified\' } }<cr>', desc = '[Z]k [E]dit' },
 		{ '<leader>zl', '<cmd>ZkLinks<cr>', desc = '[Z]k [L]inks' },
+		{ '<leader>zn', '<cmd>Telescope nzk templates<cr>', desc = 'n[Z]k [T]emplates' },
 		{ '<leader>zt', '<cmd>ZkTags<cr>', desc = '[Z]k [T]ags' },
 	},
 
