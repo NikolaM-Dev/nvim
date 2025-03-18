269d1e0282b5452e6384c6943904c4687ac55004
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-18 16:04:21 -0500

    feat(indentscope): Add full setup

---
+++ lua/plugins/indentscope.lua
@@ -0,0 +1,19 @@
+---@type LazySpec
+return {
+	'echasnovski/mini.indentscope',
+	enabled = true,
+	event = 'LazyFile',
+
+	---@type table
+	opts = {
+		options = { try_as_border = true },
+		symbol = '│',
+	},
+
+	---@param opts? table
+	config = function(_, opts)
+		opts.draw = { animation = require('mini.indentscope').gen_animation.none() }
+
+		require('mini.indentscope').setup(opts)
+	end,
+}
