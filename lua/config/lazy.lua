7d3a47c4c25abf39fd82639c8df7dfdd6210aeca
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-18 10:49:28 -0500

    style(config): Change formatting

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,4 +1,5 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
 	local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
@@ -16,9 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
 	spec = {
-		-- add LazyVim and import its plugins
 		{ 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
-		-- import/override with your plugins
 		{ import = 'plugins' },
 	},
 	defaults = {
