623e671df8f0fb646992b40b06ec6dc3975dba6b
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-18 16:00:53 -0500

    feat(keymaps): Add more undo `break-points`

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,13 @@ map('n', '<C-h>', '<C-w>h')
 map('n', '<C-j>', '<C-w>j')
 map('n', '<C-k>', '<C-w>k')
 map('n', '<C-l>', '<C-w>l')
+
+-- Add more undo `break-points`
+map('i', '!', '!<C-g>u', { desc = 'Add undo break point after type `!`' })
+map('i', ',', ',<C-g>u', { desc = 'Add undo break point after type `,`' })
+map('i', '-', '-<C-g>u', { desc = 'Add undo break point after type `-`' })
+map('i', '.', '.<C-g>u', { desc = 'Add undo break point after type `.`' })
+map('i', ';', ';<C-g>u', { desc = 'Add undo break point after type `;`' })
+map('i', '?', '?<C-g>u', { desc = 'Add undo break point after type `?`' })
+map('i', '_', '_<C-g>u', { desc = 'Add undo break point after type `_`' })
+map('n', 'o', 'o<C-g>u', { desc = 'Add undo break point after type `o`' })
