5f2ebf9eac732cae8a703af5b6640a647bc7797d
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-18 16:48:44 -0500

    feat(core): Change to colorscheme
    From tokyonight to rose-pine-moon

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,10 +1,12 @@
 ---@type LazySpec
 return {
 	'LazyVim/LazyVim',
+	enabled = true,
 
 	---@type LazyVimConfig
 	---@diagnostic disable-next-line: missing-fields
 	opts = {
+		colorscheme = 'rose-pine-moon',
 		defaults = {
 			autocmds = false,
 			keymaps = false,
