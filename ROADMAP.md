c6e8a959b8b5bba68419c9818c0c736346b69ce7
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-21 19:42:11 -0500

    feat(keymaps): Remove global library use

--- ROADMAP.md
+++ ROADMAP.md
@@ -1,8 +1,10 @@
 # General
 
 - [x] Add Neogit cmd
-- [ ] Change LICENSE
+- [x] Change LICENSE
 - [ ] Remove global lib instant `n`
+  - [x] Remove from [Keymaps](./lua/config/keymaps.lua)
+- [ ] Change to use `neogit` instead of `fugitive` to commit messages
 
 # Docs
 
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,17 +1,28 @@
-local map, logger = n.map, n.logger:new('Keymaps')
+---Adds a new |mapping|. By default with silent mode active
+---@param mode string|string[] Mode short-name, see |nvim_set_keymap()|.
+---							   Can also be list of modes to create mapping on multiple modes.
+---@param lhs string           Left-hand side |{lhs}| of the mapping.
+---@param rhs string|function  Right-hand side |{rhs}| of the mapping, can be a Lua function.
+---@param opts? vim.keymap.set.Opts
+local function map(mode, lhs, rhs, opts)
+	opts = opts or {}
+	opts.silent = opts.silent ~= false
+
+	return vim.keymap.set(mode, lhs, rhs, opts)
+end
 
 -- Better up/down
-map.set({ 'n', 'x' }, 'j', 'v:count == 0 ? \'gj\' : \'j\'', { expr = true, silent = true })
-map.set({ 'n', 'x' }, 'k', 'v:count == 0 ? \'gk\' : \'k\'', { expr = true, silent = true })
+map({ 'n', 'x' }, 'j', 'v:count == 0 ? \'gj\' : \'j\'', { expr = true, silent = true })
+map({ 'n', 'x' }, 'k', 'v:count == 0 ? \'gk\' : \'k\'', { expr = true, silent = true })
 
-map.set(
+map(
 	'n',
 	'<leader>,',
 	'm\'$a,<Esc>`\'',
 	{ desc = 'Put a comma at the other end of the current line and return to last position' }
 )
 
-map.set(
+map(
 	'n',
 	'<leader>.',
 	'm\'$a.<Esc>`\'',
@@ -19,35 +30,35 @@ map.set(
 )
 
 -- Move Lines
-map.set('i', '<A-J>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move down' })
-map.set('i', '<A-K>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move up' })
-map.set('n', '<A-J>', '<cmd>m .+1<cr>==', { desc = 'Move down' })
-map.set('n', '<A-K>', '<cmd>m .-2<cr>==', { desc = 'Move up' })
-map.set('v', '<A-J>', ':m \'>+1<cr>gv=gv', { desc = 'Move down' })
-map.set('v', '<A-K>', ':m \'<-2<cr>gv=gv', { desc = 'Move up' })
+map('i', '<A-J>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move down' })
+map('i', '<A-K>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move up' })
+map('n', '<A-J>', '<cmd>m .+1<cr>==', { desc = 'Move down' })
+map('n', '<A-K>', '<cmd>m .-2<cr>==', { desc = 'Move up' })
+map('v', '<A-J>', ':m \'>+1<cr>gv=gv', { desc = 'Move down' })
+map('v', '<A-K>', ':m \'<-2<cr>gv=gv', { desc = 'Move up' })
 
 -- Insert line below
-map.set('i', '<C-o>', '<esc>o')
+map('i', '<C-o>', '<esc>o')
 
 -- Clear search with <esc>
-map.set({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and clear hlsearch' })
+map({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and clear hlsearch' })
 
 -- Better indenting
-map.set('v', '<', '<gv', { desc = 'Indent Left' })
-map.set('v', '>', '>gv', { desc = 'Indent right' })
+map('v', '<', '<gv', { desc = 'Indent Left' })
+map('v', '>', '>gv', { desc = 'Indent right' })
 
 -- Sort
-map.set('v', '<leader>sl', ':sort<cr>', { desc = '[S]ort [L]ines' })
+map('v', '<leader>sl', ':sort<cr>', { desc = '[S]ort [L]ines' })
 
 -- Save
-map.set({ 'i', 'n', 's', 'v' }, '<C-s>', '<esc><cmd>w<cr>', { desc = '[S]ave file' })
-map.set('n', '<leader><C-s>', map.cmd('noautocmd w'), { desc = '[S]ave file without auto-indent' })
+map({ 'i', 'n', 's', 'v' }, '<C-s>', '<esc><cmd>w<cr>', { desc = '[S]ave file' })
+map('n', '<leader><C-s>', '<cmd>noautocmd w<cr>', { desc = '[S]ave file without auto-indent' })
 
 -- Save all
-map.set('n', '<leader>w', map.cmd('wall'), { desc = 'Write all' })
+map('n', '<leader>w', '<cmd>wall<cr>', { desc = 'Write all' })
 
 -- Quit
-map.set('n', '<C-q>', function()
+map('n', '<C-q>', function()
 	if vim.bo.filetype == 'gitcommit' then
 		return vim.cmd.quit()
 	end
@@ -80,90 +91,89 @@ map.set('n', '<C-q>', function()
 end, { desc = 'Quit' })
 
 -- Quit & save
-map.set('i', '<C-q>', '<esc><cmd>wq<cr>', { desc = 'Quit' })
+map('i', '<C-q>', '<esc><cmd>wq<cr>', { desc = 'Quit' })
 
 -- Quit all
-map.set('n', '<leader>q', map.cmd('qall'), { desc = '[Q]uit all' })
-map.set('n', '<leader><leader>q', map.cmd('qall!'), { desc = '[Q]uit all forced' })
+map('n', '<leader>q', '<cmd>qall<cr>', { desc = '[Q]uit all' })
+map('n', '<leader><leader>q', '<cmd>qall!<cr>', { desc = '[Q]uit all forced' })
 
 -- Quit without save
-map.set('n', '<leader><leader>z', map.cmd('q!'), { desc = 'Quit without save' })
+map('n', '<leader><leader>z', '<cmd>q!<cr>', { desc = 'Quit without save' })
 
 -- Centralized scroll
-map.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up centralized' })
-map.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down centralized' })
+map('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up centralized' })
+map('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down centralized' })
 
 -- Centralized search
-map.set('n', 'N', 'Nzzzv', { desc = 'Previous centralized' })
-map.set('n', 'n', 'nzzzv', { desc = 'Next search centralized' })
+map('n', 'N', 'Nzzzv', { desc = 'Previous centralized' })
+map('n', 'n', 'nzzzv', { desc = 'Next search centralized' })
 
 -- Centralized history
-map.set('n', '<C-o>', '<C-o>zz', { desc = 'Previous History centralized' })
-map.set('n', '<C-i>', '<C-i>zz', { desc = 'Next History centralized' })
+map('n', '<C-o>', '<C-o>zz', { desc = 'Previous History centralized' })
+map('n', '<C-i>', '<C-i>zz', { desc = 'Next History centralized' })
 
 -- Lazy
-map.set('n', '<leader>lp', map.cmd('Lazy profile'), { desc = 'Lazy profile' })
-map.set('n', '<leader>lu', map.cmd('Lazy update'), { desc = 'Lazy update' })
+map('n', '<leader>lp', '<cmd>Lazy profile<cr>', { desc = 'Lazy profile' })
+map('n', '<leader>lu', '<cmd>Lazy update<cr>', { desc = 'Lazy update' })
 
 -- H and L go to begining/end of line
-map.set('n', 'H', '^', { desc = 'Go to Beginning of Line' })
-map.set('n', 'L', '$', { desc = 'Go to End of Line' })
+map('n', 'H', '^', { desc = 'Go to Beginning of Line' })
+map('n', 'L', '$', { desc = 'Go to End of Line' })
 
 -- Add undo break-points
-map.set('i', '!', '!<c-g>u')
-map.set('i', ',', ',<c-g>u')
-map.set('i', '-', '-<c-g>u')
-map.set('i', '.', '.<c-g>u')
-map.set('i', ';', ';<c-g>u')
-map.set('i', '?', '?<c-g>u')
-map.set('i', '_', '_<c-g>u')
-map.set('n', 'o', 'o<c-g>u')
+map('i', '!', '!<c-g>u')
+map('i', ',', ',<c-g>u')
+map('i', '-', '-<c-g>u')
+map('i', '.', '.<c-g>u')
+map('i', ';', ';<c-g>u')
+map('i', '?', '?<c-g>u')
+map('i', '_', '_<c-g>u')
+map('n', 'o', 'o<c-g>u')
 
 -- Paste & replace without copy
-map.set('n', '<leader>Y', [["+Y]])
-map.set({ 'n', 'v' }, '<leader>d', [["_d]])
-map.set({ 'n', 'v' }, '<leader>y', [["+y]])
-map.set('x', '<leader>p', [["_dP]])
+map('n', '<leader>Y', [["+Y]])
+map({ 'n', 'v' }, '<leader>d', [["_d]])
+map({ 'n', 'v' }, '<leader>y', [["+y]])
+map('x', '<leader>p', [["_dP]])
 
 -- Change to right vertial split
-map.set('n', '<leader><leader>l', function()
+map('n', '<leader><leader>l', function()
 	vim.cmd.wincmd('L')
 end, { desc = 'Change to vertial split' })
 
 -- Rename all occurrences in the file
-map.set(
+map(
 	'n',
 	'<leader><leader>s',
-
-	map.cmd('%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', { delay_cmd = true }),
+	':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>',
 	{ desc = 'Rename all occurrences in the file', silent = false }
 )
-map.set(
+map(
 	'v',
 	'<leader><leader>s',
-	map.cmd('s///gI<Left><Left><Left><Left>', { delay_cmd = true }),
+	':s///gI<Left><Left><Left><Left>',
 	{ desc = 'Rename all occurrences in the file (v)', silent = false }
 )
 
-map.set('n', '<leader><leader>;', function()
+map('n', '<leader><leader>;', function()
 	require('utils').go_to_window(4)
 end, { desc = 'Go to window 4' })
 
-map.set(
+map(
 	'n',
 	'<leader>rf',
-	map.cmd('e'),
+	'<cmd>e<cr>',
 	{ desc = '[R]eloads the [F]ile from disk, but will fail if there are unsaved changes' }
 )
 
-map.set(
+map(
 	'n',
 	'<leader>;rf',
-	map.cmd('e!'),
+	'<cmd>e!<cr>',
 	{ desc = '[R]eloads the [F]ile from disk, but forces the reload, discarding any unsaved changes in the buffer' }
 )
 
-map.set('n', '<leader>ip', function()
+map('n', '<leader>ip', function()
 	local line_number = vim.api.nvim_win_get_cursor(0)[1]
 	local line_content = vim.api.nvim_buf_get_lines(0, line_number - 1, line_number, false)[1]
 
@@ -181,28 +191,28 @@ map.set('n', '<leader>ip', function()
 	os.execute(command)
 end, { desc = '[I]mage [P]review' })
 
-map.set('n', '<leader>yp', map.cmd('let @+=expand("%:p:~")'), { desc = '[Y]ank [P]ath from current file' })
+map('n', '<leader>yp', '<cmd>let @+=expand("%:p:~")<cr>', { desc = '[Y]ank [P]ath from current file' })
 
-map.set('n', '<M-j>', map.cmd('cnext'), { desc = 'Next Quickfix list item' })
-map.set('n', '<M-k>', map.cmd('cprev'), { desc = 'Previous Quickfix list item' })
+map('n', '<C-n>', '<cmd>cnext<cr>', { desc = 'Next Quickfix list item' })
+map('n', '<C-p>', '<cmd>cprev<cr>', { desc = 'Previous Quickfix list item' })
 
-map.set('n', '<leader><leader>r', function()
+map('n', '<leader><leader>r', function()
 	local cmd = vim.fn.getline('.')
 
 	vim.cmd(cmd)
 end, { desc = 'Run command on current line' })
 
-map.set('n', '<leader>tw', function()
+map('n', '<leader>tw', function()
 	vim.wo.wrap = not vim.wo.wrap
 
 	if vim.wo.wrap then
-		logger:info('Word wrap is enabled')
+		-- logger:info('Word wrap is enabled')
 	else
-		logger:warn('Word wrap is disabled')
+		-- logger:warn('Word wrap is disabled')
 	end
 end, { desc = '[T]oggle word [W]rap' })
 
-map.set('n', '<leader>tcl', function()
+map('n', '<leader>tcl', function()
 	local current_value = vim.api.nvim_get_option_value('colorcolumn', {})
 
 	if current_value == '' then
@@ -214,14 +224,14 @@ map.set('n', '<leader>tcl', function()
 	end
 end, { desc = '[T]oggle Color[C]o[L]umn' })
 
-map.set('n', '<leader>ts', function()
+map('n', '<leader>ts', function()
 	n.map.toggle_opt('spell', true, false, 'Spelling')
 end, { desc = '[T]oggle [S]pell' })
 
 -- Execute
-map.set('n', '<leader><leader>x', map.cmd('source %'), { desc = 'e[X]ecute current lua file' })
-map.set('n', '<leader>x', map.cmd('.lua'), { desc = 'e[X]ecute current lua line' })
-map.set('v', '<leader>x', ':lua<cr>', { desc = 'e[X]ecute selected lua lines' })
+map('n', '<leader><leader>x', '<cmd>source %<cr>', { desc = 'e[X]ecute current lua file' })
+map('n', '<leader>x', '<cr>.lua<cr>', { desc = 'e[X]ecute current lua line' })
+map('v', '<leader>x', ':lua<cr>', { desc = 'e[X]ecute selected lua lines' })
 
 vim.api.nvim_create_user_command('TmuxLazyGit', function()
 	local command = 'tmux new-window -c "#{pane_current_path}" -n "lazygit" lazygit'
@@ -229,7 +239,7 @@ vim.api.nvim_create_user_command('TmuxLazyGit', function()
 	vim.fn.system(command)
 end, { desc = 'Tmux [L]azy[G]it' })
 
-map.set('n', '<leader>lg', map.cmd('TmuxLazyGit'))
+map('n', '<leader>lg', '<cr>TmuxLazyGit<cr>')
 
 vim.api.nvim_create_user_command('TmuxLazyDocker', function()
 	local command = 'tmux new-window -c "#{pane_current_path}" -n "lazydocker" lazydocker'
@@ -237,7 +247,7 @@ vim.api.nvim_create_user_command('TmuxLazyDocker', function()
 	vim.fn.system(command)
 end, { desc = 'Tmux [L]azy[D]ocker' })
 
-map.set('n', '<leader>ld', map.cmd('TmuxLazyDocker'))
+map('n', '<leader>ld', '<cmd>TmuxLazyDocker<cr>')
 
-map.set('i', '<C-z>', map.cmd('b#', { use_esc = true }), { desc = 'Go to last buffer' })
-map.set('n', '<C-z>', map.cmd('b#'), { desc = 'Go to last buffer' })
+map('i', '<C-z>', '<esc><cmd>b#<cr>', { desc = 'Go to last buffer' })
+map('n', '<C-z>', '<cmd>b#<cr>', { desc = 'Go to last buffer' })
