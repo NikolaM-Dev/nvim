afe50e91ebe979709e65f156d7d7ce329cf38a49
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-24 18:58:58 -0500

    feat(lazy): Add feedback when lazy bootstrap fails

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -4,10 +4,16 @@ if not vim.uv.fs_stat(lazypath) then
 	local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
 	local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
 
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable',
-		lazypath,
-	})
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+			{ out, 'WarningMsg' },
+			{ '\nPress any key to exit...' },
+		}, true, {})
+
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 
 vim.opt.rtp:prepend(lazypath)
