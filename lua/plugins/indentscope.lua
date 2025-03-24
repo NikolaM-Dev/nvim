b2d033f2f0232e6dbe446111f1688024df027ca4
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-23 21:22:48 -0500

    feat(indentscope): Disable indentscope in Snacks dashboard

--- lua/plugins/indentscope.lua
+++ lua/plugins/indentscope.lua
@@ -13,6 +13,27 @@ return {
 		symbol = '│',
 	},
 
+	init = function()
+		vim.api.nvim_create_autocmd('FileType', {
+			pattern = {
+				'snacks_dashboard',
+				-- ...
+			},
+			callback = function()
+				vim.b.miniindentscope_disable = true
+			end,
+		})
+
+		vim.api.nvim_create_autocmd('User', {
+			pattern = 'SnacksDashboardOpened',
+			desc = 'Disable mini indentscope in Snacks Dashboard',
+			group = _G.augroup('disable_mini_indentscope'),
+			callback = function(data)
+				vim.b[data.buf].miniindentscope_disable = true
+			end,
+		})
+	end,
+
 	---@param opts? table
 	config = function(_, opts)
 		opts.draw = { animation = require('mini.indentscope').gen_animation.none() }
