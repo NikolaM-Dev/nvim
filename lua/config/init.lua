9bced7b89cb748d38146a97ffbcfb2b4c97665bb
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-24 19:07:15 -0500

    style(config): Change formatting

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -2,21 +2,24 @@
 vim.uv = vim.uv or vim.loop
 
 require('config.globals')
+
+-- autocmds can be loaded lazily when not opening a file
+local lazy_autocmds = vim.fn.argc(-1) == 0
+if not lazy_autocmds then
+	require('config.autocmds')
+end
+
+vim.api.nvim_create_autocmd('User', {
+	group = _G.augroup('bootstrap'),
+	pattern = 'VeryLazy',
+	callback = function()
+		if lazy_autocmds then
+			require('config.autocmds')
+		end
+
+		require('config.keymaps')
+	end,
+})
+
 require('config.options')
 require('config.lazy')
-
-if vim.fn.argc(-1) == 0 then
-	-- autocmds and keymaps can wait to load
-	vim.api.nvim_create_autocmd('User', {
-		group = vim.api.nvim_create_augroup('LazyVim', { clear = true }),
-		pattern = 'VeryLazy',
-		callback = function()
-			require('config.autocmds')
-			require('config.keymaps')
-		end,
-	})
-else
-	-- load them now so they affect the opened buffers
-	require('config.autocmds')
-	require('config.keymaps')
-end
