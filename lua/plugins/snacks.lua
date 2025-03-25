3dbb8428f6c2a60a5c8844ec229bb24960f28dea
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-24 19:04:55 -0500

    feat(snakcs): Add keymaps
    - delete buffer
    - git stash
    - git status

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,6 +8,21 @@ return {
 	priority = 1000,
 	lazy = false,
 
+	keys = function()
+		local snacks = require('snacks')
+
+		return {
+			-- stylua: ignore start
+			-- buffers
+			{ '<leader>bd', function() snacks.bufdelete() end, desc = '[D]elete [B]uffer' },
+
+			-- git
+			{ '<leader>gS', function() snacks.picker.git_stash() end, desc = '[G]it [S]tash' },
+			{ '<leader>gs', function() snacks.picker.git_status() end, desc = '[G]it [S]tatus' },
+			-- stylua: ignore end
+		}
+	end,
+
 	---@type snacks.Config
 	opts = {
 		bigfile = enabled,
