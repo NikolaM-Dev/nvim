c9d18aa93d5933aac0afdcbd751143527d93af07
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-28 21:58:29 -0500

    feat(options): Add base options

---
+++ lua/config/options.lua
@@ -0,0 +1,17 @@
+vim.o.number = true -- show line numbers
+vim.o.relativenumber = true -- show relative numbers
+vim.o.signcolumn = 'yes:2' -- always show the sign column, otherwise it would shift the text each time
+vim.o.laststatus = 3 -- always and ONLY the last window a status line
+vim.o.showmode = false -- removes the text indicating the mode
+vim.o.splitbelow = true -- split window below
+vim.o.splitright = true -- split window right
+vim.o.linebreak = true -- wrap long lines at characters, not in the middle of a word
+vim.o.autoread = true -- automatically read the file when changed outside of neovim
+vim.o.backup = false -- create backup files
+vim.o.wrap = false -- wrap lines
+
+vim.schedule(function()
+	vim.o.clipboard = 'unnamedplus' -- use system clipboard
+end)
+
+vim.g.border = 'rounded'
