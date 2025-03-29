a9fa9ec0b145d3fba19eaedb3941a154d8c05916
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-29 16:08:42 -0500

    refactor(keymaps): Change to use map utility

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,6 @@
+local map = require('lib').map
 vim.g.mapleader = ' '
 
----@param mode string|string[]
----@param lhs string
----@param rhs string|function
----@param opts? vim.keymap.set.Opts
-local function map(mode, lhs, rhs, opts)
-	opts = opts or {}
-	opts.silent = opts.silent ~= false
 
 	return vim.keymap.set(mode, lhs, rhs, opts)
 end
