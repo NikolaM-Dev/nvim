467b1d4b07f6d340d1285750dee10f37bca17030
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-29 10:40:15 -0500

    feat(keymaps): Add undo breaks
    After type certain characters

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,11 @@ local function map(mode, lhs, rhs, opts)
 
 	return vim.keymap.set(mode, lhs, rhs, opts)
 end
+map('i', '!', '!<c-g>u', { desc = 'Add undo break after type !' })
+map('i', ',', ',<c-g>u', { desc = 'Add undo break after type ,' })
+map('i', '-', '-<c-g>u', { desc = 'Add undo break after type -' })
+map('i', '.', '.<c-g>u', { desc = 'Add undo break after type .' })
+map('i', ';', ';<c-g>u', { desc = 'Add undo break after type ;' })
+map('i', '?', '?<c-g>u', { desc = 'Add undo break after type ?' })
+map('i', '_', '_<c-g>u', { desc = 'Add undo break after type _' })
+map('n', 'o', 'o<c-g>u', { desc = 'Add undo break after type o' })
