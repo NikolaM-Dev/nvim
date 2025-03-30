60b0c60c2f2888d9efb0b1901b525d7b51ed9905
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-29 19:31:09 -0500

    feat(ts_comments): Add base setup

---
+++ lua/plugins/ts_comments.lua
@@ -0,0 +1,10 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+	'folke/ts-comments.nvim',
+	desc = 'Tiny plugin to enhance Neovim\'s native comments',
+	enabled = true,
+
+	event = 'VeryLazy',
+	config = true,
+}
