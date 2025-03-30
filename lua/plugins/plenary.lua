a5c6ad310e9debe58ceab0e35b7bc8529bb5520d
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-29 19:28:01 -0500

    feat(plenary): Add base setup

---
+++ lua/plugins/plenary.lua
@@ -0,0 +1,7 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+	'nvim-lua/plenary.nvim',
+	desc = 'full; complete; entire; absolute; unqualified. All the lua functions I don\'t want to write twice',
+	enabled = true,
+}
