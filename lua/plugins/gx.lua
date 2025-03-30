8c0917379efa3affc8dcb64f053cee8575f9e8d0
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-29 19:22:50 -0500

    feat(gx): Add base setup

---
+++ lua/plugins/gx.lua
@@ -0,0 +1,15 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+	'chrishrb/gx.nvim',
+	desc = 'Implementation of gx without the need of netrw',
+	enabled = true,
+
+	keys = {
+		{ 'gx', '<cmd>Browse<cr>', mode = { 'n', 'x' }, desc = 'Browse in browser' },
+	},
+	init = function()
+		vim.g.netrw_nogx = 1 -- disable netrw gx
+	end,
+	config = true,
+}
