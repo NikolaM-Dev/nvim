7e5a71c4e710cfecd1beb05fa2f21284d04470e5
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-29 21:14:16 -0500

    feat(lua): Add `bullets.vim` base setup

--- lua/plugins/langs/markdwon.lua
+++ lua/plugins/langs/markdwon.lua
@@ -27,4 +27,15 @@ return {
 			opts.formatters_by_ft.markdown = { 'prettier' }
 		end,
 	},
+	{
+		'bullets-vim/bullets.vim',
+		desc = 'Automated bullet lists',
+		enabled = true,
+
+		ft = { 'gitcommit', 'markdown', 'text' },
+		init = function()
+			vim.g.bullets_checkbox_markers = ' .oOx' -- Change `X` to `x` to keep markdown format standard
+			vim.g.bullets_outline_levels = { 'ROM', 'ABC', 'num', 'abc', 'rom', 'std-' } -- Remove * + as outline levels
+		end,
+	},
 }
