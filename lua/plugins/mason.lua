a3300be402c82898b6f262d75e89ffc77a1228f9
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-29 20:56:45 -0500

    feat(mason): Add base setup

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,47 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+	'williamboman/mason.nvim',
+	desc = 'Portable package manager for Neovim that runs everywhere Neovim runs. Easily install and manage LSP servers, DAP servers, linters, and formatters',
+	enabled = true,
+
+	event = 'VeryLazy',
+
+	keys = {
+		{ '<leader>cm', '<cmd>Mason<cr>', desc = '[M]ason' },
+	},
+
+	opts = {
+		ensure_installed = {},
+	},
+
+	config = function(_, opts)
+		require('mason').setup({
+			ensure_installed = opts.ensure_installed,
+			ui = {
+				icons = {
+					package_installed = '󰄳 ',
+					package_pending = ' ',
+					package_uninstalled = '󰚌 ',
+				},
+			},
+		})
+
+		local mason_registry = require('mason-registry')
+		local function ensure_installed()
+			for _, s in ipairs(opts.ensure_installed) do
+				local p = mason_registry.get_package(s)
+
+				if not p:is_installed() then
+					p:install()
+				end
+			end
+		end
+
+		if mason_registry.refresh then
+			mason_registry.refresh(ensure_installed)
+		else
+			ensure_installed()
+		end
+	end,
+}
