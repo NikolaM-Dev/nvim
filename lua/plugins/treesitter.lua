8f16ba557be7ecdbd6f9a49db51187333d5ed5a1
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-29 20:53:05 -0500

    feat(treesitter): Add base setup

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,23 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+	'nvim-treesitter/nvim-treesitter',
+	desc = 'Nvim Treesitter configurations and abstraction layer',
+	enabled = true,
+
+	event = { 'BufNewFile', 'BufReadPost', 'BufWritePre', 'VeryLazy' },
+	build = ':TSUpdate',
+
+	opts = {
+		languages = {},
+	},
+
+	config = function(_, opts)
+		---@diagnostic disable-next-line: missing-fields
+		require('nvim-treesitter.configs').setup({
+			auto_install = false, -- Automatically install missing parsers when entering buffer
+			ensure_installed = opts.languages,
+			highlight = { enable = true },
+		})
+	end,
+}
