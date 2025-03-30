e21a3d4b98025578edeb5be46a4b0b61ebeda3fa
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-29 21:18:39 -0500

    feat(lua): Add stylua as lua formatter

--- lua/plugins/langs/lua.lua
+++ lua/plugins/langs/lua.lua
@@ -20,4 +20,11 @@ return {
 			opts.servers.lua_ls = {}
 		end,
 	},
+	{
+		'stevearc/conform.nvim',
+		ft = 'lua',
+		opts = function(_, opts)
+			opts.formatters_by_ft.lua = { 'stylua' }
+		end,
+	},
 }
