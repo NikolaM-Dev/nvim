51740b743008f188eaa7ebab4d137be94ed762eb
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-29 19:23:32 -0500

    feat(lualine): Add base setup

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,10 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+	'nvim-lualine/lualine.nvim',
+	desc = 'A blazing fast and easy to configure neovim statusline plugin written in pure lua',
+	enabled = true,
+
+	event = 'UIEnter',
+	config = true,
+}
