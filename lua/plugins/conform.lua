92b3c2d20623d01b1b09ec5769ce65034d68e526
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-29 21:07:06 -0500

    feat(conform): Add base setup

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,33 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+	'stevearc/conform.nvim',
+	desc = 'Lightweight yet powerful formatter plugin for Neovim',
+	enabled = true,
+
+	cmd = { 'ConformInfo' },
+	event = 'BufWritePre',
+
+	---@type conform.setupOpts
+	opts = {
+		log_level = vim.log.levels.TRACE,
+		notify_on_error = true,
+		formatters_by_ft = {},
+
+		format_on_save = function()
+			return {
+				timeout_ms = 1500,
+				lsp_format = 'never',
+			}
+		end,
+	},
+
+	---@param opts conform.setupOpts
+	config = function(_, opts)
+		require('conform').setup(opts)
+	end,
+
+	-- init = function()
+	-- 	vim.o.formatexpr = 'v:lua.require\'conform\'.formatexpr()'
+	-- end,
+}
