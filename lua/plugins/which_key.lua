7f6cc91ec0b042cc4c537c947028e9b9b7dc7133
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-29 19:31:40 -0500

    feat(which_key): Add base setup

---
+++ lua/plugins/which_key.lua
@@ -0,0 +1,21 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+	'folke/which-key.nvim',
+	desc = 'WhichKey helps you remember your Neovim keymaps, by showing available keybindings in a popup as you type',
+	enabled = true,
+
+	event = 'VeryLazy',
+
+	---@class wk.Opts
+	opts = {
+		preset = 'helix',
+		spec = {
+			{
+				mode = { 'n', 'v' },
+				{ '[', group = 'prev' },
+				{ ']', group = 'next' },
+			},
+		},
+	},
+}
