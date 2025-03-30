392e7ae04a9edcb06b541cbab862b28ca324243b
Juan David Merchan Torres <juan1014166@gmail.com>
2025-03-29 21:17:50 -0500

    feat(lua): Add lua-language-server and stylua to mason

--- lua/plugins/langs/lua.lua
+++ lua/plugins/langs/lua.lua
@@ -6,4 +6,11 @@ return {
 			vim.list_extend(opts.languages, { 'lua', 'luadoc', 'query' })
 		end,
 	},
+	{
+		'williamboman/mason.nvim',
+		ft = 'lua',
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, { 'lua-language-server', 'stylua' })
+		end,
+	},
 }
