1608cf918d6fbbc06a35327ee56aa05dc94614e5
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-04 13:49:36 -0500

    feat(snacks): Change to allow to show hidden files

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -14,7 +14,7 @@ return {
 		{ '<leader><space>', function() Snacks.picker.smart() end, desc = 'Smart Find Files' },
 		{ '<leader>fb', function() Snacks.picker.buffers() end, desc = 'Buffers' },
 		{ '<leader>fc', function() Snacks.picker.files({ cwd = vim.fn.stdpath('config') }) end, desc = 'Find Config File' },
-		{ '<leader>ff', function() Snacks.picker.files() end, desc = 'Find Files' },
+		{ '<leader>ff', function() Snacks.picker.files({ hidden = true }) end, desc = 'Find Files' },
 		{ '<leader>fg', function() Snacks.picker.git_files() end, desc = 'Find Git Files' },
 		{ '<leader>fp', function() Snacks.picker.projects() end, desc = 'Projects' },
 		{ '<leader>fr', function() Snacks.picker.recent() end, desc = 'Recent' },
