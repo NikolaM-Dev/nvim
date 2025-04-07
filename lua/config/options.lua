190d3b8264b3adcf2b8698ed77fad53c687d8507
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-07 18:25:00 -0500

    sytle(options): Change formatting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,15 +1,16 @@
-vim.o.autoread = true -- automatically read the file when changed outside of neovim
-vim.o.backup = false -- create backup files
+vim.o.autoread = true -- Automatically read the file when changed outside of neovim
+vim.o.backup = false -- Create backup files
 vim.o.laststatus = 3 -- always and ONLY the last window a status line
-vim.o.linebreak = true -- wrap long lines at characters, not in the middle of a word
-vim.o.number = true -- show line numbers
-vim.o.relativenumber = true -- show relative numbers
-vim.o.showmode = false -- removes the text indicating the mode
-vim.o.signcolumn = 'yes:2' -- always show the sign column, otherwise it would shift the text each time
-vim.o.splitbelow = true -- split window below
-vim.o.splitright = true -- split window right
+vim.o.laststatus = 3 -- Always and ONLY the last window a status line
+vim.o.linebreak = true -- Wrap long lines at characters, not in the middle of a word
+vim.o.number = true -- Show line numbers
+vim.o.relativenumber = true -- Show relative numbers
+vim.o.showmode = false -- Removes the text indicating the mode
+vim.o.signcolumn = 'yes:2' -- Always show the sign column, otherwise it would shift the text each time
+vim.o.splitbelow = true -- Split window below
+vim.o.splitright = true -- Split window right
 vim.o.winborder = 'rounded' -- Defines the default border style of floating windows
-vim.o.wrap = false -- wrap lines
+vim.o.wrap = false -- Wrap lines
 
 vim.schedule(function()
 	vim.o.clipboard = 'unnamedplus' -- use system clipboard
