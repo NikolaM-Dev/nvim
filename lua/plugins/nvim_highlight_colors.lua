95b4617b632794b9a8140d84cc01ca0978c924d3
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-08 16:36:13 -0500

    feat(nvim_highlight_colors): Add base setup

---
+++ lua/plugins/nvim_highlight_colors.lua
@@ -0,0 +1,14 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+	'brenoprata10/nvim-highlight-colors',
+	desc = 'Highlight colors for neovim',
+	enabled = true,
+
+	event = 'BufReadPre',
+
+	---@type table
+	opts = {
+		enable_tailwind = true,
+	},
+}
