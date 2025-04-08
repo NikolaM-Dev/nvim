5c9d1784057e954b5ac0a66440a088fcd3f700b5
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-07 19:08:50 -0500

    feat(options): Add list and listchars options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,19 @@
+	-- eol = '¬',
+	extends = '»',
+	lead = ' ',
+	nbsp = '␣',
+	precedes = '«',
+	space = ' ',
+	tab = '  ', -- │
+	trail = '·',
+}
+
 vim.o.autoread = true -- Automatically read the file when changed outside of neovim
 vim.o.backup = false -- Create backup files
 vim.o.inccommand = 'split' -- Shows the effects of a command incrementally using an split
 vim.o.laststatus = 3 -- Always and ONLY the last window a status line
 vim.o.linebreak = true -- Wrap long lines at characters, not in the middle of a word
+vim.o.list = true -- Useful to see the difference between tabs and spaces and for trailing blanks
 vim.o.number = true -- Show line numbers
 vim.o.relativenumber = true -- Show relative numbers
 vim.o.showmode = false -- Removes the text indicating the mode
