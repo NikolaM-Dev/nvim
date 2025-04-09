eed70eb1ae3c6f32f516953dc1961916f6da111b
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-09 16:27:44 -0500

    feat(markview): Add base setup

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,12 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+	'OXY2DEV/markview.nvim',
+	desc = 'A hackable markdown, Typst, latex, html(inline) & YAML previewer for Neovim',
+	enabled = true,
+
+	lazy = false, -- Don't lazy load this plugin as it is already lazy-loaded
+
+	---@type table
+	opts = {},
+}
