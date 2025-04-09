e978139395b80e6caa5d1d0413cbb3349710f4c4
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-09 16:12:06 -0500

    feat(after-bash): Add local options

--- lua/plugins/nvim_lsp.lua
+++ lua/plugins/nvim_lsp.lua
@@ -54,7 +54,7 @@ return {
 			signs = {
 				text = {
 					[vim.diagnostic.severity.ERROR] = '●', --' ',
-					[vim.diagnostic.severity.HINT] = '○', --' ',
+					[vim.diagnostic.severity.HINT] = '○', --' ', ○
 					[vim.diagnostic.severity.INFO] = '●', --' ',
 					[vim.diagnostic.severity.WARN] = '●', --' ',
 				},
