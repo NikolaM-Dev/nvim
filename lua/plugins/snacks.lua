47fd66e2b19fe19baba99ac5bcf7e029ad9ef44f
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-09 15:47:39 -0500

    docs(snacks): Change keymaps description

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -12,23 +12,24 @@ return {
 		-- stylua: ignore start
 		-- find
 		{ '<leader><space>', function() Snacks.picker.smart() end, desc = 'Smart Find Files' },
-		{ '<leader>fb', function() Snacks.picker.buffers() end, desc = 'Buffers' },
-		{ '<leader>fc', function() Snacks.picker.files({ cwd = vim.fn.stdpath('config') }) end, desc = 'Find Config File' },
-		{ '<leader>ff', function() Snacks.picker.files({ hidden = true }) end, desc = 'Find Files' },
-		{ '<leader>fg', function() Snacks.picker.git_files() end, desc = 'Find Git Files' },
-		{ '<leader>fp', function() Snacks.picker.projects() end, desc = 'Projects' },
-		{ '<leader>fr', function() Snacks.picker.recent() end, desc = 'Recent' },
+		{ '<leader>fb', function() Snacks.picker.buffers() end, desc = '[F]ind [B]uffers' },
+		{ '<leader>fc', function() Snacks.picker.files({ cwd = vim.fn.stdpath('config') }) end, desc = '[F]ind [C]onfig Files' },
+		{ '<leader>ff', function() Snacks.picker.files({ hidden = true }) end, desc = '[F]ind [F]iles' },
+		{ '<leader>fg', function() Snacks.picker.git_files() end, desc = '[F]ind [G]it Files' },
+		{ '<leader>fp', function() Snacks.picker.projects() end, desc = '[F]ind [P]rojects' },
+		{ '<leader>fr', function() Snacks.picker.recent() end, desc = '[F]ind [R]ecent' },
 
 		-- search
-		{ '<leader>sh', function() Snacks.picker.help() end, desc = '[H]elp Pages' },
+		{ '<leader>sh', function() Snacks.picker.help() end, desc = 'Search [H]elp Pages' },
 		{ '<leader>sH', function() Snacks.picker.highlights() end, desc = 'Highlights' },
-		{ '<leader>sk', function() Snacks.picker.keymaps() end, desc = 'Keymaps' },
+		{ '<leader>sH', function() Snacks.picker.highlights() end, desc = '[S]earch [H]ighlights' },
+		{ '<leader>sk', function() Snacks.picker.keymaps() end, desc = '[S]earch [K]eymaps' },
 		{ '<leader>si', function() Snacks.picker.icons() end, desc = '[S]earch [I]cons' },
 
 		-- grep
-		{ '<leader>sb', function() Snacks.picker.lines() end, desc = 'Buffer Lines' },
+		{ '<leader>sb', function() Snacks.picker.lines() end, desc = '[S]earch [B]uffer Lines' },
 		{ '<leader>sB', function() Snacks.picker.grep_buffers() end, desc = 'Grep Open Buffers' },
-		{ '<leader>sg', function() Snacks.picker.grep() end, desc = 'Grep' },
+		{ '<leader>sg', function() Snacks.picker.grep() end, desc = '[S]each [G]rep' },
 		{ '<leader>sw', function() Snacks.picker.grep_word() end, desc = 'Visual selection or word', mode = { 'n', 'x' } },
 
 		-- buffers
