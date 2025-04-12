e8edbc2c27b6226fce70ea68d1c62faa2fb9c44b
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-12 16:00:13 -0500

    feat(langs-go): Remove unnecessary ft option

--- lua/plugins/langs/go.lua
+++ lua/plugins/langs/go.lua
@@ -1,28 +1,24 @@
 return {
 	{
 		'nvim-treesitter/nvim-treesitter',
-		ft = 'go',
 		opts = function(_, opts)
 			vim.list_extend(opts.languages, { 'go', 'gomod', 'gosum', 'gotmpl', 'gowork' })
 		end,
 	},
 	{
 		'williamboman/mason.nvim',
-		ft = 'go',
 		opts = function(_, opts)
 			vim.list_extend(opts.ensure_installed, { 'gopls', 'gofumpt', 'goimports' })
 		end,
 	},
 	{
 		'neovim/nvim-lspconfig',
-		ft = 'go',
 		opts = function(_, opts)
 			opts.servers.gopls = {}
 		end,
 	},
 	{
 		'stevearc/conform.nvim',
-		ft = 'go',
 		opts = function(_, opts)
 			opts.formatters_by_ft.go = { 'goimports', 'gofumpt' }
 		end,
