cb29c27788ba62c6061d3b1d175cd301ce2c66f2
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-12 14:48:43 -0500

    feat(nvim_lsp): Change sings symbols

--- lua/plugins/nvim_lsp.lua
+++ lua/plugins/nvim_lsp.lua
@@ -53,10 +53,10 @@ return {
 			severity_sort = true,
 			signs = {
 				text = {
-					[vim.diagnostic.severity.ERROR] = '●', --' ',
-					[vim.diagnostic.severity.HINT] = '○', --' ', ○
+					[vim.diagnostic.severity.ERROR] = '', --' ',
+					[vim.diagnostic.severity.HINT] = '', --' ',  ○
 					[vim.diagnostic.severity.INFO] = '●', --' ',
-					[vim.diagnostic.severity.WARN] = '●', --' ',
+					[vim.diagnostic.severity.WARN] = '▲', --' ',
 				},
 			},
 			virtual_text = {
