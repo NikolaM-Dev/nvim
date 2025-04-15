606a29d4a369c0890792d822e34bf046bad9b476
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-15 09:31:09 -0500

    feat(langs-git): Add diff treesitter syntax

--- lua/plugins/langs/git.lua
+++ lua/plugins/langs/git.lua
@@ -2,7 +2,14 @@ return {
 	{
 		'nvim-treesitter/nvim-treesitter',
 		opts = function(_, opts)
-			vim.list_extend(opts.languages, { 'git_config', 'git_rebase', 'gitattributes', 'gitcommit', 'gitignore' })
+			vim.list_extend(opts.languages, {
+				'diff',
+				'git_config',
+				'git_rebase',
+				'gitattributes',
+				'gitcommit',
+				'gitignore',
+			})
 		end,
 	},
 }
