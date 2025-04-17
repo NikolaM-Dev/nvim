1cb2a86d888cd2c6eb2385361a207c7335dbebce
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-17 11:10:40 -0500

    refactor(langs-mardown): Remove unnecessary ft

--- lua/plugins/langs/markdwon.lua
+++ lua/plugins/langs/markdwon.lua
@@ -1,28 +1,24 @@
 return {
 	{
 		'nvim-treesitter/nvim-treesitter',
-		ft = 'markdown',
 		opts = function(_, opts)
 			vim.list_extend(opts.languages, { 'markdown', 'markdown_inline' })
 		end,
 	},
 	{
 		'williamboman/mason.nvim',
-		ft = 'markdown',
 		opts = function(_, opts)
 			vim.list_extend(opts.ensure_installed, { 'marksman', 'prettier' })
 		end,
 	},
 	{
 		'neovim/nvim-lspconfig',
-		ft = 'markdown',
 		opts = function(_, opts)
 			opts.servers.marksman = {}
 		end,
 	},
 	{
 		'stevearc/conform.nvim',
-		ft = 'markdown',
 		opts = function(_, opts)
 			opts.formatters_by_ft.markdown = { 'prettier' }
 		end,
