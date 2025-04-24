135afbc2b5a81d7d1629ec7528408d7b242cf1ee
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-24 07:05:58 -0500

    feat(fugitive): Add git init keymap

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -11,6 +11,7 @@ return {
 		-- stylua: ignore start
 		{ '<leader>gca', ':vert Git commit -v --amend<cr>', desc = '[G]it [C]ommit [A]mend' },
 		{ '<leader>gcm', ':vert Git commit -v<cr>',         desc = '[G]it [C]o[M]mit' },
+		{ '<leader>gi',  '<cmd>Git init<cr>',               desc = '[G]it [I]nit' },
 		-- stylua: ignore end
 	},
 }
