f789b6c6126a1b8282617cf7707d1f3066ba8c3f
Juan David Merchan Torres <juan1014166@gmail.com>
2025-04-29 16:11:45 -0500

    feat(text): Add base local options

---
+++ after/ftplugin/text.lua
@@ -0,0 +1,9 @@
+vim.opt_local.expandtab = true
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.tabstop = 2
+
+vim.opt_local.colorcolumn = '80'
+vim.opt_local.spell = true
+vim.opt_local.textwidth = 80
+vim.opt_local.wrap = true
