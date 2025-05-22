055dab298b9d513bbad74d0ec3d6eccbac244237
Juan David Merchan Torres <juan1014166@gmail.com>
2025-05-22 13:21:58 -0500

    feat(nvim_fundo): Add base setup

---
+++ lua/plugins/nvim_fundo.lua
@@ -0,0 +1,15 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+	'kevinhwang91/nvim-fundo',
+	desc = ' Forever undo in Neovim ',
+	enabled = true,
+
+	event = 'BufReadPost',
+
+	build = function()
+		require('fundo').install()
+	end,
+
+	config = true,
+}
