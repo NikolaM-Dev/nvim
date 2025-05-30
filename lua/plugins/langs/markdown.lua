a3b5db60b1e27277e6259ec5e05f62f84c640d25
Juan David Merchan Torres <juan1014166@gmail.com>
2025-05-29 19:29:49 -0500

    feat(markdown): Change percentage marks

--- lua/plugins/langs/markdown.lua
+++ lua/plugins/langs/markdown.lua
@@ -30,7 +30,7 @@ return {
 
 		ft = { 'gitcommit', 'markdown', 'text' },
 		init = function()
-			vim.g.bullets_checkbox_markers = ' .oOx' -- Change `X` to `x` to keep markdown format standard
+			vim.g.bullets_checkbox_markers = ' 369x' -- Change `X` to `x` to keep markdown format standard
 			vim.g.bullets_outline_levels = { 'ROM', 'ABC', 'num', 'abc', 'rom', 'std-' } -- Remove * + as outline levels
 		end,
 	},
