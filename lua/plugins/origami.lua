49e1f102b5df34d0ed358f88639fa9269b26e3a9
Juan David Merchan Torres <juan1014166@gmail.com>
2025-08-06 19:33:12 -0500

    feat(origami): Change initial options for folding

--- lua/plugins/origami.lua
+++ lua/plugins/origami.lua
@@ -7,6 +7,11 @@ return {
 
 	event = 'VeryLazy',
 
+	init = function()
+		vim.o.foldlevel = 99 -- disable vim's auto-fold
+		vim.o.foldlevelstart = 99
+	end,
+
 	opts = {
 		autoFold = {
 			enabled = true,
