098909796e4aa48328dcc19b0ac1e63782ff1a6a
Juan David Merchan Torres <juan1014166@gmail.com>
2025-08-06 20:41:29 -0500

    feat(obsidian): Add cmd & some keys

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -6,4 +6,17 @@ return {
 	enabled = true,
 
 	ft = 'markdown',
+
+	cmd = { 'Obsidian' },
+
+	keys = {
+		{ '<leader>o', '<cmd>Obsidian<cr>', desc = '[O]bsidian' },
+		{ 'gb', '<cmd>Obsidian backlinks<cr>', desc = 'Obsidian [G]o to [B]acklinks' },
+		{ 'gl', '<cmd>Obsidian links<cr>', desc = 'Obsidian [G]o to [L]inks' },
+	},
+	---@module 'obsidian'
+	---@param opts obsidian.config.ClientOpts
+	config = function(_, opts)
+		require('obsidian').setup(opts)
+	end,
 }
