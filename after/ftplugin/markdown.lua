5d8884edb205f477546be9325019531b762493e7
Juan David Merchan Torres <juan1014166@gmail.com>
2025-09-01 16:33:08 -0500

    feat(markdown): Remove spell and add conceallevel

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -4,6 +4,6 @@ vim.opt_local.softtabstop = 2
 vim.opt_local.tabstop = 2
 
 vim.opt_local.colorcolumn = '80'
-vim.opt_local.spell = true
+vim.opt_local.conceallevel = 2
 vim.opt_local.textwidth = 80
 vim.opt_local.wrap = true
