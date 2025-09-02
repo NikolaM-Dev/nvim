52e30064d2c1fa3ca2578bd447cb4a6ebd74460c
Juan David Merchan Torres <juan1014166@gmail.com>
2025-09-02 16:24:14 -0500

    style(origami): Change formatting

--- lua/plugins/origami.lua
+++ lua/plugins/origami.lua
@@ -2,7 +2,7 @@
 ---@type LazyPluginSpec
 return {
 	'chrisgrieser/nvim-origami',
-	desc = 'Fold with relentless elegance. A collection of Quality-of-life features related to folding. ',
+	desc = 'Fold with relentless elegance. A collection of Quality-of-life features related to folding.',
 	enabled = true,
 
 	event = 'VeryLazy',
