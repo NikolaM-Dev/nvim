e78ba75bfd684ee12c1899af439645fe1356450f
Juan David Merchan Torres <juan1014166@gmail.com>
2025-09-02 16:30:13 -0500

    chore(flash): Add comments for ideas

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -14,5 +14,10 @@ return {
 	keys = {
 		-- stylua: ignore start
 		{ 's',  function() require('flash').jump() end, desc = 'Flash', mode = { 'n', 'x', 'o' } },
+
+		-- { '<C-s>', mode = { 'c' }, function() require('flash').toggle() end, desc = 'Toggle Flash Search' },
+		-- { 'R', mode = { 'o', 'x' }, function() require('flash').treesitter_search() end, desc = 'Treesitter Search' },
+		-- { 'S', mode = { 'n', 'o', 'x' }, function() require('flash').treesitter() end, desc = 'Flash Treesitter' },
+		-- { 'r', mode = 'o', function() require('flash').remote() end, desc = 'Remote Flash' },
 	},
 }
