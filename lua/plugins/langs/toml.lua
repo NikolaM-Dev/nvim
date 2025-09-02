061b08373e593bf3b3bd764727abbcb7cc576685
Juan David Merchan Torres <juan1014166@gmail.com>
2025-09-02 16:16:54 -0500

    feat(langs): Add toml lsp support using `taplo`

--- lua/plugins/langs/toml.lua
+++ lua/plugins/langs/toml.lua
@@ -5,4 +5,16 @@ return {
 			vim.list_extend(opts.languages, { 'toml' })
 		end,
 	},
+	{
+		'williamboman/mason.nvim',
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, { 'taplo' })
+		end,
+	},
+	{
+		'neovim/nvim-lspconfig',
+		opts = function(_, opts)
+			opts.servers.taplo = {}
+		end,
+	},
 }
