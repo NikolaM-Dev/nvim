8807439586d51e5b32cbd850c4d96d921c8349d2
Juan David Merchan Torres <juan1014166@gmail.com>
2025-09-02 16:10:18 -0500

    feat(langs): Add yaml lsp support

--- lua/plugins/langs/yaml.lua
+++ lua/plugins/langs/yaml.lua
@@ -4,5 +4,17 @@ return {
 		opts = function(_, opts)
 			vim.list_extend(opts.languages, { 'yaml' })
 		end,
+		{
+			'williamboman/mason.nvim',
+			opts = function(_, opts)
+				vim.list_extend(opts.ensure_installed, { 'yaml-language-server' })
+			end,
+		},
+		{
+			'neovim/nvim-lspconfig',
+			opts = function(_, opts)
+				opts.servers.yamlls = {}
+			end,
+		},
 	},
 }
