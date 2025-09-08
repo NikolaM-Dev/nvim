4c212af60c0d7d07a28966a91c0a57a8067efa60
Juan David Merchan Torres <juan1014166@gmail.com>
2025-09-08 16:22:07 -0500

    feat(autocmds): Add auto save autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -16,3 +16,36 @@ autocmd({ 'BufWritePre' }, {
 	group = augroup('remove_trailing_whitespace'),
 	pattern = '*',
 })
+autocmd({ 'BufLeave', 'FocusLost', 'InsertLeave', 'TextChanged' }, {
+	desc = 'Auto save',
+	group = augroup('auto_save'),
+	callback = function(ctx)
+		local saveInstantly = ctx.event == 'FocusLost' or ctx.event == 'BufLeave'
+		local bufnr = ctx.buf
+		local bo, b = vim.bo[bufnr], vim.b[bufnr]
+		local bufname = ctx.file
+
+		if bo.buftype ~= '' or bo.ft == 'gitcommit' or bo.readonly then
+			return
+		end
+
+		if b.saveQueued and not saveInstantly then
+			return
+		end
+
+		b.saveQueued = true
+		vim.defer_fn(function()
+			if not vim.api.nvim_buf_is_valid(bufnr) then
+				return
+			end
+
+			vim.api.nvim_buf_call(bufnr, function()
+				-- saving with explicit name prevents issues when changing `cwd`
+				-- `:update!` suppresses "The file has been changed since reading it!!!"
+				local vimCmd = ('silent! noautocmd lockmarks update! %q'):format(bufname)
+				vim.cmd(vimCmd)
+			end)
+			b.saveQueued = false
+		end, saveInstantly and 0 or 2000)
+	end,
+})
