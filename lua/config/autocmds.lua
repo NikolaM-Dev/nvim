cfa733e6269b2705caebcb13f18083fa331af9d9
Juan David Merchan Torres <juan1014166@gmail.com>
2025-09-08 16:24:22 -0500

    feat(autocmds): Add close all non-existing buffers autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -16,6 +16,65 @@ autocmd({ 'BufWritePre' }, {
 	group = augroup('remove_trailing_whitespace'),
 	pattern = '*',
 })
+
+-- From https://github.com/chrisgrieser/.config/blob/main/nvim/lua/config/autocmds.lua
+local function close_buffers()
+	local closedBuffers = {}
+	local allBufs = vim.fn.getbufinfo({ buflisted = 1 })
+
+	vim.iter(allBufs):each(function(buf)
+		if not vim.api.nvim_buf_is_valid(buf.bufnr) then
+			return
+		end
+
+		local stillExists = vim.uv.fs_stat(buf.name) ~= nil
+		local specialBuffer = vim.bo[buf.bufnr].buftype ~= ''
+		local newBuffer = buf.name == ''
+
+		if stillExists or specialBuffer or newBuffer then
+			return
+		end
+
+		table.insert(closedBuffers, vim.fs.basename(buf.name))
+		vim.api.nvim_buf_delete(buf.bufnr, { force = false })
+	end)
+	if #closedBuffers == 0 then
+		return
+	end
+
+	if #closedBuffers == 1 then
+		vim.notify(closedBuffers[1], nil, { title = 'Buffer closed', icon = '  ' })
+	else
+		local text = '- ' .. table.concat(closedBuffers, '\n- ')
+		vim.notify(text, nil, { title = 'Buffers closed', icon = '󰅗' })
+	end
+
+	-- If ending up in empty buffer, re-open the first oldfile that exists
+	vim.defer_fn(function()
+		if vim.api.nvim_buf_get_name(0) ~= '' then
+			return
+		end
+
+		for _, file in ipairs(vim.v.oldfiles) do
+			if vim.uv.fs_stat(file) and vim.fs.basename(file) ~= 'COMMIT_EDITMSG' then
+				vim.cmd.edit(file)
+				return
+			end
+		end
+	end, 1)
+end
+
+autocmd({ 'FocusGained' }, {
+	desc = 'Close all non-existing buffers on `FocusGained`.',
+	group = augroup('close_all_non-existing_buffers_on_focus_gained'),
+	callback = close_buffers,
+})
+	callback = function()
+		close_buffers()
+	end,
+})
+
+-- From https://github.com/chrisgrieser/.config/blob/main/nvim/lua/config/autocmds.lua
 autocmd({ 'BufLeave', 'FocusLost', 'InsertLeave', 'TextChanged' }, {
 	desc = 'Auto save',
 	group = augroup('auto_save'),
