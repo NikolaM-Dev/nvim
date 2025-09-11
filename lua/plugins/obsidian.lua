4b7ce2b10f2e490429ef26cd2f279e1f6a2eab9f
Juan David Merchan Torres <juan1014166@gmail.com>
2025-09-10 19:53:30 -0500

    feat(obsidian): Add more default frontMatter properties

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -41,6 +41,33 @@ return {
 			return tostring(os.date('%Y%m%dT%H%M%S'))
 		end,
 
+		note_frontmatter_func = function(note)
+			-- Add the title of the note as an alias.
+			if note.title then
+				note:add_alias(note.title)
+			end
+
+			local currentDate = os.date('%Y-%m-%d, %H:%M:%S')
+			local frontMatter = {
+				id = note.id,
+				aliases = note.aliases,
+				tags = note.tags,
+				updatedAt = currentDate,
+			}
+
+			-- `note.metadata` contains any manually added fields in the frontmatter.
+			-- So here we just make sure those fields are kept in the frontmatter.
+			if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+				for k, v in pairs(note.metadata) do
+					frontMatter[k] = v
+				end
+			end
+
+			frontMatter.updatedAt = currentDate
+
+			return frontMatter
+		end,
+
 		picker = {
 			name = 'snacks.pick',
 		},
