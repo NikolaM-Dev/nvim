b31f185c21858cf8a8cb31476d687d22349c9887
Juan David Merchan Torres <juan1014166@gmail.com>
2025-09-12 07:57:17 -0500

    feat(langs): Add lsp support for json files

--- lua/plugins/langs/json.lua
+++ lua/plugins/langs/json.lua
@@ -2,13 +2,19 @@ return {
 	{
 		'nvim-treesitter/nvim-treesitter',
 		opts = function(_, opts)
-			vim.list_extend(opts.languages, { 'json' })
+			vim.list_extend(opts.languages, { 'json', 'jsonc' })
 		end,
 	},
 	{
 		'williamboman/mason.nvim',
 		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, { 'prettierd' })
+			vim.list_extend(opts.ensure_installed, { 'json-lsp', 'prettierd' })
+		end,
+	},
+	{
+		'neovim/nvim-lspconfig',
+		opts = function(_, opts)
+			opts.servers.jsonls = {}
 		end,
 	},
 	{
