ec7055927d964c8deef5ea8b7e25cf7cde9d8d90
Juan David Merchan Torres <juan1014166@gmail.com>
2025-10-16 20:25:47 -0500

    feat(gruvbox_material): Change options

--- lua/plugins/gruvbox_material.lua
+++ lua/plugins/gruvbox_material.lua
@@ -8,6 +8,14 @@ return {
 	lazy = false,
 	priority = 1000,
 
+	init = function()
+		vim.g.gruvbox_material_better_performance = 1
+		vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
+		vim.g.gruvbox_material_enable_bold = 1
+		vim.g.gruvbox_material_enable_italic = 1
+		vim.g.gruvbox_material_float_style = 'none'
+		vim.g.gruvbox_material_transparent_background = 2
+	end,
 
 	config = function()
 		vim.cmd.colorscheme('gruvbox-material')
