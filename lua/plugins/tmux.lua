268b15b5d4c9995f9a00d523196a04005ab5ff76
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-11 20:07:59 -0500

    feat(tmux): Add Neovim terminal support

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -7,6 +7,13 @@ return {
 
 	event = 'VeryLazy',
 
+	keys = {
+		{ '<C-h>', '<cmd>lua require("tmux").move_left()<cr>', desc = 'Change Focus to Left Window', mode = 't' },
+		{ '<C-j>', '<cmd>lua require("tmux").move_bottom()<cr>', desc = 'Change Focus to Down Window', mode = 't' },
+		{ '<C-k>', '<cmd>lua require("tmux").move_top()<cr>', desc = 'Change Focus to Top Window', mode = 't' },
+		{ '<C-l>', '<cmd>lua require("tmux").move_right()<cr>', desc = 'Change Focus to Right Window', mode = 't' },
+	},
+
 	---@type table
 	opts = {
 		copy_sync = { sync_clipboard = false },
