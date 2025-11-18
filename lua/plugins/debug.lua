4b96d9facb53595ca37714d005963d820db3b500
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-18 09:23:18 -0500

    feat(debug): Add overseer base setup

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -48,8 +48,8 @@ return {
 
 	{
 		'theHamsta/nvim-dap-virtual-text',
-		enabled = true,
 		desc = 'This plugin adds virtual text support to nvim-dap.',
+		enabled = true,
 
 		---@module 'nvim-dap-virtual-text'
 		---@type nvim_dap_virtual_text_options
@@ -59,5 +59,15 @@ return {
 			clear_on_continue = true,
 			virt_text_pos = 'eol',
 		},
+	},
+
+	{
+		'stevearc/overseer.nvim',
+		desc = 'A task runner and job management plugin for Neovim',
+		enabled = true,
+
+		---@module 'overseer'
+		---@type overseer.SetupOpts
+		opts = {},
 	},
 }
