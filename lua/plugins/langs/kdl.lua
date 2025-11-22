98a124573313744037cbb2f6152b66e7c3e8c64f
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-22 13:52:42 -0500

    feat(kdl): Add support for kdl lang

---
+++ lua/plugins/langs/kdl.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		'nvim-treesitter/nvim-treesitter',
+		opts = function(_, opts)
+			vim.list_extend(opts.languages, { 'kdl' })
+		end,
+	},
+	{
+		'williamboman/mason.nvim',
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, { 'kdlfmt' })
+		end,
+	},
+	{
+		'stevearc/conform.nvim',
+		opts = function(_, opts)
+			opts.formatters_by_ft.kdl = { 'kdlfmt' }
+		end,
+	},
+}
