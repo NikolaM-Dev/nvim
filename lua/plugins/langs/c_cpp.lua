3e4bee8323bdbfec20dcf5f6cf70a476a46b76bf
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-22 08:56:36 -0500

    feat(c_cpp): Add debug setup to use `Launch main.c` with codelldb

--- lua/plugins/langs/c_cpp.lua
+++ lua/plugins/langs/c_cpp.lua
@@ -8,7 +8,7 @@ return {
 	{
 		'williamboman/mason.nvim',
 		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, { 'clang-format', 'clangd' })
+			vim.list_extend(opts.ensure_installed, { 'clang-format', 'clangd', 'codelldb' })
 		end,
 	},
 	{
@@ -28,6 +28,32 @@ return {
 		opts = function(_, opts)
 			opts.formatters_by_ft.c = { 'clang-format' }
 			opts.formatters_by_ft.cpp = { 'clang-format' }
+		end,
+	},
+	{
+		'mfussenegger/nvim-dap',
+
+		---@param opts nkl.debug
+		opts = function(_, opts)
+			opts.adapters.codelldb = {
+				command = 'codelldb',
+				id = 'codelldb',
+				type = 'executable',
+			}
+
+			opts.configurations.c = {
+				{
+					name = '  Launch main.c (codelldb)',
+					type = 'codelldb',
+					request = 'launch',
+					program = vim.fn.getcwd() .. '/main',
+					cwd = '${workspaceFolder}',
+					stopOnEntry = false,
+					preLaunchTask = 'build main.c',
+				},
+			}
+
+			opts.configurations.cpp = opts.configurations.c
 		end,
 	},
 }
