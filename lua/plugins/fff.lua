24bb74e1278af74bc11c8b1ddef4e96bae9609da
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-22 09:31:58 -0500

    feat(fff): Add base setup

---
+++ lua/plugins/fff.lua
@@ -0,0 +1,45 @@
+---@module 'lazy.types'
+---@type LazyPluginSpec
+return {
+	'dmtrKovalenko/fff.nvim',
+	desc = 'Finally a Fast Fuzzy File Finder for neovim',
+	enabled = true,
+	lazy = false,
+
+	build = function()
+		require('fff.download').download_or_build_binary()
+	end,
+
+	keys = {
+		-- stylua: ignore start
+		{ '<leader>ff', function() require('fff').find_files() end, desc = '[F][F]Files' },
+		-- stylua: ignore end
+	},
+
+	---@type table
+	opts = {
+		lazy_sync = false,
+		max_threads = 10,
+		prompt = '   ',
+
+		layout = {
+			height = 0.85,
+			preview_position = 'right',
+			preview_size = 0.6,
+			prompt_position = 'top',
+			width = 0.85,
+		},
+		keymaps = {
+			close = '<Esc>',
+			select = '<CR>',
+			select_split = '<C-s>',
+			select_vsplit = '<C-v>',
+			select_tab = '<C-t>',
+			move_up = { '<Up>', '<C-p>', '<C-k>' },
+			move_down = { '<Down>', '<C-n>', '<C-j>' },
+			preview_scroll_up = '<C-u>',
+			preview_scroll_down = '<C-d>',
+			toggle_debug = '<F2>',
+		},
+	},
+}
