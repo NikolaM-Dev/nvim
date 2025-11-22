9b5bd6a50025620032449d1c37494f0a5eecd4c7
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-22 08:55:38 -0500

    feat(debug): Add nvim-dap keymaps based on LazyVim standard

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,3 +1,22 @@
+---@param config {type?:string, args?:string[]|fun():string[]?}
+local function get_args(config)
+	local args = type(config.args) == 'function' and (config.args() or {}) or config.args or {} --[[@as string[] | string ]]
+	local args_str = type(args) == 'table' and table.concat(args, ' ') or args --[[@as string]]
+
+	config = vim.deepcopy(config)
+	---@cast args string[]
+	config.args = function()
+		local new_args = vim.fn.expand(vim.fn.input('Run with args: ', args_str)) --[[@as string]]
+		if config.type and config.type == 'java' then
+			---@diagnostic disable-next-line: return-type-mismatch
+			return new_args
+		end
+		return require('dap.utils').splitstr(new_args)
+	end
+
+	return config
+end
+
 ---@module 'lazy.types'
 ---@type LazyPluginSpec[]
 return {
@@ -7,10 +26,26 @@ return {
 		docs = 'https://codeberg.org/mfussenegger/nvim-dap/wiki',
 		enabled = true,
 
-		dependencies = {
-			'rcarriga/nvim-dap-ui',
-			'stevearc/overseer.nvim',
-			'theHamsta/nvim-dap-virtual-text',
+		keys = {
+			-- stylua: ignore start
+			{ '<leader>dB', function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = 'Breakpoint Condition' },
+			{ '<leader>db', function() require('dap').toggle_breakpoint() end, desc = 'Toggle Breakpoint' },
+			{ '<leader>dc', function() require('dap').continue() end, desc = 'Run/Continue' },
+			{ '<leader>da', function() require('dap').continue({ before = get_args }) end, desc = 'Run with Args' },
+			{ '<leader>dC', function() require('dap').run_to_cursor() end, desc = 'Run to Cursor' },
+			{ '<leader>dg', function() require('dap').goto_() end, desc = 'Go to Line (No Execute)' },
+			{ '<leader>di', function() require('dap').step_into() end, desc = 'Step Into' },
+			{ '<leader>dj', function() require('dap').down() end, desc = 'Down' },
+			{ '<leader>dk', function() require('dap').up() end, desc = 'Up' },
+			{ '<leader>dl', function() require('dap').run_last() end, desc = 'Run Last' },
+			{ '<leader>do', function() require('dap').step_out() end, desc = 'Step Out' },
+			{ '<leader>dO', function() require('dap').step_over() end, desc = 'Step Over' },
+			{ '<leader>dP', function() require('dap').pause() end, desc = 'Pause' },
+			{ '<leader>dr', function() require('dap').repl.toggle() end, desc = 'Toggle REPL' },
+			{ '<leader>ds', function() require('dap').session() end, desc = 'Session' },
+			{ '<leader>dt', function() require('dap').terminate() end, desc = 'Terminate' },
+			{ '<leader>dw', function() require('dap.ui.widgets').hover() end, desc = 'Widgets' },
+			-- stylua: ignore end
 		},
 
 		---@class nkl.debug
