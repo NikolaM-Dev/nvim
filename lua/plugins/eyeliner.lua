ce4a7ee535f6cc7341548961c357b61b620286cd
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-25 11:02:25 -0500

    feat(eyeliner): Add base setup

---
+++ lua/plugins/eyeliner.lua
@@ -0,0 +1,11 @@
+---@module 'lazy.types'
+---@type LazySpec
+return {
+	'jinh0/eyeliner.nvim',
+	desc = '👀 Move faster with unique f/F indicators.',
+	enabled = true,
+
+	opts = {
+		dim = true,
+	},
+}
