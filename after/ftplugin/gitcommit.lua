22434a841a0a4188fc1183cd28c543ad8a3bf0bc
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-25 10:59:20 -0500

    feat(gitcommit): Change to use `nkl.key`

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,6 +1,5 @@
-local bmap = require('lib').bmap
-
 vim.opt_local.spell = true
 
+local bmap = nkl.key.bmap
 bmap('n', '<C-s>', '<cmd>wq<cr>', { desc = 'Write and Quit buffer' })
 bmap({ 'i', 'x' }, '<C-s>', '<esc><cmd>wq<cr>', { desc = 'Write and Quite buffer' })
