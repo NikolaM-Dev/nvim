f8bd78598f29abcd3ca3ec8a8cbef756c3ae2c70
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-25 10:58:51 -0500

    feat(go): Add amazing local keymaps to type with ease

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -2,3 +2,10 @@ vim.opt_local.expandtab = false
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.tabstop = 4
+
+local bmap = nkl.key.bmap
+bmap('i', ';', ':=', { desc = '󰟓  Insert `:=` after type `;`' })
+bmap('i', ',', '<-', { desc = '󰟓  Insert `<-` after type `,`' })
+bmap('i', ',,', '->', { desc = '󰟓  Insert `->` after type `,,`' })
+bmap('i', '\'', '""<Left>', { desc = '󰟓  Insert `"$0"` after type `\'$0\'`' })
+bmap('i', '"', '\'\'<Left>', { desc = '󰟓  Insert `\'$0\'` after type `"$0"`' })
