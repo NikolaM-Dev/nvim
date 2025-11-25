985e30c7d3801c3f31ef50e9cddb8c73b649eb44
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-25 11:00:35 -0500

    feat(markdown): Remove local conceallevel and wrap options

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -4,9 +4,7 @@ vim.opt_local.softtabstop = 2
 vim.opt_local.tabstop = 2
 
 vim.opt_local.colorcolumn = '80'
-vim.opt_local.conceallevel = 2
 vim.opt_local.textwidth = 80
-vim.opt_local.wrap = true
 
 -- -- Add the key mappings only for Markdown files in a zk notebook.
 -- if require('zk.util').notebook_root(vim.fn.expand('%:p')) ~= nil then
