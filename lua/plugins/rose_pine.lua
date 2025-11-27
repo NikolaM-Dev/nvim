f3e98f5667ac70f7cc75996093783bf0b3bdc369
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-27 13:25:05 -0500

    feat(rose_pine): Change snacks.lazygit highlights

--- lua/plugins/rose_pine.lua
+++ lua/plugins/rose_pine.lua
@@ -25,6 +25,19 @@ return {
 	config = function(_, opts)
 		require('rose-pine').setup(opts)
 
-		vim.cmd('colorscheme rose-pine-moon')
+		vim.cmd.colorscheme(' rose-pine-moon')
+
+		-- snacks.lazygit highlights
+		vim.api.nvim_set_hl(0, 'activeBorderColor', { fg = '#3e8fb0', bold = true })
+		vim.api.nvim_set_hl(0, 'cherryPickedCommitBgColor', { fg = 'none' })
+		vim.api.nvim_set_hl(0, 'cherryPickedCommitFgColor', { fg = '#f6c177' })
+		vim.api.nvim_set_hl(0, 'defaultFgColor', { fg = '#e0def4' })
+		vim.api.nvim_set_hl(0, 'inactiveBorderColor', { fg = '#6e6a86' })
+		vim.api.nvim_set_hl(0, 'markedBaseCommitBgColor', { fg = 'none' })
+		vim.api.nvim_set_hl(0, 'markedBaseCommitFgColor', { fg = '#f6c177' })
+		vim.api.nvim_set_hl(0, 'optionsTextColor', { fg = '#9ccfd8' })
+		vim.api.nvim_set_hl(0, 'searchingActiveBorderColor', { fg = '#f6c177', bold = true })
+		vim.api.nvim_set_hl(0, 'selectedLineBgColor', { fg = '#393552' })
+		vim.api.nvim_set_hl(0, 'unstagedChangesColor', { fg = '#6e6a86' })
 	end,
 }
