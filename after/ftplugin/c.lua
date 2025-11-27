459ac954fc88761da08419b67894e68760f7e6e9
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-27 12:58:58 -0500

    feat(c): Add insert keymaps to type with ease

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -5,3 +5,7 @@ vim.opt_local.tabstop = 2
 
 vim.opt_local.colorcolumn = '80'
 vim.opt_local.textwidth = 80
+
+local bmap = nkl.key.bmap
+bmap('i', ';;', '<Esc>mzA;<Esc>`za', { desc = '  Insert `;` at the eol and comeback' })
+bmap('i', '\'', '""<Left>', { desc = '  Insert `"$0"` after type `\'$0\'`' })
