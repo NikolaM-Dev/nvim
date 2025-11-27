09a58b68cc3eba17bebd630d5fddc284e4788294
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-27 13:04:45 -0500

    feat(qf): Add custom rename keymap

---
+++ after/ftplugin/qf.lua
@@ -0,0 +1,8 @@
+nkl.key.bmap('n', 'grn', function()
+	local is_loc_list = vim.fn.getloclist(0, { winid = 0 }).winid == 0
+	if is_loc_list then
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(':cdo s//g<left><left>', true, false, true), 'n', false)
+	else
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(':ldo s//gc<left><left>', true, false, true), 'n', false)
+	end
+end, { desc = ' [R]e[n]ame'})
