7d3262f3cddc59c314ca9d565253aea97b97649f
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-27 12:59:34 -0500

    feat(go): Change channel insert keymaps lhs

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -5,7 +5,7 @@ vim.opt_local.tabstop = 4
 
 local bmap = nkl.key.bmap
 bmap('i', ';', ':=', { desc = '󰟓  Insert `:=` after type `;`' })
-bmap('i', ',', '<-', { desc = '󰟓  Insert `<-` after type `,`' })
-bmap('i', ',,', '->', { desc = '󰟓  Insert `->` after type `,,`' })
+bmap('i', ',,', '<-', { desc = '󰟓  Insert `<-` after type `,,`' })
+bmap('i', '..', '->', { desc = '󰟓  Insert `->` after type `;;`' })
 bmap('i', '\'', '""<Left>', { desc = '󰟓  Insert `"$0"` after type `\'$0\'`' })
 bmap('i', '"', '\'\'<Left>', { desc = '󰟓  Insert `\'$0\'` after type `"$0"`' })
