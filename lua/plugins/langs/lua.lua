e6553266027e25f6c0a727c6b4e18ae5d2d77250
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-27 19:52:14 -0500

    style(lua): Remove unnecessary comments

--- lua/plugins/langs/lua.lua
+++ lua/plugins/langs/lua.lua
@@ -33,16 +33,15 @@ return {
 			opts.formatters_by_ft.lua = { 'stylua' }
 		end,
 	},
+
 	{
-		-- TODO: Review
-		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-		-- used for completion, annotations and signatures of Neovim apis
 		'folke/lazydev.nvim',
 		desc = 'Faster LuaLS setup for Neovim',
 		enabled = true,
 
 		cmd = 'LazyDev',
 		ft = 'lua',
+
 		opts = {
 			library = {
 				-- Load luvit types when the `vim.uv` word is found
