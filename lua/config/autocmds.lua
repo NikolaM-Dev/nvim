4a5dd04b28627456960f0237b016a43d1a9c2a19
Juan David Merchan Torres <juan1014166@gmail.com>
2025-11-27 20:17:10 -0500

    style(autocmds): Change to use lua snake_case standard

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -150,7 +150,7 @@ autocmd({ 'BufLeave', 'FocusLost', 'InsertLeave', 'TextChanged' }, {
 	desc = 'Auto save',
 	group = augroup('auto_save'),
 	callback = function(ctx)
-		local saveInstantly = ctx.event == 'FocusLost' or ctx.event == 'BufLeave'
+		local save_instantly = ctx.event == 'FocusLost'
 		local bufnr = ctx.buf
 		local bo, b = vim.bo[bufnr], vim.b[bufnr]
 		local bufname = ctx.file
@@ -159,11 +159,11 @@ autocmd({ 'BufLeave', 'FocusLost', 'InsertLeave', 'TextChanged' }, {
 			return
 		end
 
-		if b.saveQueued and not saveInstantly then
+		if b.save_queued and not save_instantly then
 			return
 		end
 
-		b.saveQueued = true
+		b.save_queued = true
 		vim.defer_fn(function()
 			if not vim.api.nvim_buf_is_valid(bufnr) then
 				return
@@ -172,10 +172,10 @@ autocmd({ 'BufLeave', 'FocusLost', 'InsertLeave', 'TextChanged' }, {
 			vim.api.nvim_buf_call(bufnr, function()
 				-- saving with explicit name prevents issues when changing `cwd`
 				-- `:update!` suppresses "The file has been changed since reading it!!!"
-				local vimCmd = ('silent! noautocmd lockmarks update! %q'):format(bufname)
-				vim.cmd(vimCmd)
+				vim.cmd(string.format('silent! noautocmd lockmarks update! %q', bufname))
 			end)
-			b.saveQueued = false
-		end, saveInstantly and 0 or 2000)
+
+			b.save_queued = false
+		end, save_instantly and 0 or 2000)
 	end,
 })
