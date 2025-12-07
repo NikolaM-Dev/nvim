f2bf577761ef8d3889ac7077eab20fd3f975da3e
Juan David Merchan Torres <juan1014166@gmail.com>
2025-12-06 21:30:41 -0500

    feat(json): Change `'` by `"` in insert mode

--- after/ftplugin/json.lua
+++ after/ftplugin/json.lua
@@ -6,3 +6,7 @@ vim.opt_local.tabstop = 2
 vim.opt_local.colorcolumn = '80'
 vim.opt_local.commentstring = '// %s'
 vim.opt_local.textwidth = 80
+
+local bmap = nkl.key.bmap
+bmap('i', '\'', '""<Left>', { desc = '󰟓  Insert `"$0"` after type `\'$0\'`' })
+bmap('i', '"', '\'\'<Left>', { desc = '󰟓  Insert `\'$0\'` after type `"$0"`' })
