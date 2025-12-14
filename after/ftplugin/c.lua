aff860c26157bba7d0fa12973f2d31277bab7413
Juan David Merchan Torres <juan1014166@gmail.com>
2025-12-13 19:40:38 -0500

    feat(c): Change `"` by `'`

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -9,3 +9,4 @@ vim.opt_local.textwidth = 80
 local bmap = nkl.key.bmap
 bmap('i', ';;', '<Esc>mzA;<Esc>`za', { desc = '  Insert `;` at the eol and comeback' })
 bmap('i', '\'', '""<Left>', { desc = '  Insert `"$0"` after type `\'$0\'`' })
+bmap('i', '"', '\'\'<Left>', { desc = '  Insert `\'$0\'` after type `"$0"`' })
