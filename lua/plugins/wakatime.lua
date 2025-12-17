f71b7ac295959d3edc8df3edf60a86151d252ead
Juan David Merchan Torres <juan1014166@gmail.com>
2025-12-16 21:33:49 -0500

    feat(wakatime): Remove setup

--- lua/plugins/wakatime.lua
+++
@@ -1,8 +0,0 @@
----@module 'lazy.types'
----@type LazySpec
-return {
-	'wakatime/vim-wakatime',
-	desc = ' Vim plugin for automatic time tracking and metrics generated from your programming activity. ',
-	enabled = true,
-	lazy = false,
-}
