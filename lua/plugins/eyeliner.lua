eade3bd56fb2f8163b1d3b542382edf55bf7d8a5
Juan David Merchan Torres <juan1014166@gmail.com>
2026-01-05 19:50:36 -0500

    feat(eyeliner): Change to support clever_f

--- lua/plugins/eyeliner.lua
+++ lua/plugins/eyeliner.lua
@@ -2,10 +2,12 @@
 ---@type LazySpec
 return {
 	'jinh0/eyeliner.nvim',
+	---@see lua/plugins/clever_f.lua
 	desc = '👀 Move faster with unique f/F indicators.',
 	enabled = true,
 
 	opts = {
+		default_keymaps = false,
 		dim = true,
 		highlight_on_key = true,
 	},
