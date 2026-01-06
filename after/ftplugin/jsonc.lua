942c2ff869bee6993e7db89891b0c9c31478eb31
Juan David Merchan Torres <juan1014166@gmail.com>
2026-01-05 19:49:26 -0500

    feat(json/c): Change conceallevel to 0
    I don't want a clunky UI

--- after/ftplugin/jsonc.lua
+++ after/ftplugin/jsonc.lua
@@ -7,6 +7,8 @@ vim.opt_local.colorcolumn = '80'
 vim.opt_local.commentstring = '// %s'
 vim.opt_local.textwidth = 80
 
+vim.opt_local.conceallevel = 0
+
 local bmap = nkl.key.bmap
 bmap('i', '\'', '""<Left>', { desc = '󰘦  Insert `"$0"` after type `\'$0\'`' })
 bmap('i', '"', '\'\'<Left>', { desc = '󰘦  Insert `\'$0\'` after type `"$0"`' })
