9852715872aa2c36f7b240170903b192e4d7e8ce
Juan David Merchan Torres <juan1014166@gmail.com>
2026-02-13 21:32:31 -0500

    feat(typescript): Change to combine Javascript and Typescript

--- lua/plugins/langs/typescript.lua
+++ lua/plugins/langs/typescript.lua
@@ -4,25 +4,28 @@ return {
 	{
 		'nvim-treesitter/nvim-treesitter',
 		opts = function(_, opts)
-			vim.list_extend(opts.languages, { 'tsx', 'typescript' })
+			vim.list_extend(opts.languages, { 'javascript', 'jsdoc', 'typescript' })
 		end,
 	},
 	{
 		'williamboman/mason.nvim',
 		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, { 'prettierd', 'typescript-language-server' })
+			vim.list_extend(opts.ensure_installed, { 'eslint-lsp', 'prettier', 'tsgo', 'typescript-language-server' })
 		end,
 	},
 	{
 		'neovim/nvim-lspconfig',
 		opts = function(_, opts)
+			-- opts.servers.tsgo = {}
+			opts.servers.eslint = {}
 			opts.servers.ts_ls = {}
 		end,
 	},
 	{
 		'stevearc/conform.nvim',
 		opts = function(_, opts)
-			opts.formatters_by_ft.typescript = { 'prettierd' }
+			opts.formatters_by_ft.javascript = { 'prettier' }
+			opts.formatters_by_ft.typescript = { 'prettier' }
 		end,
 	},
 }
