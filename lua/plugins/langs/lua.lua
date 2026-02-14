b9d33aa754711c9c64e8d47011ad26abc54d5ce9
Juan David Merchan Torres <juan1014166@gmail.com>
2026-02-13 21:28:52 -0500

    feat(lua): Add wezterm-types using lazydev.nvim

--- lua/plugins/langs/lua.lua
+++ lua/plugins/langs/lua.lua
@@ -47,6 +47,7 @@ return {
 				-- Load luvit types when the `vim.uv` word is found
 				{ path = '${3rd}/luv/library', words = { 'vim%.uv' } },
 				{ path = 'snacks.nvim', words = { 'Snacks' } },
+				{ path = 'wezterm-types', mods = { 'wezterm' } },
 			},
 		},
 	},
