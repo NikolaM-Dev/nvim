2bc6b55be0a49faec17da49628794b90a6ce197c
Juan David Merchan Torres <juan1014166@gmail.com>
2026-02-13 21:28:23 -0500

    feat(wezterm_types): Add base setup

---
+++ lua/plugins/wezterm_types.lua
@@ -0,0 +1,8 @@
+---@module 'lazy.types'
+---@type LazySpec
+return {
+	'DrKJeff16/wezterm-types',
+	desc = 'WezTerm Lua config types for Lua Language Server, with Neovim and VSCode compatibility.',
+
+	ft = 'lua',
+}
