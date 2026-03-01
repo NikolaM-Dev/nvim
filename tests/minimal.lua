5b364205469b948e935e92823e06d491b67b9c54
Juan David Merchan Torres <juan1014166@gmail.com>
2026-02-28 20:07:11 -0500

    tests(tests): Add setup to tests plugins isolated

---
+++ tests/minimal.lua
@@ -0,0 +1,8 @@
+vim.env.LAZY_STDPATH = '.repro'
+load(vim.fn.system('curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua'))()
+
+require('lazy.minit').repro({
+	spec = {
+		-- Plugin to test
+	},
+})
