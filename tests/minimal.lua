e64776423eb472199bf76e32f926b8ad5f52d994
Juan David Merchan Torres <juan1014166@gmail.com>
2026-03-18 21:13:01 -0500

    test(minimal): Add sensible options

--- tests/minimal.lua
+++ tests/minimal.lua
@@ -1,8 +1,15 @@
 vim.env.LAZY_STDPATH = '.repro'
+
+vim.g.mapleader = ' '
+vim.o.number = true
+vim.o.relativenumber = true
+
 load(vim.fn.system('curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua'))()
 
 require('lazy.minit').repro({
 	spec = {
-		-- Plugin to test
+		{
+			-- Place your plugins here
+		},
 	},
 })
