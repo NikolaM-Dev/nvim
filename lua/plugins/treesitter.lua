bef60071e5631d215838ec3b61d865e4348f8f26
Juan David Merchan Torres <juan1014166@gmail.com>
2026-04-19 19:55:56 -0500

    feat(treesitter): Add missing features lost on the new version

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,21 +3,43 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
 	desc = 'Nvim Treesitter configurations and abstraction layer',
+	lazy = false,
 	enabled = true,
 
-	event = { 'BufNewFile', 'BufReadPost', 'BufWritePre', 'VeryLazy' },
 	build = ':TSUpdate',
 
+	---@class nkl.ts_opts
+	---@field languages string[]
 	opts = {
 		languages = {},
 	},
 
+	---@param opts nkl.ts_opts
 	config = function(_, opts)
-		---@diagnostic disable-next-line: missing-fields
-		require('nvim-treesitter').setup({
-			auto_install = false, -- Automatically install missing parsers when entering buffer
-			ensure_installed = opts.languages,
-			highlight = { enable = true },
+		require('nvim-treesitter').install(opts.languages)
+
+		vim.api.nvim_create_autocmd('FileType', {
+			group = vim.api.nvim_create_augroup('treesitter.setup', {}),
+			callback = function(args)
+				local buf = args.buf
+				local ft = args.match
+
+				-- Skips if the buffer and/or the filetype is not installed
+				local language = vim.treesitter.language.get_lang(ft) or ft
+				if not vim.treesitter.language.add(language) then
+					return
+				end
+
+				-- Folding
+				vim.wo.foldmethod = 'expr'
+				vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+				-- Highlight
+				vim.treesitter.start(buf, language)
+
+				-- Indentation
+				vim.bo[buf].indentexpr = 'v:lua.require\'nvim-treesitter\'.indentexpr()'
+			end,
 		})
 	end,
 }
