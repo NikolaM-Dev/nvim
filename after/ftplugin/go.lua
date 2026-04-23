40548f603e14590cce0073aeef0e002e48e1d678
Juan David Merchan Torres <juan1014166@gmail.com>
2026-04-22 20:04:49 -0500

    feat(go): Remove insertion collisions

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -2,7 +2,5 @@ nkl.opt.indent(4, 'tabs')
 
 local bmap = nkl.key.bmap
 bmap('i', ';', ':=', { desc = '󰟓  Insert `:=` after type `;`' })
-bmap('i', ',,', '<-', { desc = '󰟓  Insert `<-` after type `,,`' })
-bmap('i', '..', '->', { desc = '󰟓  Insert `->` after type `;;`' })
-bmap('i', '\'', '""<Left>', { desc = '󰟓  Insert `"$0"` after type `\'$0\'`' })
-bmap('i', '"', '\'\'<Left>', { desc = '󰟓  Insert `\'$0\'` after type `"$0"`' })
+bmap('i', '<<', '<-', { desc = '󰟓  Insert `<-` after type `,,`' })
+bmap('i', '>>', '->', { desc = '󰟓  Insert `->` after type `;;`' })
