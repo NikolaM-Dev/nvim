1e3592c0c11e0f07ab373d753b4fa15da763fc4c
Juan David Merchan Torres <juan1014166@gmail.com>
2026-04-29 09:27:44 -0500

    feat(sidekick): Change to use pi instead of opencode

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -7,7 +7,7 @@ return {
 
 	keys = {
 		-- stylua: ignore start
-		{ '<C-.>', function() require('sidekick.cli').toggle({ name = 'opencode', focus = true }) end, desc = 'Sidekick Toggle', mode = { 'n', 't', 'i', 'x' } },
+		{ '<C-.>', function() require('sidekick.cli').toggle({ name = 'pi', focus = true }) end, desc = 'Sidekick Toggle', mode = { 'n', 't', 'i', 'x' } },
 		{ '<leader>ad', function() require('sidekick.cli').close() end, desc = 'Detach a CLI Session' },
 		{ '<leader>at', function() require('sidekick.cli').send({ msg = '{this}' }) end, mode = { 'x', 'n' }, desc = 'Send This' },
 		{ '<leader>af', function() require('sidekick.cli').send({ msg = '{file}' }) end, desc = 'Send File' },
@@ -21,15 +21,30 @@ return {
 	opts = {
 		cli = {
 			mux = {
-				enabled = true,
+				enabled = false,
 			},
 			prompts = {
 				['high quality'] = 'Could you please review the following in terms of High Quality Code (correctness, design, and style) {file}',
-				['polish'] = 'Please polish the following file: {file}',
+				['polish'] = [[
+Act as a professional editor. Polish the text in the file I going to share with you to make it fluent, natural, and professional.
+
+Instructions:
+
+- **Correct:** Fix all grammar, spelling, punctuation, and capitalization errors.
+- **Improve Flow:** Enhance sentence clarity and conciseness. Use natural phrasing.
+- **Preserve Content:** Do not add, remove, or change any facts, figures, names, or technical details. Keep the original meaning and emphasis.
+- **Preserve Format:** Maintain all original formatting (markdown, lists, code blocks, etc.).
+- **Handle Ambiguity:** If text is ambiguous and cannot be fixed without adding facts, mark it with `[AMBIGUOUS — needs clarification]`.
+- **Suggest headings:** Based on the polished file suggest possible a possible heading (max 3)
+]],
 			},
 			win = {
+				-- layout = 'float',
 				keys = {
+					buffers = { '<M-n>', 'buffers', mode = 'nt', desc = 'open file picker' },
+					files = { '<M-N>', 'files', mode = 'nt', desc = 'open file picker' },
 					prompt = { '<M-p>', 'prompt', mode = 't', desc = 'insert prompt or context' },
+					-- buffers = nil,
 				},
 			},
 		},
@@ -39,24 +54,23 @@ return {
 	config = function(_, opts)
 		require('sidekick').setup(opts)
 
-		vim.api.nvim_create_autocmd({ 'WinClosed', 'WinNew' }, {
-			desc = 'Resize dap-ui when Sidekick window is opened or closed',
-			group = nkl.augroup('sidekick_dapui_resize'),
-			callback = function()
-				local dapui_ok, dapui = pcall(require, 'dapui')
-				local dap_ok, dap = pcall(require, 'dap')
-
-				if not dap_ok or not dapui_ok then
-					return
-				end
-
-				local has_debbug_session = #dap.sessions() > 0
-				if has_debbug_session then
-					vim.schedule(function()
-						dapui.open({ reset = true })
-					end)
-				end
-			end,
-		})
+		-- vim.api.nvim_create_autocmd({ 'WinClosed', 'WinNew' }, {
+		-- 	desc = 'Resize dap-ui when Sidekick window is opened or closed',
+		-- 	group = nkl.augroup('sidekick_dapui_resize'),
+		-- 	callback = function()
+		-- 		local dapui_ok, dapui = pcall(require, 'dapui')
+		-- 		local dap_ok, dap = pcall(require, 'dap')
+		--
+		-- 		if not dap_ok or not dapui_ok then
+		-- 			return
+		-- 		end
+		--
+		-- 		if dap.session() then
+		-- 			vim.schedule(function()
+		-- 				dapui.open({ reset = true })
+		-- 			end)
+		-- 		end
+		-- 	end,
+		-- })
 	end,
 }
