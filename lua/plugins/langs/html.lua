1531c1adf2e6e3ec4ae4d4401587e4ea2bb125a2
Juan David Merchan Torres <juan1014166@gmail.com>
2026-05-06 19:32:50 -0500

    feat(html): Change to use prettier over prettierd

--- lua/plugins/langs/html.lua
+++ lua/plugins/langs/html.lua
@@ -10,20 +10,19 @@ return {
 	{
 		'williamboman/mason.nvim',
 		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, { 'prettierd', 'html-lsp', 'emmet-language-server' })
+			vim.list_extend(opts.ensure_installed, { 'prettier', 'html-lsp' })
 		end,
 	},
 	{
 		'neovim/nvim-lspconfig',
 		opts = function(_, opts)
-			opts.servers.emmet_language_server = {}
 			opts.servers.html = {}
 		end,
 	},
 	{
 		'stevearc/conform.nvim',
 		opts = function(_, opts)
-			opts.formatters_by_ft.html = { 'prettierd' }
+			opts.formatters_by_ft.html = { 'prettier' }
 		end,
 	},
 }
