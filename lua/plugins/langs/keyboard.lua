53f553d66095b58a22add671ee9b68daf9b6f0cc
Juan David Merchan Torres <juan1014166@gmail.com>
2026-05-12 19:37:17 -0500

    feat(keyboard): Add dts support

--- lua/plugins/langs/keyboard.lua
+++ lua/plugins/langs/keyboard.lua
@@ -7,4 +7,16 @@ return {
 			vim.list_extend(opts.languages, { 'devicetree' })
 		end,
 	},
+	{
+		'williamboman/mason.nvim',
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, { 'ginko_ls' })
+		end,
+	},
+	{
+		'neovim/nvim-lspconfig',
+		opts = function(_, opts)
+			opts.servers.ginko_ls = {}
+		end,
+	},
 }
