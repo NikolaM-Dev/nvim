c23b7cde6bdce7b2e314bbc0e89bcb3f11cd0967
Juan David Merchan Torres <juan1014166@gmail.com>
2026-05-13 19:42:02 -0500

    feat(abbreviations): Add more abbreviations

--- lua/config/abbreviations.lua
+++ lua/config/abbreviations.lua
@@ -3,3 +3,5 @@ local ab = nkl.key.ab
 ab('ia', 'ellip', '…', 'Ellipsis')
 ab('ia', 'ie', 'i.e.', 'In Other Words')
 ab('ia', 'eg', 'e.g.', 'For Example')
+ab('ia', 'gh', 'GitHub', 'GitHub')
+ab('ia', 'yt', 'YouTube', 'YouTube')
