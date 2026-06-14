33df5ea15cc9a255c60a53658173780a40f5f399
Juan David Merchan Torres <juan1014166@gmail.com>
2026-06-14 08:24:12 -0500

    feat(bash): Add debugging support

--- lua/plugins/langs/bash.lua
+++ lua/plugins/langs/bash.lua
@@ -10,7 +10,7 @@ return {
 	{
 		'williamboman/mason.nvim',
 		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, { 'bash-language-server', 'shfmt' })
+			vim.list_extend(opts.ensure_installed, { 'bash-debug-adapter', 'bash-language-server', 'shfmt' })
 		end,
 	},
 	{
@@ -27,6 +27,45 @@ return {
 			opts.formatters_by_ft.bash = { 'shfmt' }
 			opts.formatters_by_ft.sh = { 'shfmt' }
 			opts.formatters_by_ft.zsh = { 'shfmt' }
+		end,
+	},
+
+	{
+		'mfussenegger/nvim-dap',
+
+		---@param opts nkl.debug
+		opts = function(_, opts)
+			opts.adapters.bashdb = {
+				command = 'bash-debug-adapter',
+				id = 'bashdb',
+				name = 'bashdb',
+				type = 'executable',
+			}
+
+			opts.configurations.bash = {
+				{
+					name = '  Launch file',
+					type = 'bashdb',
+					request = 'launch',
+					file = '${file}',
+					program = '${file}',
+					cwd = '${workspaceFolder}',
+					args = {},
+					argsString = '',
+					env = {},
+					pathBash = '/bin/bash',
+					pathCat = 'cat',
+					pathMkfifo = 'mkfifo',
+					pathPkill = 'pkill',
+					terminalKind = 'integrated',
+					pathBashdb = vim.fn.stdpath('data')
+						.. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
+					pathBashdbLib = vim.fn.stdpath('data') .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
+				},
+			}
+
+			opts.configurations.sh = opts.configurations.bash
+			opts.configurations.zsh = opts.configurations.bash
 		end,
 	},
 }
