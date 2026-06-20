8143ada01516ef6fc1154de58f8b4d9237a40a7b
Juan David Merchan Torres <juan1014166@gmail.com>
2026-06-19 20:42:52 -0500

    feat(env_hide): add base setup

---
+++ lua/plugins/env_hide.lua
@@ -0,0 +1,11 @@
+---@module 'lazy.types'
+---@type LazySpec
+return {
+	'MannuVilasara/env-hide.nvim',
+	desc = 'protect your env files from being shown',
+	enabled = true,
+
+	opts = {
+		enable_keymaps = false,
+	},
+}
